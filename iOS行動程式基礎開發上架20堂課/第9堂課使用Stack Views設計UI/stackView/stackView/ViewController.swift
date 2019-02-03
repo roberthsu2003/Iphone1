262c92b60258c6fd33174d0ec93adb188908ac35
徐國堂 <roberthus2003@gmail.com>
2019-02-03 14:14:25 +0800

    modify

---
+++ iOS行動程式基礎開發上架20堂課/第9堂課使用Stack Views設計UI/stackView/stackView/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  stackView
+//
+//  Created by t1 on 2019/1/13.
+//  Copyright © 2019年 gjun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBAction func leffButtonPress(_ sender:UIButton){
+        print("press Left Button");
+    }
+    
+    @IBAction func rightButtonPress(_ sender:UIButton){
+        print("press Right Button");
+    }
+
+
+}
+
