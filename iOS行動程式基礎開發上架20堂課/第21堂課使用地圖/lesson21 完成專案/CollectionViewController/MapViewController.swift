b94eae893957b42685d98b76d0e010326e2f455c
徐國堂 <roberthus2003@gmail.com>
2019-02-09 22:23:13 +0800

    modify

---
+++ iOS行動程式基礎開發上架20堂課/第21堂課使用地圖/lesson21 完成專案/CollectionViewController/MapViewController.swift
@@ -0,0 +1,60 @@
+//
+//  MapViewController.swift
+//  Map
+//
+//  Created by Robert on 2019/2/9.
+//  Copyright © 2019 Robert. All rights reserved.
+//
+
+import UIKit
+import MapKit
+class MapViewController: UIViewController {
+    @IBOutlet var mapView:MKMapView!
+    var city:City?;
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "地圖:\(city?.city ?? "")";
+        self.mapView.delegate = self;
+        let geoCoder = CLGeocoder();
+        geoCoder.geocodeAddressString(city?.city ?? "台北") { (placemarks:[CLPlacemark]?, error:Error?) in
+            if error == nil,let placemarks = placemarks  {
+                let annotation = MKPointAnnotation();
+                annotation.title = self.city!.city;
+                annotation.subtitle = self.city!.country;
+                let placemark = placemarks[0];
+                if let location = placemark.location{
+                    annotation.coordinate = location.coordinate;
+                    self.mapView.showAnnotations([annotation], animated: true);
+                    self.mapView.selectAnnotation(annotation, animated: true);
+                    let region = MKCoordinateRegion(center: annotation.coordinate, latitudinalMeters: 10000, longitudinalMeters: 10000);
+                    self.mapView.setRegion(region, animated: false);
+                }
+            }else{
+                print("出錯了");
+            }
+        }
+    }
+    
+
+}
+
+extension MapViewController:MKMapViewDelegate{
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView?{
+        let identifier = "Pin";
+        if annotation.isKind(of: MKUserLocation.self){
+            return nil;
+        }
+        
+        var annotationView:MKPinAnnotationView? = mapView.dequeueReusableAnnotationView(withIdentifier: identifier) as? MKPinAnnotationView;
+        
+        if annotationView == nil {
+            annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+            annotationView?.canShowCallout = true;
+        }
+        
+        let leftIconView = UIImageView(frame: CGRect.init(x: 0, y: 0, width: 53, height: 53))
+        leftIconView.image = UIImage(named: city!.image);
+        annotationView?.leftCalloutAccessoryView = leftIconView;
+        return annotationView;
+    }
+}
---
+++ iOS行動程式基礎開發上架20堂課/第21堂課使用地圖/lesson21 完成專案/CollectionViewController/ViewController.swift
@@ -0,0 +1,125 @@
+//
+//  ViewController.swift
+//  CollectionViewController
+//
+//  Created by Robert on 2019/2/9.
+//  Copyright © 2019 Robert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController {
+    var citys = [City]();
+    var cityIsMarded:[Bool]!;
+    let cellID = "CELL"
+    
+    override func awakeFromNib() {
+        super .awakeFromNib();
+        let bundle = Bundle.main;
+        let pathURL = bundle.url(forResource: "citylist", withExtension: "plist")!;
+        if let dicCitys = NSArray(contentsOf: pathURL) as? [[String:Any]]{
+            for dicCity in dicCitys{
+                let city = City();
+                city.city = dicCity["City"] as? String;
+                city.country = dicCity["Country"] as? String;
+                city.continent = dicCity["Continent"] as? String;
+                city.image = dicCity["Image"] as? String;
+                city.local = dicCity["Local"] as? String;
+                city.url = dicCity["url"] as? String;
+                city.latitude = dicCity["lat"] as? Double;
+                city.longitude = dicCity["long"] as? Double;
+                citys.append(city);
+            }
+        }
+    }
+    
+    override var prefersStatusBarHidden : Bool {
+        return true
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "goMap" {
+            let destinationViewController = segue.destination as! MapViewController;
+            let indexPaths = collectionView.indexPathsForSelectedItems?.first;
+            destinationViewController.city = citys[indexPaths?.row ?? 0];
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationItem.title = "城市"
+        let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout;
+        layout.itemSize = CGSize(width: 180, height: 120);
+    }
+
+
+}
+
+extension ViewController{
+    //UICollectionViewDataSource
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int{
+        return citys.count;
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: self.cellID,
+                                                      for: indexPath)
+        let city = citys[indexPath.row];
+        if cell.backgroundView == nil { // brand new cell
+            //cell.backgroundColor = UIColor.lightGray
+            
+            let bv = UIView(frame:cell.bounds)
+            bv.contentMode = .scaleToFill
+            bv.backgroundColor = UIColor.lightGray;
+            cell.backgroundView = bv
+            
+            let selectionView = UIView(frame:cell.bounds)
+            selectionView.backgroundColor = UIColor(white:0.2, alpha:0.5)
+            cell.selectedBackgroundView = selectionView
+            
+            let cityImageView = UIImageView(frame: cell.bounds);
+            cityImageView.tag = 1;
+            cityImageView.contentMode = .scaleAspectFill;
+            cityImageView.translatesAutoresizingMaskIntoConstraints = false;
+            cell.contentView.addSubview(cityImageView);
+            NSLayoutConstraint.activate([
+                cityImageView.topAnchor.constraint(equalTo: cell.contentView.topAnchor, constant: 0),
+                cityImageView.bottomAnchor.constraint(equalTo: cell.contentView.bottomAnchor, constant: 0),
+                cityImageView.leadingAnchor.constraint(equalTo: cell.contentView.leadingAnchor, constant: 0),
+                cityImageView.trailingAnchor.constraint(equalTo: cell.contentView.trailingAnchor, constant: 0)
+                ])
+            
+            
+            let cityLabel = UILabel()
+            cityLabel.translatesAutoresizingMaskIntoConstraints = false
+            cityLabel.tag = 2
+            cell.contentView.addSubview(cityLabel)
+            NSLayoutConstraint.activate([
+                cityLabel.centerXAnchor.constraint(equalTo:cell.contentView.centerXAnchor),
+                cityLabel.centerYAnchor.constraint(equalTo:cell.contentView.centerYAnchor)
+                ])
+            cityLabel.textColor = .white
+            cityLabel.highlightedTextColor = .black
+            cityLabel.backgroundColor = .clear
+            
+            
+        }
+        let cityImageView = cell.viewWithTag(1) as! UIImageView
+        cityImageView.image = UIImage(named: city.image)
+        let lab = cell.viewWithTag(2) as! UILabel
+        lab.text = city.city;
+        return cell
+    }
+}
+
+extension ViewController{
+    //UICollectionViewDelegate
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath){
+        performSegue(withIdentifier: "goMap", sender: nil);
+    }
+}
+
