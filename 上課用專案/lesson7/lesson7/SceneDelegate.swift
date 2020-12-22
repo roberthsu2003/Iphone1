c54e022357c7ca170ef5fc271b23dd6a6b3bb814
roberthsu2003 <roberthsu2003@gmail.com>
2020-12-22 20:07:09 +0800

    Update SceneDelegate.swift

--- 上課用專案/lesson7/lesson7/SceneDelegate.swift
+++ 上課用專案/lesson7/lesson7/SceneDelegate.swift
@@ -13,10 +13,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+        //UIWindow的實體會被指定給window屬性
         guard let _ = (scene as? UIWindowScene) else { return }
+        print("window已經顯示!")
+        print(window!)
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
