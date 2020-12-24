cd78918f6c54a050ceb780cd495209b1bb1114ae
roberthsu2003 <roberthsu2003@gmail.com>
2020-12-24 20:25:03 +0800

    modify

---
+++ 上課用專案/lesson7-1/lesson7-1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lesson7-1
+//
+//  Created by 徐國堂 on 2020/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        //view.backgroundColor = UIColor(red: 0.3, green: 0.2, blue: 0.6, alpha: 1)
+    }
+
+
+}
+
