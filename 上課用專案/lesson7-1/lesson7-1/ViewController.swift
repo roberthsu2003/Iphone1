ad0f53667a7b422fcb644efcf45d0f0af6df7f75
roberthsu2003 <roberthsu2003@gmail.com>
2020-12-24 20:38:13 +0800

    modify

--- 上課用專案/lesson7-1/lesson7-1/ViewController.swift
+++ 上課用專案/lesson7-1/lesson7-1/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         //view.backgroundColor = UIColor(red: 0.3, green: 0.2, blue: 0.6, alpha: 1)
+        let v1 = UIView(frame: CGRect(x: 113, y: 111, width: 132, height: 194))
+        v1.backgroundColor = UIColor(red: 1, green: 0.4, blue: 1, alpha: 1)
+        view.addSubview(v1)
     }
 
 
