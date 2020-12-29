821aaad9c7afe038d213293bad97190d58c3bbca
roberthsu2003 <roberthsu2003@gmail.com>
2020-12-29 21:54:26 +0800

    modify

---
+++ 上課用專案/lesson9-1/lesson9-1/MyViewController.swift
@@ -0,0 +1,11 @@
+//
+//  MyViewController.swift
+//  lesson9-1
+//
+//  Created by 徐國堂 on 2020/12/29.
+//
+
+import UIKit
+class MyViewController:UIViewController{
+    
+}
---
+++ 上課用專案/lesson9-1/lesson9-1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lesson9-1
+//
+//  Created by 徐國堂 on 2020/12/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
