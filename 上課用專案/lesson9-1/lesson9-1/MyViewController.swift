76ad68c177a7fec9d4bd1f9a971cef0fdf52fd10
roberthsu2003 <roberthsu2003@gmail.com>
2020-12-31 21:40:33 +0800

    modify

--- 上課用專案/lesson9-1/lesson9-1/MyViewController.swift
+++ 上課用專案/lesson9-1/lesson9-1/MyViewController.swift
@@ -15,7 +15,17 @@ class MyViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         //再多做一些自已要做的功能事
+        leftButton.addTarget(self, action: #selector(leftClick(_:)), for: .touchUpInside)
         
+        rightButton.addTarget(self, action: #selector(rightClick(_:)), for: .touchUpInside)
+    }
+    
+    @objc func leftClick(_ sender:UIButton){
+        titleLabel.text = "Left Button Click"
+    }
+    
+    @objc func rightClick(_ sender:UIButton){
+        titleLabel.text = "Right Button Click"
     }
   
  
