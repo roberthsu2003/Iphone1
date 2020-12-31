1b2de67edfb3a2a2701531ae6b076ec12eab1023
roberthsu2003 <roberthsu2003@gmail.com>
2020-12-31 20:40:52 +0800

    modify

--- 上課用專案/lesson9-1/lesson9-1/MyViewController.swift
+++ 上課用專案/lesson9-1/lesson9-1/MyViewController.swift
@@ -11,19 +11,9 @@ class MyViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        //再多做一些自已要做的功能事
+        
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+  
+ 
 }
