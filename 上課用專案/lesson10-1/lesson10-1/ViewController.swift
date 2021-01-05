5d6d577967e125233d4b4f6600e09a98ba826a01
roberthsu2003 <roberthsu2003@gmail.com>
2021-01-05 21:53:35 +0800

    modify

--- 上課用專案/lesson10-1/lesson10-1/ViewController.swift
+++ 上課用專案/lesson10-1/lesson10-1/ViewController.swift
@@ -7,13 +7,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDataSource {
+    @IBOutlet var cityTableView:UITableView!
     //要依據plist的架構來設定cities的資料類型
     //如果使用type!,代表暫時是nil,第一下就有值
     var cities:[[String:Any]]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        cityTableView.dataSource = self
         let bundle = Bundle.main
         guard let pathUrl = bundle.url(forResource: "citylist", withExtension: "plist") else{
             print("程式出錯了")
@@ -22,12 +24,23 @@ class ViewController: UIViewController {
         
         let tempArray = NSArray(contentsOf: pathUrl)
         cities = tempArray as? [[String:Any]]
-        print(cities)
+        for city in cities{
+            print(city)
+        }
         
         
-        
-        
-        
+    }
+    
+    func tableView(_ tableView: UITableView,
+                   numberOfRowsInSection section: Int) -> Int{
+        return 100;
+    }
+    
+    func tableView(_ tableView: UITableView,
+                   cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
+        cell.textLabel?.text = "Hello! World!"
+        return cell;
     }
 
 
