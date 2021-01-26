4f5204a554843f78895dff0660679f470a293c9d
roberthsu2003 <roberthsu2003@gmail.com>
2021-01-26 20:24:16 +0800

    modify

---
+++ 上課用專案/lesson14-1/lesson14-1/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  lesson14-1
+//
+//  Created by 徐國堂 on 2021/1/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func yellowClick(_ sender:UIBarButtonItem){
+        print("yellow")
+    }
+    
+    @IBAction func redClick(_ sender:UIBarButtonItem){
+        print("red")
+    }
+
+
+}
+
