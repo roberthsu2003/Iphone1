6620616027a76c94c3cb013a3e6cfb4a7fee4578
roberthsu2003 <roberthsu2003@gmail.com>
2021-02-04 22:05:11 +0800

    modify

---
+++ 上課用專案/lesson16-1/lesson10-1/DetailViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  DetailViewCell.swift
+//  lesson16-1
+//
+//  Created by 徐國堂 on 2021/2/4.
+//
+
+import UIKit
+
+class DetailViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
