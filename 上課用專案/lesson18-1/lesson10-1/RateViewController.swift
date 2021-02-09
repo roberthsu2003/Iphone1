3ac8e323ed809c60c4a847e62e05a28431c906ae
roberthsu2003 <roberthsu2003@gmail.com>
2021-02-09 21:35:37 +0800

    modify

---
+++ 上課用專案/lesson18-1/lesson10-1/RateViewController.swift
@@ -0,0 +1,23 @@
+//
+//  RateViewController.swift
+//  lesson18-1
+//
+//  Created by 徐國堂 on 2021/2/9.
+//
+
+import UIKit
+
+class RateViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func backPress(_ sender:UIBarItem){
+        dismiss(animated: true, completion: nil)
+    }
+
+}
