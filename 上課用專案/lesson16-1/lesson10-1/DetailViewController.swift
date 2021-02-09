1c03ece41df24ae77992c229e510afeeab45e8fe
roberthsu2003 <roberthsu2003@gmail.com>
2021-02-09 20:36:32 +0800

    modify

--- 上課用專案/lesson16-1/lesson10-1/DetailViewController.swift
+++ 上課用專案/lesson16-1/lesson10-1/DetailViewController.swift
@@ -15,6 +15,8 @@ class DetailViewController: UITableViewController {
         title = city.city
         let headView = tableView.tableHeaderView as! UIImageView
         headView.image = UIImage(named: city.image ?? "")
+        tableView.backgroundColor = UIColor(red: 240.0/255.0, green: 240.0/255.0, blue: 240.0/255.0, alpha: 0.2);
+        tableView.separatorColor = UIColor(red: 240.0/255.0, green: 240.0/255.0, blue: 240.0/255.0, alpha: 0.6);
                 
     }
     
@@ -38,19 +40,21 @@ extension DetailViewController{
     override func tableView(_ tableView: UITableView,
                    cellForRowAt indexPath: IndexPath) -> UITableViewCell{
         let detailViewCell = tableView.dequeueReusableCell(withIdentifier: "DetailCell") as? DetailViewCell
+        
+        detailViewCell?.backgroundColor = UIColor.clear
             
         switch indexPath.row{
         case 0:
-            detailViewCell?.fieldLabel.text = "城市"
+            detailViewCell?.fieldLabel.text = "城市:"
             detailViewCell?.valueLabel.text = city.city
         case 1:
-            detailViewCell?.fieldLabel.text = "國家"
+            detailViewCell?.fieldLabel.text = "國家:"
             detailViewCell?.valueLabel.text = city.country
         case 2:
-            detailViewCell?.fieldLabel.text = "洲"
+            detailViewCell?.fieldLabel.text = "洲:"
             detailViewCell?.valueLabel.text = city.continent
         case 3:
-            detailViewCell?.fieldLabel.text = "網址"
+            detailViewCell?.fieldLabel.text = "網址:"
             detailViewCell?.valueLabel.text = city.url
         default:
             detailViewCell?.fieldLabel.text = ""
