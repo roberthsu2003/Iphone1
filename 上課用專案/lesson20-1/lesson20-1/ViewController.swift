bcab30ec4728b3a91c9f141fec0b633f7e428073
roberthsu2003 <roberthsu2003@gmail.com>
2021-02-23 22:01:14 +0800

    modify

--- 上課用專案/lesson20-1/lesson20-1/ViewController.swift
+++ 上課用專案/lesson20-1/lesson20-1/ViewController.swift
@@ -40,6 +40,7 @@ class ViewController: UICollectionViewController {
         super.viewDidLoad()
         let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
         layout.itemSize = CGSize(width: 180, height: 120)
+        
     }
 
 
@@ -75,8 +76,8 @@ extension ViewController{
             cityImageView.translatesAutoresizingMaskIntoConstraints = false
             cell.contentView.addSubview(cityImageView)
             NSLayoutConstraint.activate([
-                cityImageView.topAnchor.constraint(equalTo: cell.contentView.topAnchor, constant: 0),
-                cityImageView.bottomAnchor.constraint(equalTo: cell.contentView.bottomAnchor, constant: 0),
+                cityImageView.topAnchor.constraint(equalTo: cell.contentView.topAnchor, constant: 10),
+                cityImageView.bottomAnchor.constraint(equalTo: cell.contentView.bottomAnchor, constant: -10),
                 cityImageView.leadingAnchor.constraint(equalTo: cell.contentView.leadingAnchor, constant: 0),
                 cityImageView.trailingAnchor.constraint(equalTo: cell.contentView.trailingAnchor, constant: 0)
             ])
