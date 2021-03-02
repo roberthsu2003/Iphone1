12c7f62e1d9f59a6ed761f5d891b5738ff315ad2
roberthsu2003 <roberthsu2003@gmail.com>
2021-03-02 19:59:31 +0800

    modify

---
+++ 上課用專案/lesson22-1/lesson22-1/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  lesson22-1
+//
+//  Created by 徐國堂 on 2021/3/2.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //bundle路後
+        let sourcePath = Bundle.main.path(forResource: "citys", ofType: "db")
+        print(sourcePath!)
+        let targetPaths = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)
+        var targetPath = targetPaths.first ?? ""
+        targetPath += "/citys.db"
+    }
+
+
+}
+
