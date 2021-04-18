f96f6f7ca7dfe49397d78bcd152aadcfc72ec811
roberthsu2003 <roberthsu2003@gmail.com>
2021-04-18 15:18:25 +0800

    modify

--- 星期日上課用專案/lesson5_1/lesson5_1/ViewController.swift
+++ 星期日上課用專案/lesson5_1/lesson5_1/ViewController.swift
@@ -64,6 +64,7 @@ class ViewController: UIViewController,UITableViewDataSource {
             content.text = cityName
             content.secondaryText = country
             content.image = UIImage(named: imageName)
+            content.imageProperties.reservedLayoutSize = CGSize(width: 80, height: 50)
             cell.contentConfiguration = content
             
         }else{
