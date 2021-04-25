8bfaeb39c23fed05efd5c5836e2a8341f9802a82
roberthsu2003 <roberthsu2003@gmail.com>
2021-04-25 16:29:29 +0800

    modify

--- 星期日上課用專案/swift/initialization/initialization/ViewController.swift
+++ 星期日上課用專案/swift/initialization/initialization/ViewController.swift
@@ -7,11 +7,21 @@
 
 import UIKit
 struct Fahrenheit{
-    var temperature:Double
-    init(){
-        temperature = 32.0
+    //default property value
+    var temperature = 32.0
+    
+}
+
+struct Celsius{
+    var temperatureInCelsius:Double
+    
+    init(fromFahrenheit fahrenheit:Double){
+        temperatureInCelsius = (fahrenheit - 32.0) / 1.8
     }
     
+    init(fromKelvin kelvin:Double){
+        temperatureInCelsius = kelvin - 273.15
+    }
 }
 
 class ViewController: UIViewController {
@@ -20,6 +30,12 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         let f = Fahrenheit()
         print("華氏溫度是:\(f.temperature)")
+        
+        let boilingPointOfWater = Celsius(fromFahrenheit: 212)
+        print("攝氏溫度的沸點是\(boilingPointOfWater.temperatureInCelsius)")
+        
+        let freezingPointerOfWater = Celsius(fromKelvin: 273.15)
+        print("攝氏溫度的零度是:\(freezingPointerOfWater.temperatureInCelsius)")
     }
 
 
