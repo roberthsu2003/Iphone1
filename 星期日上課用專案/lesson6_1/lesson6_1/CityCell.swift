bc585f1dad447c4b3bde3a389149495c5bab8d69
roberthsu2003 <roberthsu2003@gmail.com>
2021-04-25 14:40:27 +0800

    modify

---
+++ 星期日上課用專案/lesson6_1/lesson6_1/CityCell.swift
@@ -0,0 +1,27 @@
+//
+//  CityCell.swift
+//  lesson6_1
+//
+//  Created by 徐國堂 on 2021/4/25.
+//
+
+import UIKit
+
+class CityCell: UITableViewCell {
+    @IBOutlet var cityLabel:UILabel!
+    @IBOutlet var countryLabel:UILabel!
+    @IBOutlet var continentLabel:UILabel!
+    @IBOutlet var cityImageView:UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
