8563e17c2b5949d624de037d84650bd7af512391
roberthsu2003 <roberthsu2003@gmail.com>
2021-04-25 12:22:52 +0800

    modify

--- 星期日上課用專案/lesson6_1/lesson6_1/ViewController.swift
+++ 星期日上課用專案/lesson6_1/lesson6_1/ViewController.swift
@@ -22,20 +22,29 @@ class ViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        tableView.dataSource = self
+        //tableView.dataSource = self
         
     }
     
     //MARK: - UITbaleViewDataSource
     override func tableView(_ tableView: UITableView,
                    numberOfRowsInSection section: Int) -> Int{
-        
+        //tableView會來執行這個method,尋問,tableView有多少個row
+        return cities.count
     }
     
     
     override func tableView(_ tableView: UITableView,
                    cellForRowAt indexPath: IndexPath) -> UITableViewCell{
-        
+        //當畫面要出現的row範圍時,tableView會要求傳出所需要的UITableViewCell
+        let rowIndex = indexPath.row
+        let city = cities[rowIndex]
+        let cityName = city["City"] as? String
+        let imageName = city["Image"] as! String
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CELL", for: indexPath)
+        cell.textLabel!.text = cityName
+        cell.imageView!.image = UIImage(named: imageName)
+        return cell
     }
 
 }
