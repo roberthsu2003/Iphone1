0d26cb4d6a57a7d181f0883c716d52695bf81c2c
roberthsu2003 <roberthsu2003@gmail.com>
2021-05-02 16:06:37 +0800

    modify

---
+++ 星期日上課用專案/swift/viewControllerInitial/viewControllerInitial/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  viewControllerInitial
+//
+//  Created by 徐國堂 on 2021/5/2.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var a:Int!
+    var b:Int!
+    var c:Float!
+    /*
+    required init?(coder: NSCoder) {
+        a = 5
+        b = 6
+        c = 7
+        super.init(coder: coder)
+        print("有執行 init?")
+       
+    }
+ */
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        a = 5
+        b = 6
+        c = 7
+    }
+
+
+}
+
