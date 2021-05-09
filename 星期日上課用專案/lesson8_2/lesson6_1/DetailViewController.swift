a3126543a4d8971a96dcfdf846a7dbc774777d70
roberthsu2003 <roberthsu2003@gmail.com>
2021-05-09 15:00:44 +0800

    modify

--- 星期日上課用專案/lesson8_2/lesson6_1/DetailViewController.swift
+++ 星期日上課用專案/lesson8_2/lesson6_1/DetailViewController.swift
@@ -18,14 +18,7 @@ class DetailViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func backToRoot(_ sender:UIBarButtonItem){
+        navigationController?.popToRootViewController(animated: true)
     }
-    */
-
 }
