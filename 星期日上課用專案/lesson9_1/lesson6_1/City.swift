871c996cf5bb32c38e36e10e40f5df3c49430c37
roberthsu2003 <roberthsu2003@gmail.com>
2021-05-16 10:17:54 +0800

    modify

---
+++ 星期日上課用專案/lesson9_1/lesson6_1/City.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+class City{
+    var city:String!
+    var continent:String!
+    var country:String!
+    var image:String!
+    var local:String!
+    var latitude:Double!
+    var longitude:Double!
+    var url:String!
+}
