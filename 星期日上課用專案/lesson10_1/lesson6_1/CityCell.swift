9bb5d72bd1c20a6704f235f48062544e6d32016f
roberthsu2003 <roberthsu2003@gmail.com>
2021-05-16 16:09:38 +0800

    modify

---
+++ 星期日上課用專案/lesson10_1/lesson6_1/CityCell.swift
@@ -0,0 +1,26 @@
+//
+//  CityCell.swift
+//  lesson6_1
+//
+//  Created by 徐國堂 on 2021/4/25.
+//
+
+import UIKit
+
+class CityCell: UITableViewCell {
+    @IBOutlet var cityLabel:UILabel!
+    @IBOutlet var countryLabel:UILabel!
+    @IBOutlet var continentLabel:UILabel!
+    @IBOutlet var cityImageView:UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        cityImageView.layer.cornerRadius = 10
+        let backgroundView = UIView();
+        backgroundView.backgroundColor = UIColor(white: 0.9, alpha: 0.5)
+        selectedBackgroundView = backgroundView
+    }
+
+    
+
+}
---
+++ 星期日上課用專案/lesson10_1/lesson6_1/DetailViewController.swift
@@ -0,0 +1,56 @@
+//
+//  DetailViewController.swift
+//  lesson9_1
+//
+//  Created by 徐國堂 on 2021/5/16.
+//
+
+import UIKit
+
+class DetailViewController: UITableViewController {
+    var city:City!
+    @IBOutlet var cityImageView:UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let imageName = city.image
+        cityImageView.image = UIImage(named: imageName!)
+        salse.list[city.city] = 1
+    }
+    // MARK: - UITableViewDataSource
+    override func tableView(_ tableView: UITableView,
+                   numberOfRowsInSection section: Int) -> Int{
+        return 5
+    }
+    
+    override func tableView(_ tableView: UITableView,
+                   cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CELL1", for: indexPath) as! DetailViewCell
+        
+        switch indexPath.row{
+            case 0:
+                cell.fieldLable.text = "城市"
+                cell.valueLable.text = city.city
+            case 1:
+                cell.fieldLable.text = "國家"
+                cell.valueLable.text = city.country
+            case 2:
+                cell.fieldLable.text = "洲"
+                cell.valueLable.text = city.continent
+            case 3:
+                cell.fieldLable.text = "網址"
+                cell.valueLable.text = city.url
+            case 4:
+                cell.fieldLable.text = "說明"
+                cell.valueLable.text = city.local
+            default:
+                cell.fieldLable.text = ""
+                cell.valueLable.text = ""
+        }
+        
+        return cell
+    }
+
+   
+
+}
