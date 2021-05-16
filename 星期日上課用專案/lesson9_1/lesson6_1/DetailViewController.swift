f27652a35543b4691e00a72cd7a08b6fce59f130
roberthsu2003 <roberthsu2003@gmail.com>
2021-05-16 15:16:00 +0800

    modify

--- 星期日上課用專案/lesson9_1/lesson6_1/DetailViewController.swift
+++ 星期日上課用專案/lesson9_1/lesson6_1/DetailViewController.swift
@@ -19,12 +19,32 @@ class DetailViewController: UITableViewController {
     // MARK: - UITableViewDataSource
     override func tableView(_ tableView: UITableView,
                    numberOfRowsInSection section: Int) -> Int{
-        
+        return 4
     }
     
     override func tableView(_ tableView: UITableView,
                    cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CELL1", for: indexPath) as! DetailViewCell
         
+        switch indexPath.row{
+            case 0:
+                cell.fieldLable.text = "城市"
+                cell.valueLable.text = city.city
+            case 1:
+                cell.fieldLable.text = "國家"
+                cell.valueLable.text = city.country
+            case 2:
+                cell.fieldLable.text = "洲"
+                cell.valueLable.text = city.continent
+            case 3:
+                cell.fieldLable.text = "網址"
+                cell.valueLable.text = city.url
+            default:
+                cell.fieldLable.text = ""
+                cell.valueLable.text = ""
+        }
+        
+        return cell
     }
 
    
