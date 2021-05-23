cd212809d212ca37bf8e5596d453f6c9a229b19c
roberthsu2003 <roberthsu2003@gmail.com>
2021-05-23 16:48:22 +0800

    modify

--- 星期日上課用專案/lesson11_2/lesson11_2/ViewController.swift
+++ 星期日上課用專案/lesson11_2/lesson11_2/ViewController.swift
@@ -11,6 +11,13 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        //取得mainBundle內的絕路徑
+        guard let sourceUrl = Bundle.main.url(forResource: "citylist", withExtension: "plist")else{
+            print("沒有bundle內的plist")
+            return
+        }
+        
+        
         //取得sandbox的絕對路徑
         let fileManager = FileManager.default
         
@@ -18,11 +25,19 @@ class ViewController: UIViewController {
             print("出錯了")
             return
         }
+        print(docsurl.path)
         let targetFileURL = docsurl.appendingPathComponent("city.plist")
         
          if !fileManager.fileExists(atPath: targetFileURL.path){
             print("沒有這個檔")
             //copy檔案進來
+            do{
+                try fileManager.copyItem(at: sourceUrl, to: targetFileURL)
+                print("copy檔案成功")
+            }catch{
+                print("copy錯誤")
+            }
+            
             
          }else{
             print("有這個檔")
