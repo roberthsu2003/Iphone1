fb5bf66fb3cde18f9fc48942825d79bcb6ed3a86
roberthsu2003 <roberthsu2003@gmail.com>
2021-06-06 16:37:32 +0800

    modify

---
+++ 星期日上課用專案/lesson12_1/lesson12_1/CityCollectionViewController.swift
@@ -0,0 +1,44 @@
+//
+//  CityCollectionViewController.swift
+//  lesson12_1
+//
+//  Created by 徐國堂 on 2021/6/6.
+//
+
+import UIKit
+import FMDB
+
+
+
+class CityCollectionViewController: UICollectionViewController {
+    var cities = [String]()
+    
+    func getAllCity() -> [String]?{
+        let citysPath = Bundle.main.url(forResource: "citys", withExtension: "db")
+        let db = FMDatabase(url: citysPath)
+        db.open()
+        guard let rs = try? db.executeQuery("SELECT cityName FROM city", values: nil) else{
+            print("執行錯誤")
+            return nil;
+        }
+        var citys = [String]()
+        while rs.next(){
+            if let countryName = rs["cityName"] as? String{
+                citys.append(countryName)
+            }
+          
+        }
+        db.close()
+        
+        return citys
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        cities = getAllCity()!
+        
+       
+    }
+
+   
+}
