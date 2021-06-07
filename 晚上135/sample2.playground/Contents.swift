69ab47787849223c826b9120f65de2a866d675ba
roberthsu2003 <roberthsu2003@gmail.com>
2021-06-07 21:07:19 +0800

    modify

--- 晚上135/sample2.playground/Contents.swift
+++ 晚上135/sample2.playground/Contents.swift
@@ -2,3 +2,10 @@ let implicitInteger = 70
 let implicitDouble = 70.0
 let explicitDouble:Double = 70
 
+let myInt = 14
+let myDouble = 14.0
+let myFloat:Float = 14
+let myString = "這是字串"
+let myBool = true
+
+
