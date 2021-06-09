2e88ec77aa7a55f59557a1de90e368b2489c7c2c
roberthsu2003 <roberthsu2003@gmail.com>
2021-06-09 19:46:26 +0800

    modify

--- 晚上135/lesson2_1.playground/Contents.swift
+++ 晚上135/lesson2_1.playground/Contents.swift
@@ -15,5 +15,24 @@ let second = 0.14159
 //變數值要手動轉型
 let otherPi = Double(first) + second
 
+//建立tuple
+//let http404Error:(Int,String) = (404,"Not Found")
+let http404Error = (404, "Not Found")
+
+//取出tuple的值,使用索引
+http404Error.0
+http404Error.1
+
+//取出值,使用拆解法
+let (statusCode, statusMessage) = http404Error
+statusCode
+statusMessage
+
+//建立tuple,使用標籤
+let http200Status = (statusCode:200,description:"Ok")
+http200Status.statusCode
+http200Status.description
+
+
 
 
