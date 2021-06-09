a036568b70619f5060357f55c0e83bf764ca24c3
roberthsu2003 <roberthsu2003@gmail.com>
2021-06-09 22:01:37 +0800

    modify

---
+++ 晚上135/lesson2_2/lesson2_2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lesson2_2
+//
+//  Created by 徐國堂 on 2021/6/9.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
