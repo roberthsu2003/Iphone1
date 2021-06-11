c1b6e50a1dd4ef33aaffc643429146bced20a1fd
roberthsu2003 <roberthsu2003@gmail.com>
2021-06-11 20:53:36 +0800

    modify

---
+++ 晚上135/lesson3_2/lesson3_2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lesson3_2
+//
+//  Created by 徐國堂 on 2021/6/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor(red: 0.5, green: 0.0, blue: 0, alpha: 1)
+    }
+
+
+}
+
