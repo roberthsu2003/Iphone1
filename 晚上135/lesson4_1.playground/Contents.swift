3d1ce7f9dbbd63de14b12a71ce8299a0d3b90930
roberthsu2003 <roberthsu2003@gmail.com>
2021-06-16 19:41:03 +0800

    modify

--- 晚上135/lesson4_1.playground/Contents.swift
+++ 晚上135/lesson4_1.playground/Contents.swift
@@ -8,3 +8,21 @@ print("someInts內的元素數量為:\(someInts.count)")
 someInts.append(3)
 someInts.append(6)
 someInts = []
+
+//建立有預設值的陣列
+var threeDouble = Array(repeating: 0.0, count: 10)
+threeDouble
+
+//陣列+陣列(組合陣列)
+
+var anotherThreeDouble = Array(repeating: 2.5, count: 10)
+
+var sixDouble = threeDouble + anotherThreeDouble
+sixDouble
+
+//可以使用純值的表示法，建立陣列
+//[value1, value2, value3]
+var shoppingList = ["Eggs", "Milk"]
+shoppingList
+
+
