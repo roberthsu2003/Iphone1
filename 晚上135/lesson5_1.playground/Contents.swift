77d1c455e3f3386f735493896ab45bb11d1fde48
roberthsu2003 <roberthsu2003@gmail.com>
2021-06-18 19:18:20 +0800

    modify

---
+++ 晚上135/lesson5_1.playground/Contents.swift
@@ -0,0 +1,15 @@
+let names = ["Anna", "Alex", "Brian", "Jack"]
+
+for name in names{
+    print("Hello, \(name)")
+}
+
+let numberOfLegs = ["spider":8, "ant":6, "cat":4]
+for (animalName, legCount) in numberOfLegs{
+        print("\(animalName)有\(legCount)腿")
+}
+
+for index in 1...5{
+    print(index)
+}
+
