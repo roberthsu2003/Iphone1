beb46625fddd0d60b9de981c213699217eebc1ae
roberthsu2003 <roberthsu2003@gmail.com>
2021-06-18 19:49:27 +0800

    modify

--- 晚上135/lesson5_1.playground/Contents.swift
+++ 晚上135/lesson5_1.playground/Contents.swift
@@ -20,9 +20,22 @@ for tickMark in stride(from: 0, to: 60, by: 5){
 for tickMark in stride(from: 0, through: 60, by: 5){
     print(tickMark)
 }
-
+/*
 var index = 0
 while (index < names.count){
     print(names[index])
     index += 1
 }
+ */
+
+var index:Int? = nil
+
+repeat{
+    if index == nil{
+        index = 0
+    }
+    print(names[index!])
+    index! += 1
+    
+}while(index! < names.count)
+
