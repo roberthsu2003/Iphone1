cf2c04ce602f1955160253342be89b45a9d64931
roberthsu2003 <roberthsu2003@gmail.com>
2021-06-21 19:24:18 +0800

    modify

--- 晚上135/lesson6_1.playground/Contents.swift
+++ 晚上135/lesson6_1.playground/Contents.swift
@@ -1,10 +1,28 @@
 //如果要定義字元，必需明確宣告
-let someCharacter:Character = "s"
+let someCharacter:Character = "A"
 switch someCharacter{
-case "a":
-    print("是a")
+case "a","A":
+    print("是a或A")
 case "z":
     print("是z")
 default:
     print("其它")
 }
+
+//Interval Matching 數值範圍
+let approximateCount = 150
+switch approximateCount {
+case 0:
+    print("0")
+case 1..<5:
+    print("1~4")
+case 5..<12:
+    print("5~11")
+case 12..<100:
+    print("12~99")
+case 100..<1000:
+    print("100~999")
+default:
+    print("其它")
+}
+
