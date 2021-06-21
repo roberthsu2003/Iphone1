bb741ae765846ea01641a9670fad4102d32084e9
roberthsu2003 <roberthsu2003@gmail.com>
2021-06-21 21:15:11 +0800

    modify

---
+++ 晚上135/lesson6_3/lesson6_3/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  lesson6_3
+//
+//  Created by 徐國堂 on 2021/6/21.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func leftButtonClick(_ sender:UIButton){
+        print("leftButtonClick")
+    }
+    
+    @IBAction func rightButtonClick(_ sender:UIButton){
+        print("rightButtonClick")
+    }
+
+
+}
+
