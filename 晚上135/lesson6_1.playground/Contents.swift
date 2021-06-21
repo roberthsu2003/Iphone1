9b206f5f47e2351f100fc9649981b82b2edd8fc2
roberthsu2003 <roberthsu2003@gmail.com>
2021-06-21 19:32:14 +0800

    modify

--- 晚上135/lesson6_1.playground/Contents.swift
+++ 晚上135/lesson6_1.playground/Contents.swift
@@ -26,3 +26,17 @@ default:
     print("其它")
 }
 
+//tuple比對
+let somePoint = (0, 1)
+switch somePoint{
+case (0,0):
+    print("中心點")
+case (_,0):
+    print("x軸")
+case (0,_):
+    print("y軸")
+case (-2...2,-2...2):
+    print("不在軸上")
+default:
+    print("超出範圍")
+}
