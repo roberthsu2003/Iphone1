f1908ffaa9e95280af83f79f6241d1b183d00c93
roberthsu2003 <roberthsu2003@gmail.com>
2021-06-23 21:08:32 +0800

    modify

--- 晚上135/lesson7_2/lesson7_2/ViewController.swift
+++ 晚上135/lesson7_2/lesson7_2/ViewController.swift
@@ -17,11 +17,20 @@ class ViewController: UIViewController {
     
     @IBAction func userClick(_ sender:UIButton){
         let num = inputNum.text!
+        /*
         let convertedNum = Int(num)
         if convertedNum == nil{
             print("轉換失敗")
         }else{
             print("數值是\(convertedNum!)")
+        }
+        */
+        
+        //optional binding
+        if let convertedNum = Int(num){
+            print("數值是\(convertedNum)")
+        }else{
+            print("轉換失敗")
         }
         
         
