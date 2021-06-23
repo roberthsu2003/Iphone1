0e3834b4b8ac66aa08fcdc1f19b43708bbf01c2e
roberthsu2003 <roberthsu2003@gmail.com>
2021-06-23 19:41:16 +0800

    modify

--- 晚上135/lesson7_1.playground/Contents.swift
+++ 晚上135/lesson7_1.playground/Contents.swift
@@ -13,3 +13,15 @@ var mathFunction:(Int,Int)->Int = addTwoInts
 mathFunction(20, 30)
 mathFunction = multiplyTwoInts
 mathFunction(20, 30)
+
+
+let names = ["Chris", "Alex", "Ewa", "Barry", "Daniella"]
+names.sorted()
+names
+
+func backward(_ s1:String,_ s2:String) -> Bool{
+    return s1 > s2
+}
+//function當作引數的值
+names.sorted(by: backward)
+
