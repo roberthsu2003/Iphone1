e254b27e256787625cfb1188e2b20f4913bafe62
roberthsu2003 <roberthsu2003@gmail.com>
2021-06-23 20:28:23 +0800

    modify

---
+++ 晚上135/lesson7_2/lesson7_2/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  lesson7_2
+//
+//  Created by 徐國堂 on 2021/6/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var inputNum:UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func userClick(_ sender:UIButton){
+        
+    }
+
+
+}
+
