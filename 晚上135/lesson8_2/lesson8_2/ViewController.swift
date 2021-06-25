9b7e622cb5d1ea4018684ab227ef0ae67aa62029
roberthsu2003 <roberthsu2003@gmail.com>
2021-06-25 19:52:17 +0800

    modify

---
+++ 晚上135/lesson8_2/lesson8_2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lesson8_2
+//
+//  Created by 徐國堂 on 2021/6/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var mySwitch:UISwitch!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
