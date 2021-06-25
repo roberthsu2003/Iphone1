ed92c3c13013669101a04d639e5bfb966d645f07
roberthsu2003 <roberthsu2003@gmail.com>
2021-06-25 20:59:48 +0800

    modify

---
+++ 晚上135/lesson8_3/lesson8_3/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  lesson8_3
+//
+//  Created by 徐國堂 on 2021/6/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var navigationBar:UINavigationBar!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        /*
+        if let item = navigationBar.topItem{
+            print(item)
+        }else{
+            print("沒有topItem")
+        }
+ */
+        navigationBar.topItem!.title = "BMI Calulator"
+        navigationBar.topItem!.prompt = "計算BMI的小程式"
+    }
+
+
+}
+
