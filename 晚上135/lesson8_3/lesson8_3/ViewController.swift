113e5720ee0dcbce0f86e7de7172f0a02227a05c
roberthsu2003 <roberthsu2003@gmail.com>
2021-06-28 21:44:23 +0800

    modify

--- 晚上135/lesson8_3/lesson8_3/ViewController.swift
+++ 晚上135/lesson8_3/lesson8_3/ViewController.swift
@@ -20,18 +20,29 @@ class ViewController: UIViewController {
     }
     
     @IBAction func caculate(_ sender:UIButton){
-        //optional binding
+        
+        /*
+         //optional binding
         if let height = Double(heightField.text!),let weight = Double(weightField.text!){
             
             let bmi = weight/((height/100) * (height/100))
-            print(bmi)
-            messageView.text = "BMI是\(bmi)"
+            let bmiString = String(format: "%.2f", bmi)
+            messageView.text = "BMI是\(bmiString)"
         }else{
             print("有錯誤")
             return
         }
+ */
+        //提早離開function
+        guard let height = Double(heightField.text!),let weight = Double(weightField.text!) else{
+            //false
+            print("有錯誤")
+            return
+        }
         
-        
+        let bmi = weight/((height/100) * (height/100))
+        let bmiString = String(format: "%.2f", bmi)
+        messageView.text = "BMI是\(bmiString)"
         
        
         
