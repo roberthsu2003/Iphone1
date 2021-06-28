e1fbf26c6ad196a25231798984270bee0c39fe01
roberthsu2003 <roberthsu2003@gmail.com>
2021-06-28 19:39:45 +0800

    modify

--- 晚上135/lesson9_1.playground/Contents.swift
+++ 晚上135/lesson9_1.playground/Contents.swift
@@ -17,3 +17,11 @@ someResolution.height
 someVideoMode.resolution.width
 someVideoMode.resolution.width = 1280
 someVideoMode.resolution.width
+
+let vga = Resolution(width: 640, height: 480)
+let hd = Resolution(width: 1920, height: 1080)
+var cinema = hd
+cinema.width = 2048
+cinema.width
+hd.width
+
