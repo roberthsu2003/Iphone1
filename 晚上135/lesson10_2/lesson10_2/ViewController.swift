e19820acd3c02a1cd435104bc695497870893f41
roberthsu2003 <roberthsu2003@gmail.com>
2021-06-30 21:56:51 +0800

    modify

--- 晚上135/lesson10_2/lesson10_2/ViewController.swift
+++ 晚上135/lesson10_2/lesson10_2/ViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet var myImageView:UIImageView!
+    //var names:[String]?
+    var names = ["robert", "jenny", "alice"]
+    var countryCode = ["tw":"Taiwan", "jp":"Japan", "us":"US", "hk":"HongKong"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,10 +22,22 @@ class ViewController: UIViewController {
         switch sender.tag{
         case 100:
             myImageView.image = UIImage(named: "pic1")
+            print(names[0])
+            if let country = countryCode["tw"]{
+                print(country)
+            }
         case 200:
             myImageView.image = UIImage(named: "pic2")
+            print(names[1])
+            if let country = countryCode["jp"]{
+                print(country)
+            }
         case 300:
             myImageView.image = UIImage(named: "pic3")
+            print(names[2])
+            if let country = countryCode["hk"]{
+                print(country)
+            }
         default:
             print("other")
         }
