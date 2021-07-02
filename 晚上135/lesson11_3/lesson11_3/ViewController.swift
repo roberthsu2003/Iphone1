03ce29e366003296a75fe20d4b96d3ea132352e0
roberthsu2003 <roberthsu2003@gmail.com>
2021-07-02 21:03:15 +0800

    modify

---
+++ 晚上135/lesson11_3/lesson11_3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  lesson11_3
+//
+//  Created by 徐國堂 on 2021/7/2.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        let namesURL = Bundle.main.url(forResource: "names", withExtension: "plist")
+        print(namesURL!.path)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+       
+    }
+
+
+}
+
