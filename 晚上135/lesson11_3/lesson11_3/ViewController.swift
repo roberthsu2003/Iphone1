db812f2aeeb93af5cb13a4cffe641ecaad498153
roberthsu2003 <roberthsu2003@gmail.com>
2021-07-02 21:30:54 +0800

    modify

--- 晚上135/lesson11_3/lesson11_3/ViewController.swift
+++ 晚上135/lesson11_3/lesson11_3/ViewController.swift
@@ -8,17 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var names:[String]!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         if let namesURL = Bundle.main.url(forResource: "names", withExtension: "plist"){
-            print(namesURL.path)
+            if let names = NSArray(contentsOf: namesURL) as? [String]{
+                self.names = names
+            }
         }
         
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print(names!)
        
     }
 
