9c4ee622a30bd3d57ae06e760ce4036b35fb9369
roberthsu2003 <roberthsu2003@gmail.com>
2021-07-02 19:39:46 +0800

    modify

--- 晚上135/lesson11_2/lesson11_2/ViewController.swift
+++ 晚上135/lesson11_2/lesson11_2/ViewController.swift
@@ -11,7 +11,19 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        //可以存取所有storyboard建立的元件
+        //只被執行一次
         print("ViewController內的View全部可以使用了")
+    }
+    
+    override func viewWillAppear(_ animated: Bool){
+        super.viewWillAppear(animated)
+        print("View將要顯示了")
+    }
+    
+    override func viewDidAppear(_ animated: Bool){
+        super.viewDidAppear(animated)
+        print("view已經顯示了")
     }
 
 
