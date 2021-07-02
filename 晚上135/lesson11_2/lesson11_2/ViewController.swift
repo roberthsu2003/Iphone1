25ff48dabe0ea2dbdbe8f9a7bc24b104b2664df6
roberthsu2003 <roberthsu2003@gmail.com>
2021-07-02 19:26:23 +0800

    modify

---
+++ 晚上135/lesson11_2/lesson11_2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lesson11_2
+//
+//  Created by 徐國堂 on 2021/7/2.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("ViewController內的View全部可以使用了")
+    }
+
+
+}
+
