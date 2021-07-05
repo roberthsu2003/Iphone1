0d30bda11e53957e14e2aa75fb9787e0f0f5f1e3
roberthsu2003 <roberthsu2003@gmail.com>
2021-07-05 19:57:54 +0800

    modify

---
+++ 晚上135/lesson12_2/lesson11_3/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  lesson11_3
+//
+//  Created by 徐國堂 on 2021/7/2.
+//
+
+import UIKit
+
+class ViewController:UIViewController {
+    var names:[String]!
+    var countryCode:[String:String]!
+    var cities:[[String:Any]]!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        if let namesURL = Bundle.main.url(forResource: "names", withExtension: "plist"){
+            if let names = NSArray(contentsOf: namesURL) as? [String]{
+                self.names = names
+            }
+        }
+        
+        if let codeURL = Bundle.main.url(forResource: "countryCode", withExtension: "plist"){
+            if let countryCode = NSDictionary(contentsOf: codeURL) as? [String:String]{
+                self.countryCode = countryCode
+            }
+        }
+        
+        if let cityURL = Bundle.main.url(forResource: "citylist", withExtension: "plist"){
+            
+            if let cities = NSArray(contentsOf: cityURL) as? [[String:AnyObject]]{
+                self.cities = cities
+            }
+            
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        
+        for item in cities{
+            print(item)
+            print("================")
+        }
+    }
+
+
+}
+
