ae969ade07fbf81007ec3f41453a8f37391b6cd1
roberthsu2003 <roberthsu2003@gmail.com>
2021-07-07 19:22:33 +0800

    modify

---
+++ 晚上135/lesson13_1.playground/Contents.swift
@@ -0,0 +1,13 @@
+struct Fahrenheit{
+    var temperature = 32.0
+    /*
+    init(){
+        temperature = 32.0
+    }
+ */
+}
+
+var f = Fahrenheit(temperature: 40)
+print("華氏溫度是\(f.temperature)")
+
+
