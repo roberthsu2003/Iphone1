4c9567bfe1d0f9c01682f3938b1a950d281626db
roberthsu2003 <roberthsu2003@gmail.com>
2021-07-07 19:58:33 +0800

    modify

--- 晚上135/lesson13_1.playground/Contents.swift
+++ 晚上135/lesson13_1.playground/Contents.swift
@@ -70,3 +70,17 @@ class SurveyQuestion{
 let cheeseQuestion = SurveyQuestion(text: "您喜歡cheese嗎?")
 cheeseQuestion.response = "是的，我喜歡cheese"
 
+class ShoppingListItem{
+    var name:String?
+    var quantity = 1
+    var purchased = false
+}
+var item = ShoppingListItem()
+
+struct Size{
+    var width = 0.0, height = 0.0
+}
+let twoByTwo = Size(width: 2.0, height: 2.0)
+
+
+
