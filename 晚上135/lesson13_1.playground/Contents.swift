27d130ecdeabfa0a4ca617de8ed0703b33dfb127
roberthsu2003 <roberthsu2003@gmail.com>
2021-07-07 20:17:57 +0800

    modify

--- 晚上135/lesson13_1.playground/Contents.swift
+++ 晚上135/lesson13_1.playground/Contents.swift
@@ -77,10 +77,43 @@ class ShoppingListItem{
 }
 var item = ShoppingListItem()
 
+struct Size1{
+    var width = 0.0, height = 0.0
+}
+let twoByTwo = Size1(width: 2.0, height: 2.0)
+
 struct Size{
     var width = 0.0, height = 0.0
 }
-let twoByTwo = Size(width: 2.0, height: 2.0)
+
+struct Point{
+    var x = 0.0, y = 0.0
+}
+
+struct Rect{
+    var origin = Point()
+    var size = Size()
+    init(){
+        
+    }
+    init(origin:Point, size:Size){
+        self.origin = origin
+        self.size = size
+    }
+    
+    init(center:Point, size:Size){
+        let originX = center.x - (size.width / 2)
+        let originY = center.y - (size.height / 2)
+        self.init(origin: Point(x: originX, y: originY), size: size)
+    }
+}
+
+let originRect = Rect(origin: Point(x: 2.0, y: 2.0), size: Size(width: 5.0, height: 5.0))
+
+let basicRect = Rect()
+
+let centerRect = Rect(center: Point.init(x: 4.0, y: 4.0), size: Size.init(width: 3.0, height: 3.0))
+
 
 
 
