866e6e296b152d4aa84c35e8f7bbd6097dccdca6
roberthsu2003 <roberthsu2003@gmail.com>
2021-07-07 19:43:03 +0800

    modify

--- 晚上135/lesson13_1.playground/Contents.swift
+++ 晚上135/lesson13_1.playground/Contents.swift
@@ -28,3 +28,23 @@ let freezingPointOfWater = Celsius(fromKelvin: 273.15)
 boilingPointOfWater.temperatureInCelsius
 freezingPointOfWater.temperatureInCelsius
 
+struct Color{
+    let red, green, blue:Double
+    
+    init(red:Double, green:Double, blue:Double){
+        self.red = red
+        self.green = green
+        self.blue = blue
+    }
+    
+    init(white:Double){
+        red = white
+        green = white
+        blue = white
+    }
+}
+
+let magenta = Color(red: 1.0, green: 0.0, blue: 1.0)
+
+let halfGray = Color(white: 0.5)
+
