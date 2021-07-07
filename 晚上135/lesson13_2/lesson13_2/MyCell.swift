8f19124c96c909989d4367ba09984893fea44316
roberthsu2003 <roberthsu2003@gmail.com>
2021-07-07 21:33:09 +0800

    modify

---
+++ 晚上135/lesson13_2/lesson13_2/MyCell.swift
@@ -0,0 +1,23 @@
+//
+//  MyCell.swift
+//  lesson13_2
+//
+//  Created by 徐國堂 on 2021/7/7.
+//
+
+import UIKit
+
+class MyCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
