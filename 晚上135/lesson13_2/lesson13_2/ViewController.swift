69d12a86ca4b922d98570b854e3fdd96712f84a2
roberthsu2003 <roberthsu2003@gmail.com>
2021-07-07 20:54:40 +0800

    modify

--- 晚上135/lesson13_2/lesson13_2/ViewController.swift
+++ 晚上135/lesson13_2/lesson13_2/ViewController.swift
@@ -8,6 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var cities = [[String:Any]]()
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        Bundle.main.url(forResource: "citylist", withExtension: "plist")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
