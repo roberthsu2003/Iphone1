341ed35a5da7d31cacfe272087668de822e112d7
roberthsu2003 <roberthsu2003@gmail.com>
2021-07-09 21:01:10 +0800

    modify

--- 晚上135/lesson13_2/lesson13_2/MyCell.swift
+++ 晚上135/lesson13_2/lesson13_2/MyCell.swift
@@ -16,13 +16,14 @@ class MyCell: UITableViewCell {
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
+        cityImageView.layer.cornerRadius = 40
     }
 
 }
