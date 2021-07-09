cdacf3df9dd8f3dcb34a373d25d785e277175e5f
roberthsu2003 <roberthsu2003@gmail.com>
2021-07-09 19:38:48 +0800

    modify

--- 晚上135/lesson14_1.playground/Contents.swift
+++ 晚上135/lesson14_1.playground/Contents.swift
@@ -9,4 +9,29 @@ struct SomeStructure:SomeProtocol{
     var doesNotNeedToBeSettable: Int{
         return 50
     }
+    
+}
+
+protocol FullyNamed{
+    var fullName:String {get}
+}
+
+struct Person:FullyNamed{
+    var fullName: String
+}
+
+let john = Person(fullName: "John Appleseed")
+
+class Starship:FullyNamed{
+    var prefix:String?
+    var name:String
+    init(name:String, prefix:String? = nil){
+        self.name = name
+        self.prefix = prefix
+    }
+    
+    var fullName:String{
+        return (prefix != nil ? prefix! + " " : "") + name
+    }
+    
 }
