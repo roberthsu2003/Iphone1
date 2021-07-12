cb209c83de61b92a7ca346b133a17ee93e8ebf60
roberthsu2003 <roberthsu2003@gmail.com>
2021-07-12 20:22:32 +0800

    modify

--- 晚上135/lesson15_1.playground/Contents.swift
+++ 晚上135/lesson15_1.playground/Contents.swift
@@ -49,6 +49,27 @@ class Food{
     
 }
 
+let nameMeat = Food(name: "Bacon")
+let mysteryMeat = Food()
+
+class RecipeIngredient:Food{
+    var quantity:Int
+    init(name:String,quantity:Int){
+        self.quantity = quantity
+        super.init(name: name)
+    }
+    
+    override convenience init(name:String){
+        self.init(name:name,quantity:1)
+    }
+    
+    
+}
+
+let oneMysteryItem = RecipeIngredient()
+let oneBacon = RecipeIngredient(name: "Bacon")
+let sixEggs = RecipeIngredient(name: "Eggs", quantity: 6)
+
 
 
 
