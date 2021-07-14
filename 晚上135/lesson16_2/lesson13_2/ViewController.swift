d848104a167696b7cd387dd56d57d559b6299760
roberthsu2003 <roberthsu2003@gmail.com>
2021-07-14 21:17:05 +0800

    modify

---
+++ 晚上135/lesson16_2/lesson13_2/ViewController.swift
@@ -0,0 +1,143 @@
+//
+//  ViewController.swift
+//  lesson13_2
+//
+//  Created by 徐國堂 on 2021/7/7.
+//
+
+import UIKit
+
+class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
+    @IBOutlet var tableView:UITableView!
+    var cities:[City]={
+        if let plistURL = Bundle.main.url(forResource: "citylist", withExtension: "plist"){
+            if let array = NSArray(contentsOf: plistURL) as? [[String:Any]]{
+                var citys = [City]()
+                for cityDic in array{
+                    let city = City()
+                    city.city = cityDic["City"] as? String
+                    city.country = cityDic["Country"] as? String
+                    city.continent = cityDic["Continent"] as? String
+                    city.image = cityDic["Image"] as? String
+                    city.local = cityDic["Local"] as? String
+                    city.url = cityDic["url"] as? String
+                    city.latitude = cityDic["lat"] as? Double
+                    city.longitude = cityDic["long"] as? Double
+                    citys.append(city)
+                }
+                return citys
+            }
+        }
+        
+        return [City]()
+        
+    }()
+    
+    
+    
+   
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+    
+    //MARK: - UITableViewDataSource
+    
+    func tableView(_ tableView: UITableView,
+                   numberOfRowsInSection section: Int) -> Int{
+        return cities.count
+    }
+    
+    func tableView(_ tableView: UITableView,
+                   cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        let indexRow = indexPath.row
+        let city = cities[indexRow]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CELL", for: indexPath) as! MyCell
+        /*
+        cell.backgroundColor = UIColor.systemPink
+        let backgroundView = UIView()
+        backgroundView.backgroundColor = UIColor.systemTeal
+        cell.selectedBackgroundView = backgroundView
+         */
+        cell.cityName.text = city.city
+        cell.countryName.text = city.country
+        cell.continent.text = city.continent
+        if let imageName = city.image{
+            cell.cityImageView.image = UIImage(named:imageName)
+        }
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView,
+                     commit editingStyle: UITableViewCell.EditingStyle,
+                     forRowAt indexPath: IndexPath){
+        if editingStyle == .delete{
+            let deleteRow = indexPath.row
+            cities.remove(at: deleteRow)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
+       
+        
+        
+    }
+    
+    //MARK: - UITableViewDelegate
+    func tableView(_ tableView: UITableView,
+                   didSelectRowAt indexPath: IndexPath){
+        let selectedIndex = indexPath.row
+        let city = cities[selectedIndex]
+        if let cityName = city.city{
+            print("現在選到的城市是\(cityName)")
+            let optionMenu = UIAlertController(title: nil, message: "您選擇\(cityName)想要做什麼?", preferredStyle: .actionSheet)
+            
+            let cancelAction = UIAlertAction(title: "取消", style: .destructive, handler: nil)
+            optionMenu.addAction(cancelAction)
+            
+            let callAction = UIAlertAction(title: "聯絡旅行社", style: .default){
+                (action:UIAlertAction) -> Void in
+                let alertMessage = UIAlertController(title: "連線失敗", message: "目前正在忙線中", preferredStyle: .alert)
+                alertMessage.addAction(UIAlertAction.init(title: "OK", style: .default, handler: nil))
+                self.present(alertMessage, animated: true, completion: nil)
+            }
+            optionMenu.addAction(callAction)
+            
+            present(optionMenu, animated: true, completion: {
+                print("動畫完成執行的closure內的功能")
+            })
+            
+        }
+    }
+    
+    func tableView(_ tableView: UITableView,
+                   trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration?{
+        let deletAction = UIContextualAction(style: .normal, title: "刪除") { (action, sourceView, completionHandler) in
+            let deleteRow = indexPath.row
+            self.cities.remove(at: deleteRow)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+            completionHandler(true)
+        }
+        
+        
+        
+        deletAction.backgroundColor = UIColor.systemRed
+        
+        let insertAction = UIContextualAction(style: .normal, title: "新增") { (action:UIContextualAction, sourceView:UIView, completionHandler: (Bool) -> Void) in
+            print("insertAction")
+            completionHandler(true)
+        }
+        
+        insertAction.backgroundColor = UIColor.systemGreen
+        
+        let config = UISwipeActionsConfiguration(actions: [deletAction,insertAction])
+        return config
+        
+    }
+    
+    
+
+
+}
+
