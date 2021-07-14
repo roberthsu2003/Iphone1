c6fb95f3292ee35f8ae14a38787f6b1a21e07eaf
roberthsu2003 <roberthsu2003@gmail.com>
2021-07-14 20:29:02 +0800

    modify

--- 晚上135/lesson15_2/lesson13_2/ViewController.swift
+++ 晚上135/lesson15_2/lesson13_2/ViewController.swift
@@ -9,13 +9,28 @@ import UIKit
 
 class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
     @IBOutlet var tableView:UITableView!
-    var cities:[[String:Any]]={
+    var cities:[City]={
         if let plistURL = Bundle.main.url(forResource: "citylist", withExtension: "plist"){
             if let array = NSArray(contentsOf: plistURL) as? [[String:Any]]{
-                return array
+                var citys = [City]()
+                for cityDic in array{
+                    let city = City()
+                    city.city = cityDic["City"] as? String
+                    city.country = cityDic["Country"] as? String
+                    city.continent = cityDic["Continent"] as? String
+                    city.image = cityDic["Image"] as? String
+                    city.local = cityDic["Local"] as? String
+                    city.url = cityDic["url"] as? String
+                    city.latitude = cityDic["lat"] as? Double
+                    city.longitude = cityDic["long"] as? Double
+                    citys.append(city)
+                }
+                return citys
             }
         }
-        return [[String:Any]]()
+        
+        return [City]()
+        
     }()
     
     
