86d77e1fdd94822c290fa893697b5c256b71d12c
roberthsu2003 <roberthsu2003@gmail.com>
2021-07-16 19:52:31 +0800

    modify

--- 晚上135/lesson17_1.playground/Contents.swift
+++ 晚上135/lesson17_1.playground/Contents.swift
@@ -16,3 +16,29 @@ func canThrowError(num:Int) throws -> String{
     return "有傳出字串"
 }
 
+//執行有丟出錯誤的方法有4種
+//do..try..catch
+//try?
+//try!
+//throw
+
+if let stringValue = try? canThrowError(num: 10){
+    print(stringValue)
+}else{
+    print("method 出錯")
+}
+
+let stringValue1 = try! canThrowError(num: 5)
+print(stringValue1)
+
+do{
+    let stringValue = try canThrowError(num: 10)
+    print(stringValue)
+}catch VendingMachineError.insufficientFunds(let coinsNeeds){
+    print("出錯了,少了\(coinsNeeds)")
+}catch VendingMachineError.invalidSelection{
+    print("不合法的選擇")
+}catch{
+    print("有其它錯誤")
+}
+
