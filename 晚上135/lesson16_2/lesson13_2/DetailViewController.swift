a7f2a3314b247249ba0fd57ba8a169650ffffe84
roberthsu2003 <roberthsu2003@gmail.com>
2021-07-16 21:56:44 +0800

    modify

--- 晚上135/lesson16_2/lesson13_2/DetailViewController.swift
+++ 晚上135/lesson16_2/lesson13_2/DetailViewController.swift
@@ -12,8 +12,20 @@ class DetailViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(city.city)
        
+       
+    }
+    
+    //MARK: - UITableViewDataSource
+    
+    override func tableView(_ tableView: UITableView,
+                   numberOfRowsInSection section: Int) -> Int{
+        return 5
+    }
+    
+    override func tableView(_ tableView: UITableView,
+                   cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        return UITableViewCell()
     }
 
 }
