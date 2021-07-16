eba39b963e81448242064d80e50837182d7f3a7e
roberthsu2003 <roberthsu2003@gmail.com>
2021-07-16 20:44:09 +0800

    modify

--- 晚上135/lesson17_1.playground/Contents.swift
+++ 晚上135/lesson17_1.playground/Contents.swift
@@ -42,3 +42,35 @@ do{
     print("有其它錯誤")
 }
 
+class MediaItem{
+    var name:String
+    init(name:String){
+        self.name = name
+    }
+}
+
+class Movie:MediaItem{
+    var director:String
+    init(name:String, director:String){
+        self.director = director
+        super.init(name: name)
+    }
+}
+
+
+class Song:MediaItem{
+    var artist:String
+    init(name:String, artist:String){
+        self.artist = artist
+        super.init(name: name)
+    }
+}
+
+let library = [
+    Movie(name: "Casablanca", director: "Michael Curtiz"),
+    Song(name: "Blue Suede Shoes", artist: "Elvis Presley"),
+    Movie(name: "Citizen Kane", director: "Orson Welles"),
+    Song(name: "The One And Only", artist: "Chesney Hawkes"),
+    Song(name: "Never Gonna Give You Up", artist: "Rick Astley")
+]
+
