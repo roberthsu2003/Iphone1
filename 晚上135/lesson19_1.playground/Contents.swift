6c2a6e0269fdbbc94c7dca009a53f0c760bf4750
roberthsu2003 <roberthsu2003@gmail.com>
2021-07-21 19:25:58 +0800

    modify

--- 晚上135/lesson19_1.playground/Contents.swift
+++ 晚上135/lesson19_1.playground/Contents.swift
@@ -12,3 +12,33 @@ var anotherInt = 107
 swapTwoInts(&someInt, &anotherInt)
 someInt
 anotherInt
+
+func swapTwoInts(_ a:inout String,_ b:inout String){
+    let temporaryA = a
+    a = b
+    b = temporaryA
+}
+
+func swapTwoInts(_ a:inout Double,_ b:inout Double){
+    let temporaryA = a
+    a = b
+    b = temporaryA
+}
+
+
+//Generic Function
+func swapTwoInts<T>(_ a:inout T,_ b:inout T){
+    let temporaryA = a
+    a = b
+    b = temporaryA
+}
+
+var someInt1 = 3
+var someAnotherInt = 107
+
+swapTwoInts(&someInt, &someAnotherInt)
+
+var someString = "hello"
+var anotherString = "world"
+swapTwoInts(&someString, &anotherString)
+
