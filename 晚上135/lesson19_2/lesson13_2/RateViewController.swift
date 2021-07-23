0771bbf1a987cc566cd5371aff1824abff64421c
roberthsu2003 <roberthsu2003@gmail.com>
2021-07-23 19:35:00 +0800

    modify

--- 晚上135/lesson19_2/lesson13_2/RateViewController.swift
+++ 晚上135/lesson19_2/lesson13_2/RateViewController.swift
@@ -21,14 +21,8 @@ class RateViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func cancer(_ sender:UIButton){
+        dismiss(animated: true)
     }
-    */
 
 }
