7866e3e9355ef60bb3e69241c3f5c2f915f9abc7
roberthsu2003 <roberthsu2003@gmail.com>
2021-07-23 21:25:08 +0800

    modify

---
+++ 晚上135/lesson20/lesson13_2/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  lesson13_2
+//
+//  Created by 徐國堂 on 2021/7/7.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+   
+    var cities:[City]={
+        if let plistURL = Bundle.main.url(forResource: "citylist", withExtension: "plist"){
+            if let array = NSArray(contentsOf: plistURL) as? [[String:Any]]{
+                var citys = [City]()
+                for cityDic in array{
+                    let city = City()
+                    city.city = cityDic["City"] as? String
+                    city.country = cityDic["Country"] as? String
+                    city.continent = cityDic["Continent"] as? String
+                    city.image = cityDic["Image"] as? String
+                    city.local = cityDic["Local"] as? String
+                    city.url = cityDic["url"] as? String
+                    city.latitude = cityDic["lat"] as? Double
+                    city.longitude = cityDic["long"] as? Double
+                    citys.append(city)
+                }
+                return citys
+            }
+        }
+        
+        return [City]()
+        
+    }()
+
+}
+
+
+
+
