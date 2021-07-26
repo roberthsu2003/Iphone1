004b8a495cff330ad26f8a965c566142466ee771
roberthsu2003 <roberthsu2003@gmail.com>
2021-07-26 20:59:23 +0800

    modify

---
+++ 晚上135/lesson21/lesson13_2/RegisteViewController.swift
@@ -0,0 +1,24 @@
+//
+//  RegisteViewController.swift
+//  lesson21
+//
+//  Created by 徐國堂 on 2021/7/26.
+//
+
+import UIKit
+
+class RegisteViewController: UITableViewController {
+    @IBOutlet var nameTextField:UITextField!
+    @IBOutlet var ageTextField:UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func userRegister(_ sender:UIBarButtonItem){
+        
+    }
+
+  
+}
