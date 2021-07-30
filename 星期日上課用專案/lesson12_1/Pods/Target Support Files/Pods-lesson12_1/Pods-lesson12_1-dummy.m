7e53026766bf18a6c6580e6c475afc4d101c44e1
roberthsu2003 <roberthsu2003@gmail.com>
2021-07-30 18:30:02 +0800

    modify

--- 星期日上課用專案/lesson12_1/Pods/Target Support Files/Pods-lesson12_1/Pods-lesson12_1-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_lesson12_1 : NSObject
-@end
-@implementation PodsDummy_Pods_lesson12_1
-@end
--- 星期日上課用專案/lesson3_2/lesson3_2/TypeProperty.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-struct SomeStructure{
-    //type property
-    static var storedTypeProperty = "Some value"
-    static var computedTypeProperty:Int {
-        return 1
-    }
-}
