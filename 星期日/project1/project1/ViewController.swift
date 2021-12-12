668c5fbec975bf0a89d595c56ea503a58aca39b3
徐國堂 <roberthsu2003@gmail.com>
2021-12-12 10:46:48 +0800

    modify

---
+++ 星期日/project1/project1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  project1
+//
+//  Created by alicehsu on 2021/12/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("這是我的第一個專案")
+    }
+
+
+}
+
