10bee85a49abb52f5fc4ff0969d93745f6122ecb
徐國堂 <roberthsu2003@gmail.com>
2021-12-12 08:32:45 +0800

    modify

--- 晚上135/lesson23_FMDB/Pods/Target Support Files/Pods-lesson23_FMDB/Pods-lesson23_FMDB-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_lesson23_FMDB : NSObject
-@end
-@implementation PodsDummy_Pods_lesson23_FMDB
-@end
--- 晚上135/sample1.playground/Contents.swift
+++
@@ -1,8 +0,0 @@
-var myVariable = 42
-print(myVariable)
-myVariable = 50
-print(myVariable)
-let myConstant = 42
-print(myConstant)
-//myConstant = 50
-
