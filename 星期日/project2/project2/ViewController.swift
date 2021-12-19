245eb31e07827e6da74f936a47fd1d7336a78d3e
roberthsu2003 <roberthsu2003@gmail.com>
2021-12-19 16:04:18 +0800

    modify

--- 星期日/project2/project2/ViewController.swift
+++ 星期日/project2/project2/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet var myLabel:UILabel?
+    @IBOutlet var myLabel:UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(myLabel!.text!)
-        print(myLabel!.text ?? "text內容是nil")
+        print(myLabel.text!) //強制打開，如果是nil就crash
+        
+        print(myLabel.text ?? "text內容是nil") //使用nil運算子檢查
+        
+        if let textString = myLabel.text{ //使用optional binding 檢查
+            print(textString)
+        }else{
+            print("text內容是nil")
+        }
     }
 
 
