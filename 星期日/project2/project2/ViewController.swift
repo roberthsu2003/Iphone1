4eb5e149ef3553cc820bf606bf720f183720a1cb
roberthsu2003 <roberthsu2003@gmail.com>
2021-12-19 12:17:31 +0800

    modify

---
+++ 星期日/project2/project2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  project2
+//
+//  Created by alicehsu on 2021/12/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var myLabel:UILabel?
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
