b0f23d36077ecbedcd3c8defbe78c13c8eeea12a
徐國堂 <roberthsu2003@gmail.com>
2021-12-26 10:01:26 +0800

    modify

--- 星期日/lesson6.playground/Contents.swift
+++ 星期日/lesson6.playground/Contents.swift
@@ -2,3 +2,23 @@ let names = ["Anna", "Alex", "Brian", "Jack"]
 for name in names{
     print("Hello,\(name)")
 }
+
+let numberOfLegs = ["spider":8, "ant":6, "cat":4]
+
+for (animalName, legCount) in numberOfLegs{
+    print("\(animalName)有\(legCount)腳")
+}
+
+for index in 1...5{
+    print("\(index)*5=\(index*5)")
+}
+
+
+let base = 3
+let power = 10
+var answer = 1
+for _ in 1...power{
+    answer *= 3
+}
+print("\(base)的\(power)次方是\(answer)")
+
