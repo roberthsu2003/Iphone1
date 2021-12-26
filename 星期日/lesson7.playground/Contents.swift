a7998e0bd4b7aa0f03e6ee1483fb598a1ef8fd40
徐國堂 <roberthsu2003@gmail.com>
2021-12-26 14:16:57 +0800

    modify

--- 星期日/lesson7.playground/Contents.swift
+++ 星期日/lesson7.playground/Contents.swift
@@ -11,3 +11,16 @@ case (-2...2,-2...2):
 default:
     print("在範圍外")
 }
+
+//value binding
+let anotherPoint = (2, 0)
+switch anotherPoint{
+case (0,0):
+    print("在原點")
+case (let x,0):
+    print("在x軸上的\(x)")
+case (0, let y):
+    print("在y軸上的\(y)")
+case let (x,y):
+    print("x是:\(x),y是\(y)")
+}
