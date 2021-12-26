220956e98fbf1347cac6db1d0aeb6aef7c911982
徐國堂 <roberthsu2003@gmail.com>
2021-12-26 16:38:59 +0800

    modify

---
+++ 星期日/project4/project4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  project4
+//
+//  Created by 徐國堂 on 2021/12/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var highTextField:UITextField!
+    @IBOutlet var weightTextField:UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func userClick(_:UIButton){
+        
+    }
+
+
+}
+
