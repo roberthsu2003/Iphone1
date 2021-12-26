b6ead9372644d6b0d514c729176f685b0877c39a
徐國堂 <roberthsu2003@gmail.com>
2021-12-26 16:53:53 +0800

    modify

--- 星期日/project4/project4/ViewController.swift
+++ 星期日/project4/project4/ViewController.swift
@@ -17,7 +17,18 @@ class ViewController: UIViewController {
     }
     
     @IBAction func userClick(_:UIButton){
+        /*
+        if let heightValue = Int(highTextField.text!), let weightValue = Int(weightTextField.text!){
+            
+        }
+         */
+        guard let heightValue = Int(highTextField.text!), let weightValue = Int(weightTextField.text!) else{
+            print("輸入有錯誤")
+            return
+        }
         
+        print("身高:\(heightValue)")
+        print("體重:\(weightValue)")
     }
 
 
