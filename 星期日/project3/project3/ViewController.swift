fde8c61bcffb8624714c904d4b332a2e67b19cd6
徐國堂 <roberthsu2003@gmail.com>
2021-12-26 15:19:04 +0800

    modify

--- 星期日/project3/project3/ViewController.swift
+++ 星期日/project3/project3/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //實體method,這個method不是由我們來呼叫,而是ios自動呼叫,當storyboard的內容已經可以用程式控制時
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("viewDidLoad執行了")
+    }
+    
+    override func viewWillAppear(_ animated: Bool){
+        super.viewWillAppear(animated)
+        
+        print("view內的按鈕畫面將顯示了")
     }
 
+    override func viewDidAppear(_ animated: Bool){
+        super.viewDidAppear(animated)
+        print("view內的按鈕畫已顯示了")
+    }
 
 }
 
