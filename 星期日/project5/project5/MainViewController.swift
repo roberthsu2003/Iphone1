94b9448d7983d838d0e1a15e01ecea65f04a1392
徐國堂 <roberthsu2003@gmail.com>
2022-01-02 12:21:04 +0800

    modify

---
+++ 星期日/project5/project5/MainViewController.swift
@@ -0,0 +1,27 @@
+//
+//  MainViewController.swift
+//  project5
+//
+//  Created by 徐國堂 on 2022/1/2.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func userOk(_ sender:UIBarButtonItem){
+        print("ok")
+    }
+    
+    @IBAction func userClear(_ sender:UIBarButtonItem){
+        print("clear")
+    }
+    
+
+}
