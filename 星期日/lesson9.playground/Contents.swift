10cdb9c595295ae2ec5624826b586879895ab50e
徐國堂 <roberthsu2003@gmail.com>
2022-01-02 10:17:39 +0800

    modify

--- 星期日/lesson9.playground/Contents.swift
+++ 星期日/lesson9.playground/Contents.swift
@@ -37,6 +37,7 @@ func greet(p:String){
 greet(p: "Anna")
 
 //function 可以傳出多個值(利用tuple)
+/*
 func minMax() -> (min:Int,max:Int){
     return (3,50)
 }
@@ -44,3 +45,45 @@ func minMax() -> (min:Int,max:Int){
 let bounds = minMax()
 bounds.min
 bounds.max
+ */
+
+//function也可檢查錯誤
+func minMax(num:Int) -> (min:Int, max:Int)?{
+    if num == 10{
+        return nil
+    }
+    return (3,50)
+}
+
+//使用if來檢查是不是nil
+if minMax(num: 10) == nil {
+    print("傳出的是nil")
+}
+
+//optional binding
+if let bounds = minMax(num: 10){
+    print(bounds.min)
+    print(bounds.max)
+}else{
+    print("傳出的是nil")
+}
+
+//nil 運算子
+let bounds = minMax(num: 10) ?? (10,10)
+print(bounds.min)
+print(bounds.max)
+
+//提早離開
+func simpleFun(){
+    guard let bounds = minMax(num: 10) else{
+        print("內容是nil")
+        return
+    }
+    
+    print(bounds.max)
+    print(bounds.min)
+    
+}
+
+simpleFun()
+
