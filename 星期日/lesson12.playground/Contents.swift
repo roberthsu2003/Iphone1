02d1c8438966d51affdc5f1c6e79ab356bfc1dcf
徐國堂 <roberthsu2003@gmail.com>
2022-01-09 14:35:03 +0800

    modify

--- 星期日/lesson12.playground/Contents.swift
+++ 星期日/lesson12.playground/Contents.swift
@@ -22,3 +22,14 @@ someVideoMode.name
 someVideoMode.frameRate
 someVideoMode.resolution.width
 someVideoMode.resolution.height
+
+someVideoMode.resolution.width = 1280
+someVideoMode.resolution.height = 960
+
+someVideoMode.resolution.width
+someVideoMode.resolution.height
+
+let vga = Resolution(width: 640, height: 480)
+vga.width
+vga.height
+
