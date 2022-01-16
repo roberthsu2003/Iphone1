7cf84d53d8b5274b25c8f14cb00564706160c42c
徐國堂 <roberthsu2003@gmail.com>
2022-01-16 10:04:48 +0800

    modify

--- 星期日/lesson13.playground/Contents.swift
+++ 星期日/lesson13.playground/Contents.swift
@@ -6,6 +6,18 @@ struct FixedLengthRange{
 var rangeOfThreeItems = FixedLengthRange(firstValue: 0, length: 3)
 rangeOfThreeItems.firstValue = 60
 
+class DataImporter{
+    var filename = "data.txt"
+}
 
+class DataManager{
+    lazy var importer = DataImporter()
+    var data:[String] = []
+}
+
+let manager = DataManager()
+manager.data.append("some data")
+manager.data.append("some more data")
+manager.importer.filename
 
 
