a61f7697558b1a116e9ccfbc4c30e47cafdd23c7
徐國堂 <roberthsu2003@gmail.com>
2022-01-16 10:35:37 +0800

    modify

--- 星期日/lesson13.playground/Contents.swift
+++ 星期日/lesson13.playground/Contents.swift
@@ -54,4 +54,17 @@ square.center = Point(x: 15.0, y: 15.0)
 
 square
 
+//read-only computed Property
+
+struct Cuboid{
+    var width=0.0, height=0.0, depth = 0.0
+    var volumn:Double{
+        return width * height * depth
+    }
+}
+
+var fourByFiveByTwo = Cuboid(width: 4.0, height: 5.0, depth: 2.0)
+fourByFiveByTwo.volumn
+
+
 
