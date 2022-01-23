01005e207132c9807b4844c26c506030935b6011
徐國堂 <roberthsu2003@gmail.com>
2022-01-23 14:32:03 +0800

    modify

--- 星期日/lesson17.playground/Contents.swift
+++ 星期日/lesson17.playground/Contents.swift
@@ -30,6 +30,12 @@ struct Celsius{
     init(fromKelvin kelvin:Double){
         temperatureInCelsius = kelvin - 273.15
     }
+    
+    //沒有引數名稱的init
+    init(_
+         celsius:Double){
+        temperatureInCelsius = celsius
+    }
 }
 
 let boilingPointOfWater = Celsius(fromFahrenheit: 212.0)
@@ -37,3 +43,23 @@ boilingPointOfWater.temperatureInCelsius
 
 let freezingPointOfWater = Celsius(fromKelvin: 273.15)
 freezingPointOfWater.temperatureInCelsius
+
+let bodyTemperature = Celsius(37.0)
+
+struct Color{
+    let red, green, blue:Double
+    init(red:Double, green:Double, blue:Double){
+        self.red = red;
+        self.green = green;
+        self.blue = blue
+    }
+    
+    init(white:Double){
+        red = white
+        green = white
+        blue = white
+    }
+}
+
+let magenta = Color(red: 1.0, green: 0.0, blue: 0.0)
+let halfGray = Color(white: 0.5)
