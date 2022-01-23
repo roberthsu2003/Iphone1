0fdab0373b3ada9a9a41d4d6652f1cc3354a8ea2
徐國堂 <roberthsu2003@gmail.com>
2022-01-23 14:50:07 +0800

    modify

--- 星期日/lesson17.playground/Contents.swift
+++ 星期日/lesson17.playground/Contents.swift
@@ -98,3 +98,23 @@ struct Size{
 }
 
 let twoByTwo = Size(width: 2.0, heigth: 2.0)
+
+struct Point{
+    var x = 0.0, y = 0.0
+}
+
+struct Rect{
+    var origin = Point()
+    var size = Size()
+    
+    init(origin:Point, size:Size){
+        self.origin = origin
+        self.size = size
+    }
+    
+    init(center:Point, size:Size){
+        let originX  = center.x - (size.width / 2)
+        let originY = center.y - (size.heigth / 2)
+        self.init(origin: Point(x: originX, y: originY), size: size)
+    }
+}
