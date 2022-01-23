b77b680e361ff7384be99c6e8f12f9469a786ebb
徐國堂 <roberthsu2003@gmail.com>
2022-01-23 14:25:46 +0800

    modify

--- 星期日/lesson17.playground/Contents.swift
+++ 星期日/lesson17.playground/Contents.swift
@@ -18,3 +18,22 @@ struct Fahrenheit{
         return 32
     }()
 }
+
+//有參數的初始化者
+
+struct Celsius{
+    var temperatureInCelsius:Double
+    init(fromFahrenheit fahrenheit:Double){
+        temperatureInCelsius = (fahrenheit - 32.0) / 1.8
+    }
+    
+    init(fromKelvin kelvin:Double){
+        temperatureInCelsius = kelvin - 273.15
+    }
+}
+
+let boilingPointOfWater = Celsius(fromFahrenheit: 212.0)
+boilingPointOfWater.temperatureInCelsius
+
+let freezingPointOfWater = Celsius(fromKelvin: 273.15)
+freezingPointOfWater.temperatureInCelsius
