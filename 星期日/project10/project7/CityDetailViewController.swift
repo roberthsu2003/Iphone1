8c38248875554113849088f9cfb439686db5f5a1
徐國堂 <roberthsu2003@gmail.com>
2022-02-13 17:00:28 +0800

    modify

--- 星期日/project10/project7/CityDetailViewController.swift
+++ 星期日/project10/project7/CityDetailViewController.swift
@@ -9,11 +9,12 @@ import UIKit
 
 class CityDetailViewController: UIViewController {
     var imageName:String!
+    @IBOutlet var cityImageView:UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("imageName=\(imageName!)")
-        
+        //print("imageName=\(imageName!)")
+        cityImageView.image = UIImage(named: imageName)
         
     }
     
