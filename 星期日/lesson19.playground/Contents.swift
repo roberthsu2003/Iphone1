96280fb0b10b8d0e7972b21f5a8e93d3d930ee56
徐國堂 <roberthsu2003@gmail.com>
2022-02-13 14:40:27 +0800

    modify

--- 星期日/lesson19.playground/Contents.swift
+++ 星期日/lesson19.playground/Contents.swift
@@ -13,4 +13,34 @@ class Bank{
 
 Bank.coinsInBank
 
+class Player{
+    var coinsInPurse:Int
+    
+    init(coins:Int){
+        coinsInPurse = Bank.distribute(coins: coins)
+    }
+    
+    func win(coins:Int){
+        coinsInPurse += Bank.distribute(coins: coins)
+    }
+    
+    deinit{
+        print("記憶體被回收")
+        Bank.receive(coins: coinsInPurse)
+    }
+}
+
+var playerOne:Player? = Player(coins: 100)
+print("玩家借了\(playerOne!.coinsInPurse)幣")
+print("銀行還有多少\(Bank.coinsInBank)幣")
+
+var playerTwo:Player? = Player(coins: 200)
+print("玩家借了\(playerTwo!.coinsInPurse)幣")
+print("銀行還有多少\(Bank.coinsInBank)幣")
+
+playerOne = nil
+print("銀行還有多少\(Bank.coinsInBank)幣")
+
+playerTwo = nil
+print("銀行還有多少\(Bank.coinsInBank)幣")
 
