f0c38348d680f9575fed95317a4ab76323168cd5
徐國堂 <roberthsu2003@gmail.com>
2022-02-13 10:16:37 +0800

    modify

--- 星期日/lesson18.playground/Contents.swift
+++ 星期日/lesson18.playground/Contents.swift
@@ -7,3 +7,16 @@ class Vehicle{
 
 let vehicle = Vehicle()
 vehicle.description
+
+class Bicycle:Vehicle{
+    override init(){
+        //step1:現讓自已所有的storeproperty有值
+        //step2:super.init
+        //step3:使用繼承來的store property和method
+        super.init()
+        numberOfWheel = 2
+    }
+}
+
+let bicycle = Bicycle()
+bicycle.description
