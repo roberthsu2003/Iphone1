8bd0175cca2d354f1e3d926b34986899c8a9e78a
徐國堂 <roberthsu2003@gmail.com>
2022-02-13 16:14:44 +0800

    modify

--- 星期日/project10/project7/ViewController.swift
+++ 星期日/project10/project7/ViewController.swift
@@ -48,10 +48,14 @@ class ViewController: UITableViewController {
         print("目標:\(segue.destination)")
         print("來源:\(segue.source)")
         print("識別:\(segue.identifier ?? "沒有識別")")
-         */
+        */
         
-        let detailViewController = segue.destination as! CityDetailViewController
-        detailViewController.imageName = "abc.jpg"
+        if segue.identifier == "goDetail"{
+            let detailViewController = segue.destination as! CityDetailViewController
+            detailViewController.imageName = "abc.jpg"
+        }
+        
+        
     }
     
     //MARK: - UITableViewDataSource
