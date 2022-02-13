e306fd282764529c74c1b906305b7153660fb9ff
徐國堂 <roberthsu2003@gmail.com>
2022-02-13 15:00:04 +0800

    modify

---
+++ 星期日/project10/project7/CityDetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CityDetailViewController.swift
+//  project10
+//
+//  Created by 徐國堂 on 2022/2/13.
+//
+
+import UIKit
+
+class CityDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
