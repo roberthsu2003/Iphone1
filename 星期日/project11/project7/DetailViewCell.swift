948a2a363095449fc4a8d0cbfb44c85f33dd77c2
徐國堂 <roberthsu2003@gmail.com>
2022-02-20 15:00:18 +0800

    modify

---
+++ 星期日/project11/project7/DetailViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  DetailViewCell.swift
+//  project11
+//
+//  Created by 徐國堂 on 2022/2/20.
+//
+
+import UIKit
+
+class DetailViewCell: UITableViewCell {
+    @IBOutlet var fieldLabel:UILabel!
+    @IBOutlet var valueLabel:UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
