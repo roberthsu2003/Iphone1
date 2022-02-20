c71f76eea2e8bed1ad85859c6814508d7c44cf13
徐國堂 <roberthsu2003@gmail.com>
2022-02-20 15:26:10 +0800

    modify

--- 星期日/project11/project7/CityDetailViewController.swift
+++ 星期日/project11/project7/CityDetailViewController.swift
@@ -23,12 +23,34 @@ class CityDetailViewController: UIViewController {
 extension CityDetailViewController:UITableViewDataSource{
     func tableView(_ tableView: UITableView,
                    numberOfRowsInSection section: Int) -> Int{
-        
+        return 4
     }
     
     func tableView(_ tableView: UITableView,
                    cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CELL", for: indexPath) as! DetailViewCell
         
+        switch indexPath.row{
+        case 0:
+            cell.fieldLabel.text = "城市"
+            cell.valueLabel.text = city.city
+        case 1:
+            cell.fieldLabel.text = "國家"
+            cell.valueLabel.text = city.country
+        
+        case 2:
+            cell.fieldLabel.text = "洲"
+            cell.valueLabel.text = city.continent
+            
+        case 3:
+            cell.fieldLabel.text = "網址"
+            cell.valueLabel.text = city.url
+        default:
+            cell.fieldLabel.text = ""
+            cell.valueLabel.text = ""
+        }
+       
+        return cell;
     }
     
     
