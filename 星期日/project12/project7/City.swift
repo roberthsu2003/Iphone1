9e64543741d7ab57df8261102f10bd522dd3f294
徐國堂 <roberthsu2003@gmail.com>
2022-02-20 16:19:11 +0800

    modify

---
+++ 星期日/project12/project7/City.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+class City{
+    var city:String
+    var country:String
+    var continent:String
+    var image:String
+    var local:String
+    var latitude:Double
+    var longitude:Double
+    var url:String
+    
+    init(city:String,continent:String, country:String, image:String, local:String, lat latitude:Double, lon longitude:Double, url:String){
+        self.city = city
+        self.country = country
+        self.continent = continent
+        self.image = image
+        self.local = local
+        self.latitude = latitude
+        self.longitude = longitude
+        self.url = url
+    }
+}
