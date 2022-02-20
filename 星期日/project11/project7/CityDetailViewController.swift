992a5013981298df9a540f4b32e15acc01a56a75
徐國堂 <roberthsu2003@gmail.com>
2022-02-20 10:44:07 +0800

    modify

---
+++ 星期日/project11/project7/CityDetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CityDetailViewController.swift
+//  project10
+//
+//  Created by 徐國堂 on 2022/2/13.
+//
+
+import UIKit
+
+class CityDetailViewController: UIViewController {
+    var imageName:String!
+    @IBOutlet var cityImageView:UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //print("imageName=\(imageName!)")
+        /*
+        if let imageName = imageName {
+            cityImageView.image = UIImage(named: imageName)
+        }
+        */
+        cityImageView.image = UIImage(named: imageName)
+        
+    }
+    
+
+    
+
+}
