1d1354435f394c0eedbf90f0f95b1d0b153e5b7a
徐國堂 <roberthsu2003@gmail.com>
2022-02-27 16:14:55 +0800

    modify

--- 星期日/project13/project13/ViewController.swift
+++ 星期日/project13/project13/ViewController.swift
@@ -62,7 +62,20 @@ extension ViewController{
             let sv = UIView(frame: cell.bounds)
             sv.backgroundColor = UIColor(white: 0.2, alpha: 0.5)
             cell.selectedBackgroundView = sv;
+            
+            let cityImageView = UIImageView(frame: cell.bounds)
+            cityImageView.contentMode = .scaleAspectFill
+            cityImageView.translatesAutoresizingMaskIntoConstraints = false
+            cell.contentView.addSubview(cityImageView)
+            NSLayoutConstraint.activate([
+                cityImageView.topAnchor.constraint(equalTo: cell.contentView.topAnchor, constant: 0),
+                cityImageView.bottomAnchor.constraint(equalTo: cell.contentView.bottomAnchor, constant: 0),
+                cityImageView.leadingAnchor.constraint(equalTo: cell.contentView.leadingAnchor, constant: 0),
+                cityImageView.trailingAnchor.constraint(equalTo: cell.contentView.trailingAnchor, constant: 0),
+            ])
         }
+        
+        
         return cell;
     }
     
