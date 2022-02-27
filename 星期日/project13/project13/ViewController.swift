e6215f9fe085d1ec8bb430b795c637c7cf22a4ff
徐國堂 <roberthsu2003@gmail.com>
2022-02-27 15:37:29 +0800

    modify

--- 星期日/project13/project13/ViewController.swift
+++ 星期日/project13/project13/ViewController.swift
@@ -8,9 +8,35 @@
 import UIKit
 
 class ViewController:UICollectionViewController{
-
+    var citys = [City]()
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        let mainbundle = Bundle.main
+        let pathURL = mainbundle.url(forResource: "citylist", withExtension: "plist")!
+        guard let dicCitys = try! NSArray(contentsOf: pathURL, error: ()) as? [[String:Any]] else{
+            return
+        }
+        for dicCity in dicCitys{
+            let cityname = dicCity["City"] as! String
+            let country = dicCity["Country"] as! String
+            let continent = dicCity["Continent"] as! String
+            let image = dicCity["Image"] as! String
+            let local = dicCity["Local"] as! String
+            let url = dicCity["url"] as! String
+            let latitude = dicCity["lat"] as! Double
+            let longitude = dicCity["long"] as! Double
+            let city = City(city: cityname, continent: continent, country: country, image: image, local: local, lat: latitude, lon: longitude, url: url)
+            citys.append(city)
+        }
+        
+        
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("viewDidLoad")
         title = "城市"
         let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
         layout.itemSize = CGSize(width: 180, height: 120)
