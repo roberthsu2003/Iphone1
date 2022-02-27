7fb89bfb43285e11464db7c6e67ed906fb21b1c5
徐國堂 <roberthsu2003@gmail.com>
2022-02-27 09:47:38 +0800

    modify

---
+++ 星期日/lesson21.playground/Contents.swift
@@ -0,0 +1,22 @@
+class MediaItem{
+    var name:String
+    init(name:String){
+        self.name = name
+    }
+}
+
+class Movie:MediaItem{
+    var director:String
+    init(name:String, director:String){
+        self.director = director
+        super.init(name: name)
+    }
+}
+
+class Song:MediaItem{
+    var artist:String
+    init(name:String, artist:String){
+        self.artist = artist
+        super.init(name: name)
+    }
+}
