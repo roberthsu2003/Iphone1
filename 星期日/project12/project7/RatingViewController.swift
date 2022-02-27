b2b7ca5167ec1708ef3e39f7b0c2b8ef5156a1f6
徐國堂 <roberthsu2003@gmail.com>
2022-02-27 11:25:36 +0800

    modify

--- 星期日/project12/project7/RatingViewController.swift
+++ 星期日/project12/project7/RatingViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class RatingViewController: UIViewController {
     @IBOutlet var backgroundImageView:UIImageView!;
+    @IBOutlet var askLabel:UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        let blurEffect = UIBlurEffect(style: .dark)
+        let blurEffectView = UIVisualEffectView(effect: blurEffect)
+        blurEffectView.frame = view.bounds
+        backgroundImageView.addSubview(blurEffectView)
+        
+        askLabel.text = "您曾經來過這裏嗎?\n給這個城市一個描素吧!"
     }
     
 
