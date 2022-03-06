747460baeb1517fe4067ad2cd69bc2a462a31d57
徐國堂 <roberthsu2003@gmail.com>
2022-03-06 16:12:41 +0800

    modify

--- 星期日/project15/project15/dataSource.swift
+++ 星期日/project15/project15/dataSource.swift
@@ -2,7 +2,11 @@ import Foundation
 
 class DataSource{
     static func copyFilesToDocuments(){
+        //取得mainBundle內,citys.db的絕對路徑
         let sourceURL = Bundle.main.url(forResource: "citys", withExtension: "db")
-        print(sourceURL?.path)
+        
+        let targetURLs = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+        
+        print(targetURLs)
     }
 }
