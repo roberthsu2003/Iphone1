2c7b1541b6112b8a4614980ce869f056a5d930cb
徐國堂 <roberthsu2003@gmail.com>
2022-03-13 12:24:36 +0800

    modify

--- 星期日/project15/project15/ViewController.swift
+++ 星期日/project15/project15/ViewController.swift
@@ -8,24 +8,31 @@
 import UIKit
 
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
     let cities = DataSource.singleton.cities
-    
-    override func awakeFromNib() {
-        
-        for city in cities{
-            print(city.city)
-            print(city.country)
-            print("===============")
-        }
-        
-    }
+   
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
 
+}
 
+extension ViewController{
+    override func tableView(_ tableView: UITableView,
+                   numberOfRowsInSection section: Int) -> Int{
+        return cities.count
+    }
+    
+    override func tableView(_ tableView: UITableView,
+                   cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        let city = cities[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CELL", for: indexPath)
+
+        cell.textLabel?.text = city.city
+        return cell
+        
+    }
 }
 
