16d4afd1a57e532573151bc746434fdda288a151
徐國堂 <roberthsu2003@gmail.com>
2022-03-13 14:19:16 +0800

    modify

---
+++ 星期日/lesson25.playground/Contents.swift
@@ -0,0 +1,47 @@
+var someInt = 3
+var anotherInt = 107
+
+//inout參數
+func swapTwoInts(_ a:inout Int,_ b:inout Int){
+    let tempararyA = a
+    a = b
+    b = tempararyA
+}
+
+swapTwoInts(&someInt,&anotherInt)
+someInt
+anotherInt
+
+func swapTwoDoubles(_ a:inout Double,_ b:inout Double){
+    let tempararyA = a
+    a = b
+    b = tempararyA
+}
+
+var someDouble = 3.0
+var anotherDouble = 107.0
+
+swapTwoDoubles(&someDouble, &anotherDouble)
+someDouble
+anotherDouble
+
+func swapTwoValues<T>(_ a:inout T,_ b:inout T){
+    let tempararyA = a
+    a = b
+    b = tempararyA
+}
+
+var someInt1 = 3
+var anotherInt1 = 107
+
+swapTwoValues(&someInt1, &anotherInt1)
+someInt1
+anotherInt1
+
+
+var someDouble1 = 3.0
+var anotherDouble1 = 107.0
+
+swapTwoValues(&someDouble1, &anotherDouble1)
+someDouble1
+anotherDouble1
