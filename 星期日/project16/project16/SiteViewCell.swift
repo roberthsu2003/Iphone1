cbc522c66d6ac6fed0f7150d36933c6b9fe0eee4
徐國堂 <roberthsu2003@gmail.com>
2022-03-20 15:59:00 +0800

    modify

---
+++ 星期日/project16/project16/SiteViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  SiteViewCell.swift
+//  project16
+//
+//  Created by 徐國堂 on 2022/3/20.
+//
+
+import UIKit
+
+class SiteViewCell: UITableViewCell {
+
+    @IBOutlet var siteName:UILabel!
+    @IBOutlet var total:UILabel!
+    @IBOutlet var rent:UILabel!
+    @IBOutlet var returns:UILabel!
+    @IBOutlet var mday:UILabel!
+
+}
