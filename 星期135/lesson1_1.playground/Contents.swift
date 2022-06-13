4f1d26ac1275463b62c2a0c4e3cd1ea23b7f61bd
徐國堂 <roberthsu2003@gmail.com>
2022-06-13 19:55:54 +0800

    modify

--- 星期135/lesson1_1.playground/Contents.swift
+++ 星期135/lesson1_1.playground/Contents.swift
@@ -3,3 +3,13 @@ import Foundation
 var greeting = "Hello, playground"
 print(greeting)
 
+let maximum = 10
+var currentLogin = 0
+print(maximum)
+print(currentLogin)
+
+var x = 0.0, y = 0.0, z = 0.0
+print(x)
+print(y)
+print(z)
+
