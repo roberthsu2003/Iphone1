2074b2b25b9b9d76b8bbbcafa49b3ef12378fa83
徐國堂 <roberthsu2003@gmail.com>
2022-06-13 21:59:42 +0800

    modify

--- 星期135/lesson1_3.playground/Contents.swift
+++ 星期135/lesson1_3.playground/Contents.swift
@@ -3,9 +3,18 @@
 var serverResponseCode:Int
 //serverResponseCode
 var serverResponseCode1:Int?
-print(serverResponseCode1)
+//print(serverResponseCode1!) -> nil使用force unwrapping會出錯
 
 serverResponseCode1 = 404
-serverResponseCode1
+print(serverResponseCode1!)
+
+let possibleNumber = "123"
+let converedNumber = Int(possibleNumber)
+if converedNumber == nil{
+    print("無法轉換")
+}else{
+    print(converedNumber!)
+}
+
 
 
