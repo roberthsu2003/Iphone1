717e81894dbcddd1ef500724f896eec85d772cbf
徐國堂 <roberthsu2003@gmail.com>
2022-06-13 20:11:31 +0800

    modify

--- 星期135/lesson1_1.playground/Contents.swift
+++ 星期135/lesson1_1.playground/Contents.swift
@@ -1,5 +1,6 @@
 import Foundation
 
+//透過推測內容指定資料類型
 var greeting = "Hello, playground"
 print(greeting)
 
@@ -12,4 +13,17 @@ var x = 0.0, y = 0.0, z = 0.0
 print(x)
 print(y)
 print(z)
+
+//明確宣告資料類型
+var welcomeMessage:String
+welcomeMessage = "Hello"
+
+//一個變數宣告完成，必需要有明確的變數名稱和資料類型
+//var welcomeMessage1  -> 這是錯誤的建立變數
+
+var red, green, blue:Double
+var red1 = 0.0
+
+
+
 
