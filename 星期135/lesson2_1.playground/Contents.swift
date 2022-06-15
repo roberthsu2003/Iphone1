2f92ddbdb84e472475cd537648681c378a5ea7cd
徐國堂 <roberthsu2003@gmail.com>
2022-06-15 20:19:59 +0800

    modify

--- 星期135/lesson2_1.playground/Contents.swift
+++ 星期135/lesson2_1.playground/Contents.swift
@@ -29,9 +29,9 @@ if let convertedNumber3 = Int(possibleNumber3){
 
 //optional type
 let possibleString:String? = "An optional string."
-print(possibleString!)
+print(possibleString!.description)
 
 //implictly unwrapped optional
 let assumedString:String! = "An implicitly unwrapped optional stirng."
-print(assumedString)
+print(assumedString.description)
 
