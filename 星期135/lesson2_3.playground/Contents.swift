baefeb5db7e927b6b9f56729998914c7f89380c9
徐國堂 <roberthsu2003@gmail.com>
2022-06-15 21:21:45 +0800

    modify

---
+++ 星期135/lesson2_3.playground/Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+//array
+//建立一個空陣列
+var someInts:[Int] = []
+someInts.count
+
+//增加值在最後面
+
+someInts.append(3)
+someInts
+someInts.append(5)
+someInts
+someInts.append(7)
+someInts
+
+//透過[]符號給值
+var someDouble = [1.0,2.0,3.0,4.0,5.0]
+someDouble
+
+var threeDoubles = Array(repeating: 0.0, count: 3)
+threeDoubles
+
+
