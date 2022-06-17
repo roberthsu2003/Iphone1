b253804bf8dc816291ead5e2f8599672ec347e27
徐國堂 <roberthsu2003@gmail.com>
2022-06-17 21:42:49 +0800

    modify

--- 星期135/lesson3_3.playground/Contents.swift
+++ 星期135/lesson3_3.playground/Contents.swift
@@ -6,3 +6,32 @@ func greet(person:String) -> String{
 
 greet(person: "Robert")
 greet(person: "Jenny")
+
+//沒有參數是function
+
+func sayHelloWorld() -> String{
+    return "hello!world!"
+}
+
+sayHelloWorld()
+
+//多個參數的function
+func greet(person:String, alreadyGreeted:Bool) -> String{
+    if alreadyGreeted{
+        return "已經打過招呼"
+    }else{
+        return greet(person: person)
+    }
+}
+
+greet(person: "Tim", alreadyGreeted: false)
+
+//沒有傳出值的function
+func greet1(person:String){
+    print("Hello! \(person)")
+}
+
+greet1(person: "jenny")
+
+
+
