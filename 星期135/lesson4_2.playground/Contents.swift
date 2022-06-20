488221d07ac4b87f4fbb4c3b7f4f8d02c4d61968
徐國堂 <roberthsu2003@gmail.com>
2022-06-20 20:20:26 +0800

    modify

--- 星期135/lesson4_2.playground/Contents.swift
+++ 星期135/lesson4_2.playground/Contents.swift
@@ -9,9 +9,25 @@ func backward(_ s1:String, _ s2:String) -> Bool{
 var reverseName = names.sorted(by: backward)
 reverseName
 
-//參數使用closure
+//參數使用closure標準語法
 var reverseName1 = names.sorted(by: {
     (s1:String,s2:String) -> Bool in
     return s1 < s2
 })
 reverseName1
+
+//簡化closure的語法
+var reverseName2 = names.sorted(by: {
+    (s1,s2) in s1 < s2
+})
+
+reverseName2
+
+var reverseName3 = names.sorted(by: {
+      $0 < $1
+})
+
+reverseName3
+
+var reverseName4 = names.sorted(by: <)
+reverseName4
