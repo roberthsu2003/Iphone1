23f6016968e8e4766c6cae313088a8a03ca435be
徐國堂 <roberthsu2003@gmail.com>
2022-06-20 21:29:31 +0800

    modify

--- 星期135/lesson4_4.playground/Contents.swift
+++ 星期135/lesson4_4.playground/Contents.swift
@@ -31,3 +31,22 @@ someVideoMode.resolution.height
 
 someVideoMode.resolution.width = 1280
 someVideoMode.resolution.width
+
+//結構有2種內建的初始化
+//default init
+//成員init
+
+let vga = Resolution(width: 640, height: 480)
+
+//結構是value type
+vga.width
+//vga.width = 800 //value type let,無法更改屬性
+//value type使用=就是複制一個實體
+
+let hd = Resolution(width: 1920, height: 1080)
+var cinema = hd
+cinema.width = 2048
+hd.width
+cinema.width
+
+
