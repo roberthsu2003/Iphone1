4f72102a6faf81a183d69dc2a0afc0c681451e8a
徐國堂 <roberthsu2003@gmail.com>
2022-06-20 19:28:58 +0800

    modify

--- 星期135/lesson4_1.playground/Contents.swift
+++ 星期135/lesson4_1.playground/Contents.swift
@@ -4,3 +4,10 @@ func greet(person:String, from hometow:String) -> String{
 }
 
 greet(person: "Bill", from: "台北")
+
+//省略引數名稱
+
+func someFunction(_ firstParameterName:Int, secondParameterName:Int){
+    
+}
+someFunction(1, secondParameterName: 2)
