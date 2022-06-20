4f9036c737d372fdb0fa2c7bf5865c4bfb9fa721
徐國堂 <roberthsu2003@gmail.com>
2022-06-20 19:32:29 +0800

    modify

--- 星期135/lesson4_1.playground/Contents.swift
+++ 星期135/lesson4_1.playground/Contents.swift
@@ -11,3 +11,12 @@ func someFunction(_ firstParameterName:Int, secondParameterName:Int){
     
 }
 someFunction(1, secondParameterName: 2)
+
+//參數可以有default value
+//呼叫的方式變多了
+
+func someFunction(parameterWithoutDefault:Int, parameterWithDefault:Int=12){
+    
+}
+someFunction(parameterWithoutDefault: 10)
+someFunction(parameterWithoutDefault: 10, parameterWithDefault: 15)
