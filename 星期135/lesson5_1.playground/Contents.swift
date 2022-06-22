f1e851b94598dc612de29e6110ef2f43bd278355
徐國堂 <roberthsu2003@gmail.com>
2022-06-22 19:48:40 +0800

    modify

--- 星期135/lesson5_1.playground/Contents.swift
+++ 星期135/lesson5_1.playground/Contents.swift
@@ -28,3 +28,41 @@ let manager = DataManager()
 manager.data.append("Some Data")
 manager.data.append("some More Data")
 print(manager.importer.filename)
+
+//computed property
+struct Point{
+    var x = 0.0, y = 0.0
+}
+
+struct Size{
+    var width=0.0, height = 0.0
+}
+
+struct Rect{
+    var origin = Point()
+    var size = Size()
+    //computed Property
+    //get,set
+    //get
+    //computed Property明確宣告資料類型
+    var center:Point{
+        get{
+            let centerX = origin.x + (size.width/2)
+            let centerY = origin.y + (size.height/2)
+            return Point(x: centerX, y: centerY)
+        }
+        set(newCenter){
+            origin.x = newCenter.x - (size.width / 2)
+            origin.y = newCenter.y - (size.height / 2)
+        }
+    }
+    
+}
+
+var square = Rect(origin: Point(x: 0, y: 0), size: Size(width: 10.0, height: 10.0))
+square.center
+
+square.center = Point(x: 15.0, y: 15.0)
+
+square.center
+
