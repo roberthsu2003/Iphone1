471cdaebabf34e277219a2c30ce1df9828133f34
徐國堂 <roberthsu2003@gmail.com>
2022-06-22 21:18:53 +0800

    modify

--- 星期135/lesson5_2.playground/Contents.swift
+++ 星期135/lesson5_2.playground/Contents.swift
@@ -40,3 +40,38 @@ var somePoint = Point(x: 1.0, y: 1.0)
 somePoint.moveBy(x: 2.0, y: 3.0)
 somePoint.x
 somePoint.y
+
+//列舉也可以有method()
+//value type 可以使用self=xxxx
+struct Point1{
+    var x = 0.0, y = 0.0
+    mutating func moveBy(x deltaX:Double, y deltaY:Double){
+       self = Point1(x: x + deltaX, y: y + deltaY)
+    }
+}
+
+var somePoint1 = Point1(x: 1.0, y: 1.0)
+somePoint1.moveBy(x: 2.0, y: 3.0)
+somePoint1.x
+somePoint1.y
+
+enum TriStateSwitch{
+    case off, low, high
+    mutating func next(){
+        switch self{
+        case .off:
+            self = .low
+        case .low:
+            self = .high
+        case .high:
+            self = .off
+            
+        }
+    
+    }
+}
+
+var overLight = TriStateSwitch.off
+overLight.next()
+overLight.next()
+overLight.next()
