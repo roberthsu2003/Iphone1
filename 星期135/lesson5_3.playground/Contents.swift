9a65903b9e4d788ee3f6b94bf2ec7453aaedaa45
徐國堂 <roberthsu2003@gmail.com>
2022-06-22 21:42:55 +0800

    modify

--- 星期135/lesson5_3.playground/Contents.swift
+++ 星期135/lesson5_3.playground/Contents.swift
@@ -4,7 +4,7 @@ var numberOfLegs = ["spider":8 , "ant":6, "cat":4]
 numberOfLegs["bird"] = 2
 numberOfLegs
 
-//自訂一個subscript
+//自訂一個實體的subscript
 struct TimesTable{
     let multiplier:Int
     subscript(index:Int) -> Int{
@@ -14,3 +14,15 @@ struct TimesTable{
 
 let threeTimesTable = TimesTable(multiplier: 3)
 threeTimesTable[5]
+
+//type subscript
+enum Planet:Int{
+    case mercury = 1, venus, earth, mars, jupiter, saturn, uranus, neptune
+    
+    static subscript(n:Int) -> Planet{
+        return Planet(rawValue: n)!
+    }
+}
+
+let mars = Planet[4]
+mars.rawValue
