4b2ff93f0e92b98214ffea9f3d70b33d41e51d11
徐國堂 <roberthsu2003@gmail.com>
2022-06-22 21:10:14 +0800

    modify

--- 星期135/lesson5_2.playground/Contents.swift
+++ 星期135/lesson5_2.playground/Contents.swift
@@ -24,3 +24,19 @@ counter.increment(by: 5)
 counter.count
 counter.reset()
 counter.count
+
+//mutating method
+//只可以使用在結構
+//如果method會改變結構內store property的值,必需使用mutating method
+struct Point{
+    var x = 0.0, y = 0.0
+    mutating func moveBy(x deltaX:Double, y deltaY:Double){
+        x += deltaX
+        y += deltaY
+    }
+}
+
+var somePoint = Point(x: 1.0, y: 1.0)
+somePoint.moveBy(x: 2.0, y: 3.0)
+somePoint.x
+somePoint.y
