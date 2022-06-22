19792536d2a73837c679839f84762782d758c25d
徐國堂 <roberthsu2003@gmail.com>
2022-06-22 19:24:34 +0800

    modify

--- 星期135/lesson5_1.playground/Contents.swift
+++ 星期135/lesson5_1.playground/Contents.swift
@@ -1,5 +1,6 @@
 import UIKit
 
+//store property
 struct FixedLengthRange{
     var firstValue:Int
     let length:Int
@@ -13,3 +14,17 @@ rangeOfThreeItems.firstValue = 6
 let rangeOfFourItems = FixedLengthRange(firstValue: 0, length: 4)
 //rangeOfFourItems.firstValue = 6
 
+//lazy store property
+class DataImporter{
+    var filename = "data.txt"
+}
+
+class DataManager{
+    lazy var importer = DataImporter()
+    var data:[String] = []
+}
+
+let manager = DataManager()
+manager.data.append("Some Data")
+manager.data.append("some More Data")
+print(manager.importer.filename)
