de0f1112ed03308604f94f36a2781685d9144aed
徐國堂 <roberthsu2003@gmail.com>
2022-06-22 19:13:21 +0800

    modify

---
+++ 星期135/lesson5_1.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+struct FixedLengthRange{
+    var firstValue:Int
+    let length:Int
+}
+
+var rangeOfThreeItems = FixedLengthRange(firstValue: 0, length: 3)
+rangeOfThreeItems.firstValue
+rangeOfThreeItems.length
+rangeOfThreeItems.firstValue = 6
+
+let rangeOfFourItems = FixedLengthRange(firstValue: 0, length: 4)
+//rangeOfFourItems.firstValue = 6
+
