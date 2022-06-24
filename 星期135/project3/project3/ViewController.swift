dd3e9166ddae37230da4958781ab75557b98b391
徐國堂 <roberthsu2003@gmail.com>
2022-06-24 21:23:59 +0800

    modify

--- 星期135/project3/project3/ViewController.swift
+++ 星期135/project3/project3/ViewController.swift
@@ -8,15 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var myLabel:UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("現在執行ViewDidLoad")
+        myLabel = UILabel(
+            frame:CGRect(x: 100, y: 100, width: 100, height: 40)
+        )
+        myLabel.text = "Hello! iOS!"
+        
+        view.addSubview(myLabel)
     }
     
     override func viewWillAppear(_ animated: Bool){
         super.viewWillAppear(animated)
         print("view將要出現了")
+        
     }
     
     override func viewDidAppear(_ animated: Bool){
