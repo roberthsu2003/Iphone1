467c780de20524b4abe101489af8645ec7add1cd
徐國堂 <roberthsu2003@gmail.com>
2022-06-24 20:13:52 +0800

    modify

--- 星期135/lesson6_2.playground/Contents.swift
+++ 星期135/lesson6_2.playground/Contents.swift
@@ -1,3 +1,4 @@
+/*
 struct Fahrenheit{
     var temperature:Double
     //自訂的init
@@ -6,5 +7,39 @@ struct Fahrenheit{
     }
 }
 
+
 var f = Fahrenheit()
 f.temperature
+ */
+//store property可以有defaul value
+
+struct Fahrenheit{
+    var temperature = 32.0
+}
+
+struct Celsius{
+    var temperatureInCelsius:Double
+    
+    init(fromFahrenheit fahrenheit:Double){
+        temperatureInCelsius = (fahrenheit - 32.0) / 1.8
+    }
+    
+    init(fromKelvin kelvin:Double){
+        temperatureInCelsius = kelvin - 273.15
+    }
+    
+    init(_ celsius:Double){
+        temperatureInCelsius=celsius
+    }
+}
+
+let boilingPointOfWater = Celsius(fromFahrenheit: 212.0)
+let freezingPointOfWater = Celsius(fromKelvin: 273.15)
+boilingPointOfWater.temperatureInCelsius
+freezingPointOfWater.temperatureInCelsius
+let bodyTemperature = Celsius(37.0)
+bodyTemperature.temperatureInCelsius
+
+
+
+
