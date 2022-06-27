9ca34ab7dc5c102a749816cd5f2d10d03e73f4df
徐國堂 <roberthsu2003@gmail.com>
2022-06-27 21:32:54 +0800

    modify

---
+++ 星期135/project4/project4/FirstViewController.swift
@@ -0,0 +1,24 @@
+//
+//  FirstViewController.swift
+//  project4
+//
+//  Created by 徐國堂 on 2022/6/27.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+    @IBOutlet var heightTextField:UITextField!
+    @IBOutlet var weightTextField:UITextField!
+    @IBOutlet var okButton:UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(heightTextField.text!)
+       
+    }
+    
+
+  
+
+}
