ef79bb905f2a1cf1b544c8520f5e69e9b0a1a4b3
徐國堂 <roberthsu2003@gmail.com>
2022-06-29 19:19:55 +0800

    modify

---
+++ 星期135/lesson8_1.playground/Contents.swift
@@ -0,0 +1,34 @@
+import UIKit
+
+class Bank{
+    static var coinsInbank = 10_000
+    static func distribute(coins numberOfCoinsRequested:Int) -> Int{
+        let numberOfCoinsToVend = min(numberOfCoinsRequested,coinsInbank)
+        coinsInbank -= numberOfCoinsToVend
+        return numberOfCoinsToVend
+    }
+    
+    static func receive(coins:Int){
+        coinsInbank += coins;
+    }
+}
+
+class Player{
+    var coinsInPurse:Int
+    init(coins:Int){
+        coinsInPurse = Bank.distribute(coins: coins)
+    }
+    func win(coins:Int){
+        coinsInPurse += Bank.distribute(coins: coins)
+    }
+    
+    deinit{
+        Bank.receive(coins: coinsInPurse)
+    }
+}
+
+var playerOne:Player? = Player(coins: 100)
+print("現在此人有\(playerOne!.coinsInPurse)coins")
+print("現在銀行的有\(Bank.coinsInbank)coins")
+
+
