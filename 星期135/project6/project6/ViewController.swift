239da65a30e92f1fdce5e23e7f05722f33b85c5c
徐國堂 <roberthsu2003@gmail.com>
2022-07-01 20:36:58 +0800

    modify

--- 星期135/project6/project6/ViewController.swift
+++ 星期135/project6/project6/ViewController.swift
@@ -9,9 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet var tableView:UITableView!
+    var list:[String] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        for i in 1...100{
+            list.append("item\(i)")
+            print(list[i-1])
+        }
+        
         //tableView.dataSource = self
     }
 
