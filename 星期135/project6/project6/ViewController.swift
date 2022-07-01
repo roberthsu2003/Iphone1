a365b6f5e8f11689e447dc6960372e3ceabd87f0
徐國堂 <roberthsu2003@gmail.com>
2022-07-01 20:27:53 +0800

    modify

---
+++ 星期135/project6/project6/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  project6
+//
+//  Created by 徐國堂 on 2022/7/1.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var tableView:UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //tableView.dataSource = self
+    }
+
+
+}
+
