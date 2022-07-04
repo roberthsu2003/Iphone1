68faac2bb8537b06674646f75f5b7b0d57876e55
徐國堂 <roberthsu2003@gmail.com>
2022-07-04 19:23:02 +0800

    modify

--- 星期135/lesson10_1.playground/Contents.swift
+++ 星期135/lesson10_1.playground/Contents.swift
@@ -8,3 +8,41 @@ enum VendingMachineError:Error{
 }
 
 //throw VendingMachineError.insufficientFunds(coinsNeeded: 5)
+
+struct Item{
+    var price:Int
+    var count:Int
+}
+
+class VendingMachine{
+    var inventory = [
+        "Candy Bar":Item(price: 12, count: 7),
+        "Chips": Item(price: 10, count: 4),
+        "Pretzels": Item(price: 7, count: 11)
+    ]
+    
+    var coinsDeposited = 0
+    
+    func vend(itemNamed name:String) throws{
+        guard let item = inventory[name] else{
+            throw VendingMachineError.invalidSelection
+        }
+        
+        guard item.count > 0 else{
+            throw VendingMachineError.outOfStock
+        }
+        
+        guard item.price <= coinsDeposited else{
+            throw VendingMachineError.insufficientFunds(coinsNeeded: item.price - coinsDeposited)
+        }
+        
+        coinsDeposited -= item.price
+        var newItem = item
+        newItem.count -= 1
+        inventory[name] = newItem
+        
+        print("Dispensing \(name)")
+        
+        
+    }
+}
