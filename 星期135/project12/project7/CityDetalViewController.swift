81794457eef72b80508a7b015ce292a6b82e2517
徐國堂 <roberthsu2003@gmail.com>
2022-07-13 20:51:35 +0800

    modify

--- 星期135/project12/project7/CityDetalViewController.swift
+++ 星期135/project12/project7/CityDetalViewController.swift
@@ -18,7 +18,52 @@ class CityDetailViewController: UITableViewController {
         cityImageView.image = UIImage(named: city.image)
         
     }
+}
+
+extension CityDetailViewController{
+    //UITableViewDataSource
+    override func tableView(_ tableView: UITableView,
+                   numberOfRowsInSection section: Int) -> Int{
+        return 4;
+    }
     
-
-
+    
+    override func tableView(_ tableView: UITableView,
+                   cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CELL", for: indexPath)
+        if #available(iOS 15, *){
+            var content = cell.defaultContentConfiguration()
+            switch indexPath.row{
+            case 0:
+                content.text = city.city
+            case 1:
+                content.text = city.country
+            case 3:
+                content.text = city.continent
+                print("continent\(city.continent)")
+            case 4:
+                content.text = city.url
+                print("url\(city.url)")
+            default:
+                break
+            }
+            cell.contentConfiguration = content
+        }else{
+            switch indexPath.row{
+            case 0:
+                cell.textLabel?.text = city.city
+            case 1:
+                cell.textLabel?.text  = city.country
+            case 3:
+                cell.textLabel?.text  = city.continent
+            case 4:
+                cell.textLabel?.text  = city.url
+            default:
+                break
+            }
+        }
+        
+        return cell
+       
+    }
 }
