29e31c4588819d76cc66cc3da1bc81947e27a4d8
徐國堂 <roberthsu2003@gmail.com>
2022-07-15 21:47:34 +0800

    modify

--- 星期135/project13/project7/RatingViewController.swift
+++ 星期135/project13/project7/RatingViewController.swift
@@ -9,9 +9,24 @@ import UIKit
 
 class RatingViewController: UIViewController {
     var city:City!
+    @IBOutlet var backgroundImageView:UIImageView!
+    @IBOutlet var containerView:UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(city.city)
+        let blurEffect = UIBlurEffect(style: .dark)
+        let blurEffectView = UIVisualEffectView(effect: blurEffect)
+        blurEffectView.frame = view.bounds
+        backgroundImageView.addSubview(blurEffectView)
+        containerView.transform = CGAffineTransform(scaleX: 0.0, y: 0.0)
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        UIView.animate(withDuration: 0.3) {
+            self.containerView.transform = CGAffineTransform.identity
+        }
     }
     
 
