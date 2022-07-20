5ecda4876645e38a66b87de31032cfbd909441a1
徐國堂 <roberthsu2003@gmail.com>
2022-07-20 20:07:49 +0800

    modify

--- 星期135/lesson15/lesson15/ViewController.swift
+++ 星期135/lesson15/lesson15/ViewController.swift
@@ -7,22 +7,32 @@
 
 import UIKit
 
-class ViewController: UIViewController,UIPickerViewDataSource,UIPickerViewDelegate {
+class ViewController: UIViewController {
+    
+    var kindOfMembers = ["一般會員","銀會員","金會員","黑卡會員"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
+    
+
+
+}
+
+extension ViewController:UIPickerViewDataSource{
     func numberOfComponents(in pickerView: UIPickerView) -> Int{
-        
+        return 1
     }
     
     func pickerView(_ pickerView: UIPickerView,
                     numberOfRowsInComponent component: Int) -> Int{
-        
+        return kindOfMembers.count
     }
+}
 
-
+extension ViewController:UIPickerViewDelegate{
+    
 }
 
