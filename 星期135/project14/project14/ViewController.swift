0700e2df1481b5b226df7781e39190b12168cd6e
徐國堂 <roberthsu2003@gmail.com>
2022-07-22 20:17:11 +0800

    modify

--- 星期135/project14/project14/ViewController.swift
+++ 星期135/project14/project14/ViewController.swift
@@ -37,9 +37,29 @@ class ViewController: UICollectionViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
-        
+        layout.itemSize = CGSize(width: 180, height: 120)
     }
 
 
+}
+
+extension ViewController{
+    //UICollectionViewDataSource
+    override func collectionView(_ collectionView: UICollectionView,
+                        numberOfItemsInSection section: Int) -> Int{
+        return cities.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView,
+                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell{
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CELL", for: indexPath)
+        return cell
+    }
+}
+
+
+extension ViewController{
+    //UICollectionViewDataDelegate
+    
 }
 
