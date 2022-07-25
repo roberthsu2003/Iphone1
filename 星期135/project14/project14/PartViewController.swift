da1e1c054cd4812e8e2b85d3593459bf286166eb
徐國堂 <roberthsu2003@gmail.com>
2022-07-25 20:24:38 +0800

    modify

---
+++ 星期135/project14/project14/PartViewController.swift
@@ -0,0 +1,22 @@
+//
+//  PartViewController.swift
+//  project14
+//
+//  Created by 徐國堂 on 2022/7/25.
+//
+
+import UIKit
+
+class PartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func close(_ sender:UIButton){
+        print("user close")
+    }
+
+}
