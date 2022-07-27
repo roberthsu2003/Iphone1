ed16986705e81a01f7adcc2852540c0b8bca3984
徐國堂 <roberthsu2003@gmail.com>
2022-07-27 20:39:34 +0800

    modify

--- 星期135/lesson16/lesson16/ViewController.swift
+++ 星期135/lesson16/lesson16/ViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var documentUrl:URL!
     
     override func awakeFromNib() {
         guard let sourceUrl = Bundle.main.url(forResource: "citylist", withExtension: "plist") else{
@@ -15,8 +16,16 @@ class ViewController: UIViewController {
         }
         print(sourceUrl.path)
         
-        let documentUrl = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0].appendingPathComponent("citylist.plist")
+        documentUrl = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0].appendingPathComponent("citylist.plist")
         print(documentUrl.path)
+        
+        //copy檔案
+        if !FileManager.default.fileExists(atPath: documentUrl.path){
+            if (try? FileManager.default.copyItem(at: sourceUrl, to: documentUrl)) != nil{
+                print("copy成功")
+            }
+        }
+        
         
         
         
@@ -24,6 +33,18 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func getFile(_ sender:UIButton){
+        guard let cities_array = (try? NSArray(contentsOf: documentUrl, error: ())) else{
+            return
+        }
+        guard let cities = cities_array as? [[String:Any]] else{
+            return
+        }
+        for city in cities{
+            print(city["City"]!)
+        }
     }
 
 
