789552224adafbb72e9053cb71d2692398768204
徐國堂 <roberthsu2003@gmail.com>
2022-07-29 19:14:30 +0800

    modify

--- 星期135/lesson17/lesson17/ViewController.swift
+++ 星期135/lesson17/lesson17/ViewController.swift
@@ -7,40 +7,44 @@
 
 import UIKit
 
+func copyFileToDocuments(){
+    guard let sourceUrl = Bundle.main.url(forResource: "citylist", withExtension: "plist") else{
+        return
+    }
+    
+    guard let dbUrl = Bundle.main.url(forResource: "city", withExtension: "db") else{
+        return
+    }
+    
+    let documentUrl = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0].appendingPathComponent("citylist.plist")
+    print(documentUrl.path)
+    
+    let db_documentUrl = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0].appendingPathComponent("city.db")
+    print(documentUrl.path)
+    
+    //沒有檔案才copy檔案
+    if !FileManager.default.fileExists(atPath: documentUrl.path){
+        if (try? FileManager.default.copyItem(at: sourceUrl, to: documentUrl)) != nil{
+            print("plist copy 成功")
+        }
+    }
+    
+    if !FileManager.default.fileExists(atPath: db_documentUrl.path){
+        if (try? FileManager.default.copyItem(at: dbUrl, to: db_documentUrl)) != nil{
+            print("db copy 成功")
+        }
+    }
+}
+
+var name = "Robert Hsu"
+
 class ViewController: UIViewController {
     
     override func awakeFromNib() {
-        guard let sourceUrl = Bundle.main.url(forResource: "citylist", withExtension: "plist") else{
-            return
-        }
-        
-        guard let dbUrl = Bundle.main.url(forResource: "city", withExtension: "db") else{
-            return
-        }
-        
-        let documentUrl = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0].appendingPathComponent("citylist.plist")
-        print(documentUrl.path)
-        
-        let db_documentUrl = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0].appendingPathComponent("city.db")
-        print(documentUrl.path)
-        
-        //沒有檔案才copy檔案
-        if !FileManager.default.fileExists(atPath: documentUrl.path){
-            if (try? FileManager.default.copyItem(at: sourceUrl, to: documentUrl)) != nil{
-                print("plist copy 成功")
-            }
-        }
-        
-        if !FileManager.default.fileExists(atPath: db_documentUrl.path){
-            if (try? FileManager.default.copyItem(at: dbUrl, to: db_documentUrl)) != nil{
-                print("db copy 成功")
-            }
-        }
-        
-        
-        
-        
+        copyFileToDocuments()
+        print("name:\(name)")
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
