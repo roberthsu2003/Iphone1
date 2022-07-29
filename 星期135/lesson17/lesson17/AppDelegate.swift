102206d027c82067ebb984fa028369f2967b9c62
徐國堂 <roberthsu2003@gmail.com>
2022-07-29 19:34:24 +0800

    modify

--- 星期135/lesson17/lesson17/AppDelegate.swift
+++ 星期135/lesson17/lesson17/AppDelegate.swift
@@ -10,10 +10,10 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        //copy plist 和 db至sandbox內
+        DataSource.copyFileToDocuments()
         return true
     }
 
--- 星期135/lesson17/lesson17/ViewController.swift
+++ 星期135/lesson17/lesson17/ViewController.swift
@@ -9,13 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    override func awakeFromNib() {
-        DataSource.copyFileToDocuments()        
-    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
