fae77f3d679ffe974e853dc63a546d7d7da28ce9
徐國堂 <roberthsu2003@gmail.com>
2022-08-01 21:18:10 +0800

    modify

--- 星期135/lesson17/lesson17/CityTableViewController.swift
+++ 星期135/lesson17/lesson17/CityTableViewController.swift
@@ -9,85 +9,50 @@ import UIKit
 
 class CityTableViewController: UITableViewController {
     
-    var cities:[City]?
+    var cities:[City]!
+    
+    override func awakeFromNib() {
+        if let c = DataSource.singleton.getCities(){
+            self.cities = c
+        }
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        cities = DataSource.singleton.getCities()
-        guard let cityies = cities else{
-            print("建立失敗")
-            return;
-        }
-        for city in cityies{
-            print(city.city)
-        }
+        
+        
     }
        
-    // MARK: - Table view data source
+   
+}
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+extension CityTableViewController{
+    //UITableViewDataSource
+    override func tableView(_ tableView: UITableView,
+                   numberOfRowsInSection section: Int) -> Int{
+        return cities.count
     }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+    
+    override func tableView(_ tableView: UITableView,
+                   cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        let city = cities[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CELL", for: indexPath)
+        if #available(iOS 15, *){
+            var content = cell.defaultContentConfiguration()
+            content.text = city.city;
+            content.secondaryText = city.country
+            content.image = UIImage(named: city.image)
+            content.imageProperties.reservedLayoutSize = CGSize(width: 80, height: 50)
+            cell.contentConfiguration = content
+        }else{
+            cell.textLabel?.text = city.city
+            cell.detailTextLabel?.text = city.country
+            cell.imageView?.image = UIImage(named: city.image)
+        }
+        
+        return cell;
     }
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
+    
 }
