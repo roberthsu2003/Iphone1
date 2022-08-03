e5030e2dc37793f19c720a129a874926edbf2a7d
徐國堂 <roberthsu2003@gmail.com>
2022-08-03 21:24:45 +0800

    modify

--- 星期135/lesson17/lesson17/Country_CityViewController.swift
+++ 星期135/lesson17/lesson17/Country_CityViewController.swift
@@ -37,3 +37,29 @@ class Country_CityViewController: UITableViewController {
 
     
 }
+
+extension Country_CityViewController{
+    //UITableViewDataSource
+    
+    override func tableView(_ tableView: UITableView,
+                   numberOfRowsInSection section: Int) -> Int{
+        return 0
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int{
+        return 0
+    }
+    
+    override func tableView(_ tableView: UITableView,
+                   cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        
+    }
+    
+    override func tableView(_ tableView: UITableView,
+                            titleForHeaderInSection section: Int) -> String?{
+        
+    }
+    
+    
+    
+}
