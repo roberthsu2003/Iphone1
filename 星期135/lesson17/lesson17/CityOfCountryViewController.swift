a94fa630ac1564f41e5b8f60461ddff5899e231f
徐國堂 <roberthsu2003@gmail.com>
2022-08-03 20:42:24 +0800

    modify

--- 星期135/lesson17/lesson17/CityOfCountryViewController.swift
+++ 星期135/lesson17/lesson17/CityOfCountryViewController.swift
@@ -22,3 +22,34 @@ class CityOfCountryViewController: UITableViewController {
     
 
 }
+
+extension CityOfCountryViewController{
+    //UITableViewDataSource
+    override func tableView(_ tableView: UITableView,
+                   numberOfRowsInSection section: Int) -> Int{
+        return cities.count
+    }
+    
+    override func tableView(_ tableView: UITableView,
+                   cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        let city = cities[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CELL", for: indexPath)
+        if #available(iOS 15, *){
+            var content = cell.defaultContentConfiguration()
+            content.text = city.city;
+            content.secondaryText = city.country
+            content.image = UIImage(named: city.image)
+            content.imageProperties.reservedLayoutSize = CGSize(width: 80, height: 50)
+            cell.contentConfiguration = content
+        }else{
+            cell.textLabel?.text = city.city
+            cell.detailTextLabel?.text = city.country
+            cell.imageView?.image = UIImage(named: city.image)
+        }
+        
+        return cell;
+    }
+    
+    
+}
+
