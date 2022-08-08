83279ec87f112af77721998991f7e2b2f91851ba
徐國堂 <roberthsu2003@gmail.com>
2022-08-08 19:58:05 +0800

    modify

--- 星期135/lesson19/lesson19/ViewController.swift
+++ 星期135/lesson19/lesson19/ViewController.swift
@@ -11,6 +11,7 @@ struct Region:Codable{
 }
 
 class ViewController: UITableViewController {
+    var areas = [String]()
     let httpString = "https://flask-robert.herokuapp.com/youbike/"
     
     override func viewDidLoad() {
@@ -19,44 +20,51 @@ class ViewController: UITableViewController {
     }
     
     func download_areas(urlString:String){
-        guard let url = URL(string: httpString) else{
-            return
-        }
-        let downloadTask = URLSession.shared.downloadTask(with: url){
-            (saveURL:URL?, response:URLResponse?, error:Error?) in
-            guard let saveURL=saveURL,let response=response, error == nil else{
-                print("下載失敗")
+        let documentUrl = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0].appendingPathComponent("areas.plist")
+        if !FileManager.default.fileExists(atPath: documentUrl.path){
+            guard let url = URL(string: httpString) else{
                 return
             }
-            
-            guard (response as! HTTPURLResponse).statusCode == 200 else{
-                print("下載狀態不是200")
-                return
-            }
-            
-            guard let data = try? Data(contentsOf: saveURL) else{
-                print("下載的資料無法轉成Data")
-                return
-            }
-            /*
-            guard let jsonString = String(data: data, encoding: .utf8) else{
-                print("資料轉成字串出錯了")
-                return
-            }
-             */
-            
-            DispatchQueue.main.async {
-                print("下載成功")
-                let jsonDecoder = JSONDecoder()
-                guard let region = try? jsonDecoder.decode(Region.self, from: data) else{
-                    print("jsondecoder錯誤")
+            let downloadTask = URLSession.shared.downloadTask(with: url){
+                (saveURL:URL?, response:URLResponse?, error:Error?) in
+                guard let saveURL=saveURL,let response=response, error == nil else{
+                    print("下載失敗")
                     return
                 }
-                print(region.areas)
+                
+                guard (response as! HTTPURLResponse).statusCode == 200 else{
+                    print("下載狀態不是200")
+                    return
+                }
+                
+                guard let data = try? Data(contentsOf: saveURL) else{
+                    print("下載的資料無法轉成Data")
+                    return
+                }
+                /*
+                guard let jsonString = String(data: data, encoding: .utf8) else{
+                    print("資料轉成字串出錯了")
+                    return
+                }
+                 */
+                
+                DispatchQueue.main.async {
+                    print("下載成功")
+                    let jsonDecoder = JSONDecoder()
+                    guard let region = try? jsonDecoder.decode(Region.self, from: data) else{
+                        print("jsondecoder錯誤")
+                        return
+                    }
+                    print(region.areas)
+                    self.areas = region.areas
+                    //儲存為plist
+                   
+                }
+                
             }
-            
+            downloadTask.resume()
         }
-        downloadTask.resume()
+        
     }
 
 
