7ce456a101d15b28d3ce73f4479338feb92c17cb
徐國堂 <roberthsu2003@gmail.com>
2022-08-08 21:14:10 +0800

    modify

--- 星期135/lesson19/lesson19/DetailViewController.swift
+++ 星期135/lesson19/lesson19/DetailViewController.swift
@@ -13,6 +13,15 @@ class DetailViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         title = area;
+        var urlComponets = URLComponents()
+        urlComponets.scheme = "https"
+        urlComponets.host = "flask-robert.herokuapp.com"
+        urlComponets.path = "/youbike/" + area
+        guard let url = urlComponets.url else{
+            print("url編碼錯誤")
+            return
+        }
+        print(url.absoluteString)
         
     }
 
