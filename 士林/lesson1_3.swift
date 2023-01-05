5589e93e513769a8ae2886aef7278008520d7585
徐國堂 <roberthsu2003@gmail.com>
2023-01-05 13:07:36 +0800

    Update lesson1_3.swift

--- 士林/lesson1_3.swift
+++ 士林/lesson1_3.swift
@@ -12,10 +12,14 @@ let convertedNumber = Int(possibleNumber)
 if convertedNumber == nil{
 	print("輸入錯誤")
 }else{
-	print("轉換成功:" + String(convertedNumber!))
+	//print("轉換成功:" + String(convertedNumber!))
+	//字串插補
+	print("轉換成功:\(convertedNumber!)")
 }
 
 var n = 10
 var m = 20
-print("n+m=" + String(n+m))
+//print("n+m=" + String(n+m))
+//字串插補
+print("n+m=\(n+m)")
 
