9ab8099a5ad016121e07e1b48583341adb502fef
徐國堂 <roberthsu2003@gmail.com>
2023-01-10 11:11:23 +0800

    modify

--- 士林/lesson2_1.playground/Contents.swift
+++ 士林/lesson2_1.playground/Contents.swift
@@ -5,10 +5,33 @@ var n = 10  //推測
 var i:Int  = 20
 
 //5個分數
+/*
 var name="徐國堂"
 var chinese = 78
 var english = 78
 var math = 90
+ */
 
 var students = ("徐國堂", 78, 80, 90)
-students
+
+let (name,chinese,english,math) = students
+name
+chinese
+english
+math
+
+let possibleNumber = "123a"
+let convertedNumber = Int(possibleNumber)
+
+5 == 5
+5 != 5
+
+if convertedNumber != nil{
+    print("轉換成功")
+}else{
+    print("轉換失敗")
+}
+
+
+
+
