7239d7b6fb72ad3bbb410f40fa72cd93dba16708
徐國堂 <roberthsu2003@gmail.com>
2023-01-17 09:36:54 +0800

    modify

---
+++ 士林/lesson3_1.playground/Contents.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+let quotation  = """
+諾羅拉警報！上周13.9萬人腹瀉就醫　近4周爆64起群聚
+
+過年快到了，腹瀉疫情升溫中！疾病管制署今（16）日公布，國內上周因為腹瀉就診達到13.9萬人，且過去四周通報64起腹瀉群聚，都高於去年同期，群聚案件病原以諾羅病毒為主。疾管署提醒，民眾春節連假期間與親友圍爐聚餐時，務必留意手部衛生與飲食安全，如有腹瀉情形，待症狀解除至少48小時以後再恢復上班上學，以降低病毒傳播的風險。
+"""
+
+var empytString = ""
+var anotherEmptyString = String()
+
+var variableString = "Horse"
+
+variableString = variableString + "and carriage"
+
+var variableString1 = "Horse"
+variableString1 += "and carriage"
+variableString1
+
+
+var name = "robert"
+var myName = name;
+myName = "Mr. Robert"
+name
+myName
