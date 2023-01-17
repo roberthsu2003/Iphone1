5940e020996f030eea2fb38c1bcd19f4a09ea8e3
徐國堂 <roberthsu2003@gmail.com>
2023-01-17 11:13:13 +0800

    modify

---
+++ 士林/lesson3_2.playground/Contents.swift
@@ -0,0 +1,41 @@
+//建立空的Set
+
+var letter = Set<Character>()
+letter.insert("a")
+letter
+
+letter = []
+letter
+
+//var favoriteGenres:Set<String> = ["Rock", "Classical", "Hip hop"]
+
+//var favoriteGenres:Set = ["Rock", "Classical", "Hip hop"]
+
+var numbers_set:Set = [1,2,2,3,3,4,5,6,7,8,9,10]
+
+//元素數量
+numbers_set.count
+
+var favoriteGenres:Set = ["Rock", "Classical", "Hip hop"]
+favoriteGenres.insert("Jazz")
+
+//optional binding,檢查有沒有移除成功
+if let removedGenre = favoriteGenres.remove("Rock"){
+    print("移除是\(removedGenre)")
+}else{
+    print("移除失敗")
+}
+
+//取出set內所有的元素
+for item in favoriteGenres{
+    print(item)
+}
+
+//排序
+print("============")
+for item in favoriteGenres.sorted(){
+    print(item)
+}
+
+
+
