c11e5c8b00c4c1e4e6ab090c93d0087497e536f2
徐國堂 <roberthsu2003@gmail.com>
2023-01-17 11:42:26 +0800

    modify

---
+++ 士林/lesson3_3.playground/Contents.swift
@@ -0,0 +1,46 @@
+//建立空的Dictionary
+
+var namesOfIntegers:[Int:String] = [:] //明確宣告
+namesOfIntegers[16] = "sixteen"
+namesOfIntegers
+
+namesOfIntegers = [:]
+namesOfIntegers
+
+//純值建立Dictionary
+/*
+ var airports:[String:String] = [
+ "YYZ":"Toronto Pearson",
+ "DUB":"Dublin"
+ ]
+*/
+
+var airports = [
+"YYZ":"Toronto Pearson",
+"DUB":"Dublin"
+]
+airports.count
+//新增
+airports["LHR"] = "London"
+airports
+//修改
+airports["LHR"] = "London Heathrow"
+airports
+
+//updateValue()修改內容
+
+if let oldValue = airports.updateValue("Dublin Airport", forKey: "DUB"){
+    print("舊的值是\(oldValue)")
+}else{
+    print("修改錯誤")
+}
+
+//Dictionary透過key取出值,一定是傳出optional type
+
+if let airportName = airports["dUB"]{
+    print("取出的值是\(airportName)")
+}else{
+    print("沒有這個key")
+}
+
+
