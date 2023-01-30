02de04302f386a9b5a8fee2ca3428cb586e3b154
徐國堂 <roberthsu2003@gmail.com>
2023-01-30 09:34:39 +0800

    modify

---
+++ 士林/lesson4_1.playground/Contents.swift
@@ -0,0 +1,9 @@
+let names = ["Anna", "Alex", "Brian", "Jack"]
+for name in names{
+    print("Hello! \(name)!")
+}
+
+let numberOfLegs = ["spider":8,"ant":6,"cat":4]
+for (animalName, legCount) in numberOfLegs{
+    print("\(animalName)有\(legCount)隻腳!")
+}
