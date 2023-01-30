0da6e8efacbb2987ff77c6e428d451503801bad5
徐國堂 <roberthsu2003@gmail.com>
2023-01-30 12:03:49 +0800

    modify

--- 士林/lesson4_3.playground/Contents.swift
+++ 士林/lesson4_3.playground/Contents.swift
@@ -1,11 +1,25 @@
-func greet(person:[String:String]){
-    guard let name = person["name"] else{
-        return
-    }
-}
+
 
 if #available(iOS 10, macOS 10.13, *){
     print("iOS10以上,macOS 10.13")
 }else{
     print("其它版本")
 }
+//自訂function
+func greet(person:String) -> String{
+    let greet = "\(person)您好"
+    return greet
+}
+
+greet(person: "robert")
+greet(person: "alice")
+greet(person: "jenny")
+
+//自訂function
+func add(one:Int, two:Int) -> Int{
+    return one + two
+}
+
+add(one: 10, two: 50)
+add(one: 35, two: 97)
+
