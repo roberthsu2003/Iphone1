e6ff608b52fdb87b11d990b717fcbd2e7fb186d6
徐國堂 <roberthsu2003@gmail.com>
2023-01-30 10:14:59 +0800

    modify

---
+++ 士林/lesson4_2.playground/Contents.swift
@@ -0,0 +1,25 @@
+var  temperatureInFahrenheit = 30
+
+if temperatureInFahrenheit <= 32{
+    print("非常冷")
+}
+
+var  temperatureInFahrenheit1 = 40
+
+if temperatureInFahrenheit1 <= 32{
+    print("非常冷")
+}else{
+    print("不會非常冷")
+}
+
+var  temperatureInFahrenheit2 = 70
+
+if temperatureInFahrenheit2 <= 32{
+    print("非常冷")
+}else if temperatureInFahrenheit2 >= 86{
+    print("非常熱")
+}else{
+    print("溫暖")
+}
+
+
