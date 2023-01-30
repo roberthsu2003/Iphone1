b473af26981df207d165a70ef6ea13823cb25025
徐國堂 <roberthsu2003@gmail.com>
2023-01-30 09:48:19 +0800

    modify

--- 士林/lesson4_1.playground/Contents.swift
+++ 士林/lesson4_1.playground/Contents.swift
@@ -7,3 +7,28 @@ let numberOfLegs = ["spider":8,"ant":6,"cat":4]
 for (animalName, legCount) in numberOfLegs{
     print("\(animalName)有\(legCount)隻腳!")
 }
+
+for index in 1...5{
+    print("索引是\(index), *5的值是\(index*5)")
+}
+
+let base = 5;
+let power = 10;
+var answer = 1
+for _ in 1...power{
+    answer *= base;
+}
+print("\(base)的10次方是\(answer)")
+
+let minutes = 60
+for tickMark in 0..<minutes{
+    print(tickMark)
+}
+
+for tickMark in stride(from: 0, to: minutes, by: 5){
+    print(tickMark)
+}
+
+for tickMark in stride(from: 0, through: minutes, by: 5){
+    print(tickMark)
+}
