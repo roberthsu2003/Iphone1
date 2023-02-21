82d617f2d01a53b7262ac6a869762e69e24e2834
徐國堂 <roberthsu2003@gmail.com>
2023-02-21 10:52:27 +0800

    modify

--- 士林/lesson5_1.playground/Contents.swift
+++ 士林/lesson5_1.playground/Contents.swift
@@ -8,3 +8,29 @@ func greeting(person:String) -> String{
 
 greeting(person:"robert")
 greeting(person:"alice")
+
+
+func greeting(person:String,word:String) -> String{
+    return "Hello!\(person),\(word)"
+}
+
+greeting(person: "robert", word: "nice to meet you")
+
+//傳出多個值使用tuple
+func minMax(array:[Int]) -> (min:Int,max:Int){
+    var currentMin = array[0]
+    var currentMax = array[0]
+    for value in array[1..<array.count]{
+        if value < currentMin{
+            currentMin = value
+        }else if value > currentMax{
+            currentMax = value
+        }
+    }
+    
+    return (currentMin, currentMax)
+}
+
+let bounds =  minMax(array: [8, -6, 2, 109, 3, 71])
+
+print("最小值是:\(bounds.min),最大值是\(bounds.max)")
