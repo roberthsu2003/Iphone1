2b0f4459e80d6c9a2df6903364ecb8f98ee20039
徐國堂 <roberthsu2003@gmail.com>
2023-02-21 11:03:58 +0800

    modify

--- 士林/lesson5_1.playground/Contents.swift
+++ 士林/lesson5_1.playground/Contents.swift
@@ -34,3 +34,30 @@ func minMax(array:[Int]) -> (min:Int,max:Int){
 let bounds =  minMax(array: [8, -6, 2, 109, 3, 71])
 
 print("最小值是:\(bounds.min),最大值是\(bounds.max)")
+
+func minMax1(array:[Int]) -> (min:Int,max:Int)?{
+    if array.isEmpty{return nil}
+    var currentMin = array[0]
+    var currentMax = array[0]
+    for value in array[1..<array.count]{
+        if value < currentMin{
+            currentMin = value
+        }else if value > currentMax{
+            currentMax = value
+        }
+    }
+    
+    return (currentMin, currentMax)
+}
+
+if let bounds1 = minMax1(array: [8, -6, 2, 109, 3, 71]){
+    print("最小值是:\(bounds1.min),最大值是\(bounds1.max)")
+}else{
+    print("您的陣列是空的")
+}
+
+if let bounds1 = minMax1(array:[Int]()){
+    print("最小值是:\(bounds1.min),最大值是\(bounds1.max)")
+}else{
+    print("您的陣列是空的")
+}
