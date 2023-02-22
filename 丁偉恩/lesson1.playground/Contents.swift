2190a10b21ee32bdb9eb635f1d78ba5a620d53ed
徐國堂 <roberthsu2003@gmail.com>
2023-02-22 14:43:57 +0800

    modify

--- 丁偉恩/lesson1.playground/Contents.swift
+++ 丁偉恩/lesson1.playground/Contents.swift
@@ -1,4 +1,25 @@
-import UIKit
+var i = 10 //推測
+var j:Int = 20
 
-var greeting = "Hello, playground"
-print(greeting)
+
+var n:Float = 12.5 //明確宣告
+
+Float(i) + n
+
+//optional type
+let possibleNumber = "123a"
+if let convertedNumber = Int(possibleNumber){
+    print(convertedNumber)
+}else{
+    print("轉換有誤")
+}
+/*
+if convertedNumber == nil{
+    print("轉換有誤")
+}else{
+    print(convertedNumber!)
+}
+ */
+
+
+
