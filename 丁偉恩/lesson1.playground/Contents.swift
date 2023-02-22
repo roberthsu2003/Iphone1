c180d8c0379470cff69a71c72a0353500f59772c
徐國堂 <roberthsu2003@gmail.com>
2023-02-22 14:53:05 +0800

    modify

--- 丁偉恩/lesson1.playground/Contents.swift
+++ 丁偉恩/lesson1.playground/Contents.swift
@@ -21,5 +21,17 @@ if convertedNumber == nil{
 }
  */
 
+if let firstNumber = Int("4"){
+    if let secondNumber = Int("42"){
+        if firstNumber < secondNumber && secondNumber < 100{
+            print("\(firstNumber)<\(secondNumber)<100")
+        }
+    }
+}
+
+if let firstNumber = Int("4"),let secondNumber = Int("42"),firstNumber < secondNumber && secondNumber < 100{    
+    print("\(firstNumber)<\(secondNumber)<100")
+}
+
 
 
