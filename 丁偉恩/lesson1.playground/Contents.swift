c140ad432223157bd21a501bdeb9a95358674324
徐國堂 <roberthsu2003@gmail.com>
2023-02-22 15:19:49 +0800

    modify

--- 丁偉恩/lesson1.playground/Contents.swift
+++ 丁偉恩/lesson1.playground/Contents.swift
@@ -29,9 +29,36 @@ if let firstNumber = Int("4"){
     }
 }
 
-if let firstNumber = Int("4"),let secondNumber = Int("42"),firstNumber < secondNumber && secondNumber < 100{    
+if let firstNumber = Int("4"),let secondNumber = Int("42"),firstNumber < secondNumber && secondNumber < 100{
     print("\(firstNumber)<\(secondNumber)<100")
 }
+
+func greet(person:[String:String]){
+    /*
+    if let name = person["name"]{
+        print(name)
+        print("成功")
+    }else{
+        print("出錯了")
+        return
+    }
+     */
+    guard let name = person["name"] else{
+        print("name出錯了")
+        return
+    }
+    print("Hello! \(name)")
+    
+    guard let location = person["location"] else{
+        print("location出錯了")
+        return
+    }
+    
+    print("name\(name), \(location)")
+}
+
+greet(person:["name":"John"])
+greet(person:["name":"John","location":"Taipei"])
 
 
 
