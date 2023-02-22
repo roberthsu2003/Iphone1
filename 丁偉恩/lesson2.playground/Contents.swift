65be6377da486edd199ee5c166f1f008df63fa04
徐國堂 <roberthsu2003@gmail.com>
2023-02-22 16:12:57 +0800

    modify

--- 丁偉恩/lesson2.playground/Contents.swift
+++ 丁偉恩/lesson2.playground/Contents.swift
@@ -44,3 +44,13 @@ case let(x,y):
     print("x=\(x),y=\(y)")
 }
 
+let yetAnotherPoint = (1, -1)
+switch yetAnotherPoint{
+case let(x,y) where x == y:
+    print("x==y")
+case let(x,y) where x == -y:
+    print("x==-y")
+case let(x,y):
+    print("\(x),\(y)")
+}
+
