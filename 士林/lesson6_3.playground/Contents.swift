468d234b8a1298989059401ed76e256269c65a4f
徐國堂 <roberthsu2003@gmail.com>
2023-02-23 12:38:13 +0800

    modify

--- 士林/lesson6_3.playground/Contents.swift
+++ 士林/lesson6_3.playground/Contents.swift
@@ -15,10 +15,18 @@ let someVideoMode = VideoMode()
 
 someResolution.width
 someResolution.height
+//someResolution.width  = 100
 
 someVideoMode.resolution.width
 someVideoMode.resolution.height
 someVideoMode.frameRate
+
+someVideoMode.resolution.width = 1280
+
+let vga = Resolution(width: 640, height: 480)  // 智慧型成員初始化
+
+
+
 
 
 
