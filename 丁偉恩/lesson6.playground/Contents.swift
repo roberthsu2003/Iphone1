c30d82ebceefebf0ea6afbee84c7d9d0d8e397c3
徐國堂 <roberthsu2003@gmail.com>
2023-03-03 16:37:48 +0800

    modify

--- 丁偉恩/lesson6.playground/Contents.swift
+++ 丁偉恩/lesson6.playground/Contents.swift
@@ -21,3 +21,38 @@ let manager = DataManager()
 manager.data.append("some data")
 manager.data.append("some more data")
 manager.importer.filename
+
+
+struct Point{
+    var x = 0.0, y = 0.0
+}
+
+struct Size{
+    var width = 0.0, height = 0.0
+}
+
+struct Rect{
+    var origin = Point()
+    var size = Size()
+    
+    //computed property
+    var center:Point{
+        get{
+            let centerX = origin.x + (size.width / 2)
+            let centerY = origin.y + (size.height / 2)
+            return Point(x: centerX, y: centerY)
+        }
+        
+        set(newCenter){
+            origin.x = newCenter.x - (size.width / 2)
+            origin.y = newCenter.y - (size.height / 2)
+        }
+    }
+}
+
+var square = Rect(origin: Point(x: 0.0, y: 0.0), size: Size(width: 10.0, height: 10.0))
+
+var initialSquareCenter = square.center
+
+square.center = Point(x: 15.0, y: 15.0)
+square.origin
