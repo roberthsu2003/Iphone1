b569a00e3ad7660edcc278cfb7a4a45477e78333
徐國堂 <roberthsu2003@gmail.com>
2023-03-03 16:20:58 +0800

    modify

---
+++ 丁偉恩/lesson6.playground/Contents.swift
@@ -0,0 +1,23 @@
+struct FixedLengthRange{
+    var firstValue:Int
+    let length:Int
+}
+
+var rangeOfThreeItems = FixedLengthRange(firstValue: 0, length: 3)
+rangeOfThreeItems.firstValue = 6
+
+//lazy stored Property
+
+class DataImporter{
+    var filename = "data.txt"
+}
+
+class DataManager{
+    lazy var importer = DataImporter()
+    var data:[String] = []
+}
+
+let manager = DataManager()
+manager.data.append("some data")
+manager.data.append("some more data")
+manager.importer.filename
