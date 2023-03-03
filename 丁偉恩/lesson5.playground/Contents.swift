ef47236edac999a670d2060b31daf4803fd90f26
徐國堂 <roberthsu2003@gmail.com>
2023-03-03 15:40:28 +0800

    modify

--- 丁偉恩/lesson5.playground/Contents.swift
+++ 丁偉恩/lesson5.playground/Contents.swift
@@ -19,3 +19,28 @@ someVideoMode.resolution.width
 someVideoMode.resolution.width = 1280
 
 //someResolution.width = 1280
+
+//struct memberwise initializor
+Resolution(width: 640, height: 480)
+
+
+//enumerator and struct are value type
+
+let hd = Resolution(width: 1920, height: 1080)
+var cinema = hd
+
+cinema.width = 2048
+
+hd.width
+
+
+enum CompasssPoint{
+    case north, south, east, west
+    mutating func turnNorth(){
+        self = .north
+    }
+}
+
+var currentDirection = CompasssPoint.west
+currentDirection.turnNorth()
+currentDirection
