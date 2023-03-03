ffda9298fd955725ccc92e2f802296895d8761c7
徐國堂 <roberthsu2003@gmail.com>
2023-03-03 15:27:12 +0800

    modify

---
+++ 丁偉恩/lesson5.playground/Contents.swift
@@ -0,0 +1,21 @@
+struct Resolution{
+    var width = 0
+    var height = 0
+}
+
+class VideoMode{
+    var resolution = Resolution()
+    var interlaced = false
+    var frameRate = 0.0
+    var name:String?
+}
+
+let someResolution = Resolution()
+let someVideoMode = VideoMode()
+
+someResolution.width
+
+someVideoMode.resolution.width
+someVideoMode.resolution.width = 1280
+
+//someResolution.width = 1280
