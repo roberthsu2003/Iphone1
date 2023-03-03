0768f9a5e29003235aa8ada8f085e0a53328a41a
徐國堂 <roberthsu2003@gmail.com>
2023-03-03 14:35:07 +0800

    modify

--- 丁偉恩/lesson3.playground/Contents.swift
+++ 丁偉恩/lesson3.playground/Contents.swift
@@ -44,3 +44,32 @@ numbers.map{
     return "Hello!"
 }
 
+func makeIncrementer(forIncrement amount:Int) -> () -> Int{
+    var runningTotal = 0;
+    func incrementer() -> Int{
+        runningTotal += amount
+        return runningTotal
+    }
+    return incrementer
+}
+
+let incrementByTen = makeIncrementer(forIncrement: 10)
+incrementByTen()
+
+incrementByTen()
+
+incrementByTen()
+
+let incrementBySeven = makeIncrementer(forIncrement: 7)
+
+incrementBySeven()
+
+incrementBySeven()
+
+incrementBySeven()
+
+let alsoIncrementByTen = incrementByTen
+
+alsoIncrementByTen()
+alsoIncrementByTen()
+
