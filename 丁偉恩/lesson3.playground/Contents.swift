4bb4a92f08e6fce17b2d02b0ab935e75b64f1832
徐國堂 <roberthsu2003@gmail.com>
2023-03-03 14:10:04 +0800

    modify

---
+++ 丁偉恩/lesson3.playground/Contents.swift
@@ -0,0 +1,17 @@
+let names = ["Chris", "Alex", "Ewa", "Barry", "Daniella"]
+names.sorted(by: {
+    (s1:String, s2:String) -> Bool in
+    return s1 < s2
+})
+
+//依內容推測型別
+names.sorted(by: {
+    s1, s2  in
+    return s1 < s2
+})
+
+//依內容只有一行,會自動return
+names.sorted(by: {s1, s2  in s1 < s2})
+
+//使用$0,$1取代參數名稱
+names.sorted(by:<)
