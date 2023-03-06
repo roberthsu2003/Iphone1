48ab37734c24ae2567b0e7963886194c8ea11287
徐國堂 <roberthsu2003@gmail.com>
2023-03-06 10:52:28 +0800

    modify

--- 士林/lesson8_2.playground/Contents.swift
+++ 士林/lesson8_2.playground/Contents.swift
@@ -20,3 +20,46 @@ let counter = Counter()
 counter.increment()
 counter.increment(by: 5)
 counter.reset()
+
+//實體的method
+struct Point{
+    var x = 0.0, y = 0.0
+    
+    func isToTheRightOf(x:Double) -> Bool{
+        return self.x > x
+    }
+}
+
+let somePoint = Point(x: 4.0, y: 5.0)
+somePoint.isToTheRightOf(x: 1.0)
+
+//mutating function
+struct Point1{
+    var x = 0.0, y = 0.0
+    mutating func moveBy(x deltaX:Double, y deltaY:Double){
+        x += deltaX
+        y += deltaY
+    }
+}
+
+enum TriStateSwitch{
+    case off, low, high
+    mutating func next(){
+        switch self{
+        case .off:
+            self = .low
+        case .low:
+            self = .high
+        case .high:
+            self = .off
+        }
+    }
+    
+}
+
+var ovenLight = TriStateSwitch.low
+ovenLight.next()
+ovenLight.next()
+ovenLight.next()
+
+
