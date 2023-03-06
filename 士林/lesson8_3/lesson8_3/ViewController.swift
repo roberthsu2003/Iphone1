02db9c72b785f2141f86689bdc659ce5c0b3e9c6
徐國堂 <roberthsu2003@gmail.com>
2023-03-06 12:29:21 +0800

    modify

--- 士林/lesson8_3/lesson8_3/ViewController.swift
+++ 士林/lesson8_3/lesson8_3/ViewController.swift
@@ -19,6 +19,21 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         myLabel.text = "這是程式控制的文字"
         myLabel.textAlignment = NSTextAlignment.center
+        //保證一定有值,使用force unwrapping
+        let button1_titleLabel = button1.titleLabel
+        button1_titleLabel!.text = "左開關"
+         
+        //使用optional binding 同時檢查,同時force unwrapping
+        if let button2_titleLabel = button2.titleLabel{
+            button2_titleLabel.text = "中間開關"
+        }
+        
+        //使用guard else        
+        guard let button3_titleLabel = button3.titleLabel else{
+            return
+        }
+        button3_titleLabel.text = "右邊開關"
+        
     }
 
 
