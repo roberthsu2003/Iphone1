e38c4a38c1e39cdd6141b4aed95276328099e161
徐國堂 <roberthsu2003@gmail.com>
2023-03-06 12:13:37 +0800

    modify

--- 士林/lesson8_3/lesson8_3/ViewController.swift
+++ 士林/lesson8_3/lesson8_3/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet var myLabel:UILabel!
+    @IBOutlet var button1:UIButton!
+    @IBOutlet var button2:UIButton!
+    @IBOutlet var button3:UIButton!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        myLabel.text = "這是程式產生的文字"
+        myLabel.text = "這是程式控制的文字"
+        myLabel.textAlignment = NSTextAlignment.center
     }
 
 
