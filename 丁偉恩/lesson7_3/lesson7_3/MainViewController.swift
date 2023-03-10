91fdb6e3e2eec0a52003a7bb2737eb2add4edc4c
徐國堂 <roberthsu2003@gmail.com>
2023-03-10 15:04:41 +0800

    modify

---
+++ 丁偉恩/lesson7_3/lesson7_3/MainViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MainViewController.swift
+//  lesson7_3
+//
+//  Created by 徐國堂 on 2023/3/10.
+//
+
+import UIKit
+
+class MainViewController:UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("ViewDidLoad")
+    }
+}
