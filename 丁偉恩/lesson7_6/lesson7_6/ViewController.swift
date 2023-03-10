1f694c0c1837662ffc518cfd885c9c6bcae3fc5d
徐國堂 <roberthsu2003@gmail.com>
2023-03-10 17:00:13 +0800

    modify

---
+++ 丁偉恩/lesson7_6/lesson7_6/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  lesson7_6
+//
+//  Created by 徐國堂 on 2023/3/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var myLabel:UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        myLabel.backgroundColor = UIColor.blue
+        
+    }
+
+
+}
+
