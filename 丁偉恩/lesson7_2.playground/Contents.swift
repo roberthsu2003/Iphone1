f98c56da156bb80e28c1bf5ab35d93a22aa2a0d7
徐國堂 <roberthsu2003@gmail.com>
2023-03-10 14:33:09 +0800

    modify

--- 丁偉恩/lesson7_2.playground/Contents.swift
+++ 丁偉恩/lesson7_2.playground/Contents.swift
@@ -24,3 +24,45 @@ counter.count
 counter.reset()
 counter.count
 
+struct Point{
+    var x=0.0, y=0.0
+    func isToTheRightOf(x:Double)->Bool{
+        return self.x > x
+    }
+}
+
+struct Point1{
+    var x=0.0, y=0.0
+    mutating func moveBy(x deltaX:Double, y deltaY:Double){
+        x += deltaX
+        y += deltaY
+    }
+}
+
+struct Point2{
+    var x=0.0, y=0.0
+    mutating func moveBy(x deltaX:Double, y deltaY:Double){
+        self = Point2(x: x+deltaX, y: y+deltaY)
+    }
+}
+
+enum TriStateSwitch{
+    case off, low, high
+    
+    mutating func next(){
+        switch(self){
+        case .off:
+            self = .low
+        case .low:
+            self = .high
+        case .high:
+            self = .off
+        }
+    }
+    
+}
+
+var ovenLight = TriStateSwitch.low
+ovenLight.next()
+ovenLight.next()
+ovenLight.next()
