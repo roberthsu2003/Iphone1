0d778567184f2a2baafe3562a4efd163fb143b0c
徐國堂 <roberthsu2003@gmail.com>
2023-03-15 14:17:58 +0800

    modify

--- 丁偉恩/lesson8_1.playground/Contents.swift
+++ 丁偉恩/lesson8_1.playground/Contents.swift
@@ -12,3 +12,14 @@ struct TimesTable{
 
 let threeTimesTable = TimesTable(multiplier: 3)
 threeTimesTable[5]
+
+
+enum Planet:Int{
+    case mercury=1,venus, earth, mars, jupiter, saturn, uranus, neptune
+    static subscript(n:Int) -> Planet{
+        return Planet(rawValue: n)!
+    }
+}
+
+let planet = Planet[4]
+planet
