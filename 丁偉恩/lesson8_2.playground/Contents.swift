71bf06b8411316bbc5eaa3082203ec367de5e7a4
徐國堂 <roberthsu2003@gmail.com>
2023-03-15 14:45:44 +0800

    modify

--- 丁偉恩/lesson8_2.playground/Contents.swift
+++ 丁偉恩/lesson8_2.playground/Contents.swift
@@ -34,4 +34,38 @@ tandem.currentSpeed = 22.0
 tandem.description
 
 
+//override method
+class Train:Vehicle{
+    override func makeNoise(){
+        print("Choo Choo")
+    }
+}
+
+let train = Train()
+train.makeNoise()
+
+//override properties
+class Car:Vehicle{
+    var gear = 1
+    override var description:String{
+        return super.description + "檔速為\(gear)"
+    }
+}
+
+let car = Car()
+car.currentSpeed = 25.0
+car.gear = 3
+car.description
+
+class AutomaticCar:Car{
+    override var currentSpeed:Double{
+        didSet{
+            gear = Int(currentSpeed / 10.0) + 1
+        }
+    }
+}
+
+let automatic = AutomaticCar()
+automatic.currentSpeed = 35.0
+automatic.description
 
