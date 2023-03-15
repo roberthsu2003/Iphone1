181ebefd9404e69a865301cbce640244a427817d
徐國堂 <roberthsu2003@gmail.com>
2023-03-15 15:13:02 +0800

    modify

---
+++ 丁偉恩/lesson8_3/lesson8_3/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  lesson8_3
+//
+//  Created by 徐國堂 on 2023/3/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var myButton:UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        myButton.addTarget(self, action: #selector(userClick(_:)), for: UIControl.Event.touchUpInside)
+        
+    }
+    
+    @objc func userClick(_ sender:UIButton){
+        print("USER Click")
+    }
+
+
+}
+
