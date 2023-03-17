3c8886244a05d76f60f64028497e47dd5f629366
徐國堂 <roberthsu2003@gmail.com>
2023-03-17 15:24:24 +0800

    modify

--- 丁偉恩/lesson9_3.playground/Contents.swift
+++ 丁偉恩/lesson9_3.playground/Contents.swift
@@ -19,5 +19,41 @@ class Bicycle:Vehicle{
 let bicycle = Bicycle()
 bicycle.description
 
+class Hoverboard:Vehicle{
+    var color:String
+    init(color:String){
+        self.color = color
+        super.init()
+    }
+    override var description: String{
+        return "\(super.description),顏色是\(color)"
+    }
+}
+
+let hoverboard = Hoverboard(color: "silver")
+hoverboard.description
+
+//自動繼承
+//如果沒有自訂的init(),全部繼承父類別的
+//如果自訂的init(),有所有父類別的主要的init(),次要的init會自動繼承
+//如果自訂的init(),和所有父類別的主要的init()不同,不會繼承
+
+class Food{
+    var name:String
+    init(name:String){
+        self.name = name
+    }
+    convenience init(){
+        self.init(name:"沒有名字")
+    }
+}
+
+let namedMeat = Food(name: "Bacon")
+let mysteryMeat = Food()
+
+
+
+
+
 
 
