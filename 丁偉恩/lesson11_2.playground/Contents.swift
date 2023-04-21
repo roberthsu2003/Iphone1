afa07701fb37f7952a88734614628baeb5aa0870
roberthsu2003 <roberthsu2003@gmail.com>
2023-04-21 15:10:17 +0800

    modify

---
+++ 丁偉恩/lesson11_2.playground/Contents.swift
@@ -0,0 +1,45 @@
+class MediaItem{
+    var name:String
+    init(name:String){
+        self.name = name
+    }
+}
+
+class Movie:MediaItem{
+    var director:String
+    init(name:String, director:String){
+        self.director = director
+        super.init(name: name)
+    }
+}
+
+class Song:MediaItem{
+    var artist:String
+    init(name:String, artist:String){
+        self.artist = artist
+        super.init(name: name)
+    }
+}
+
+let library = [
+    Movie(name: "Casablanca", director: "Michael Curtiz"),
+    Song(name: "Blue Suede Shoes", artist: "Elvis Presley"),
+    Movie(name: "Citizen Kane", director: "Orson Welles"),
+    Song(name: "The One And Only", artist: "Chesney Hawkes"),
+    Song(name: "Never Gonna Give You Up", artist: "Rick Astley")
+    ]
+
+var movieCount = 0
+var songCount = 0
+
+for item in library{
+    if item is Movie{
+        movieCount += 1
+    }else if item is Song{
+        songCount += 1
+    }
+}
+
+movieCount
+songCount
+    
