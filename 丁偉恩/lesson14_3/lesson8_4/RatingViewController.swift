b387a5b19ca0a1667b6c97616132334a5563b7a9
roberthsu2003 <roberthsu2003@gmail.com>
2023-05-19 16:19:47 +0800

    modify

--- 丁偉恩/lesson14_3/lesson8_4/RatingViewController.swift
+++ 丁偉恩/lesson14_3/lesson8_4/RatingViewController.swift
@@ -9,16 +9,20 @@ import UIKit
 
 class RatingViewController: UIViewController {
     @IBOutlet var backgroundImageView:UIImageView!
+    @IBOutlet var containerImageView:UIImageView!
     var city:City!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         backgroundImageView.image = UIImage(named: city.image)
+        containerImageView.image = UIImage(named: city.image)
         let blurEffect = UIBlurEffect(style: .dark)
         let blurEffectView = UIVisualEffectView(effect: blurEffect)
-        blurEffectView.frame = backgroundImageView.bounds
+        blurEffectView.frame = view.bounds
         backgroundImageView.addSubview(blurEffectView)
     }
+    
+    
     
 }
