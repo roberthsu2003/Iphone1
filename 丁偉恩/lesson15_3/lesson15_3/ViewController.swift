40737cbecdd3466074ec9e52c79aebd29d9d1c48
roberthsu2003 <roberthsu2003@gmail.com>
2023-06-04 14:33:24 +0800

    modify

--- 丁偉恩/lesson15_3/lesson15_3/ViewController.swift
+++ 丁偉恩/lesson15_3/lesson15_3/ViewController.swift
@@ -14,9 +14,32 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         let urlRequest = URLRequest(url: URL(string: url)!)
-        let downloadTask = urlSession.downloadTask(with: urlRequest) { (saveURL:URL?, response:URLResponse?, erro:Error?) in
+        let downloadTask = urlSession.downloadTask(with: urlRequest) { (saveURL:URL?, response:URLResponse?, error:Error?) in
+            guard let saveURL = saveURL, let response = response, error == nil else{
+                print("下載失敗")
+                return
+            }
             
+            guard (response as! HTTPURLResponse).statusCode == 200 else{
+                print("狀態不是200")
+                return
+            }
+            
+            guard let data = try? Data(contentsOf: saveURL) else{
+                print("下載完的資料沒有辦法轉出")
+                return
+            }
+            print(data)
+            let str = String(decoding: data, as: UTF8.self)
+            
+            print(str)
+            
+            
+            
+            
+          
         }
+        downloadTask.resume()
     }
 
 
