5c2321b1ccf6a59ec0b4450039c90c1c3eee9082
roberthsu2003 <roberthsu2003@gmail.com>
2023-06-04 10:42:57 +0800

    modify

---
+++ 丁偉恩/lesson15_1/lesson8_4/RatingViewController.swift
@@ -0,0 +1,43 @@
+//
+//  RatingViewController.swift
+//  lesson13_1
+//
+//  Created by 徐國堂 on 2023/5/12.
+//
+
+import UIKit
+
+class RatingViewController: UIViewController {
+    @IBOutlet var backgroundImageView:UIImageView!
+    @IBOutlet var containerImageView:UIImageView!
+    @IBOutlet var askLabel:UILabel!
+    @IBOutlet var containerView:UIView!
+    var city:City!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        backgroundImageView.image = UIImage(named: city.image)
+        containerImageView.image = UIImage(named: city.image)
+        let blurEffect = UIBlurEffect(style: .dark)
+        let blurEffectView = UIVisualEffectView(effect: blurEffect)
+        blurEffectView.frame = view.bounds
+        backgroundImageView.addSubview(blurEffectView)
+        askLabel.text = "您曾經來過這裏嗎?\n給這個城市一個描素吧?"
+        containerView.transform = CGAffineTransform(scaleX: 0, y: 0)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        UIView.animate(withDuration: 0.3) {
+            self.containerView.transform = CGAffineTransform.identity
+        }
+    }
+    
+    @IBAction func userClick(_ sender:UIButton){
+        print(sender)
+    }
+    
+    
+    
+}
