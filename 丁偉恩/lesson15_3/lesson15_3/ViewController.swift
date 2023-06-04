58897d84f205a1bc539f2b1862de2576e9dc45bc
roberthsu2003 <roberthsu2003@gmail.com>
2023-06-04 12:30:56 +0800

    modify

--- 丁偉恩/lesson15_3/lesson15_3/ViewController.swift
+++ 丁偉恩/lesson15_3/lesson15_3/ViewController.swift
@@ -8,10 +8,15 @@
 import UIKit
 
 class ViewController: UITableViewController {
-
+    var url = "https://youbike-json.onrender.com/youbike"
+    var urlSession = URLSession.shared
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let urlRequest = URLRequest(url: URL(string: url)!)
+        let downloadTask = urlSession.downloadTask(with: urlRequest) { (saveURL:URL?, response:URLResponse?, erro:Error?) in
+            
+        }
     }
 
 
