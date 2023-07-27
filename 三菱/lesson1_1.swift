8118ef620788a15a68c0993397bf1fbf2bd1df9f
徐國堂 <roberthsu2003@gmail.com>
2023-07-27 14:17:31 +0800

    Create lesson1_1.swift

---
+++ 三菱/lesson1_1.swift
@@ -0,0 +1,25 @@
+var myVariable = 42
+myVariable = 50
+print(myVariable)
+let myConstant = 42
+//myConstant = 50 常數無法給值
+print(myConstant)
+
+//不明確的宣告
+let implicitInteger = 70
+let implicitDouble = 70.0
+
+//明確的宣告
+let explicitDouble:Double = 70
+
+print(explicitDouble)
+
+//字串插補
+print("I have 3 apples.")
+
+let apples = 3
+let oranges = 5
+print("I have \(apples) apples")
+print("I have \(apples + oranges) pieces of fruit.")
+
+
