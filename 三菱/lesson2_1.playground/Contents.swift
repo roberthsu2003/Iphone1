ac1b192eae7c598ceaf2f154dfa497b234f9e862
roberthsu2003 <roberthsu2003@gmail.com>
2023-08-01 14:01:43 +0800

    modify

--- 三菱/lesson2_1.playground/Contents.swift
+++ 三菱/lesson2_1.playground/Contents.swift
@@ -1,10 +1,17 @@
 var greeting = "Hello, playground"
-let possibleNumber = "123a"
+let possibleNumber = "123"
 let convertedNumber = Int(possibleNumber)
 //print(convertedNumber!)
 //使用if檢查
 if convertedNumber != nil{
     print(convertedNumber!)
+}else{
+    print("資料為nil,轉換錯誤")
+}
+
+//使用optional binding檢查
+if let actualNumber = Int(possibleNumber){
+    print(actualNumber)
 }else{
     print("資料為nil,轉換錯誤")
 }
