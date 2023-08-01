de1150267b98082e0b13f11fb6137f316e9734aa
roberthsu2003 <roberthsu2003@gmail.com>
2023-08-01 15:36:42 +0800

    modify

--- 三菱/lesson2_4.playground/Contents.swift
+++ 三菱/lesson2_4.playground/Contents.swift
@@ -15,3 +15,21 @@ if name == "world"{
 
 (1, "zebra") < (2, "apple")
 (3, "apple") < (3, "bird")
+
+/*
+let contentHeight = 40
+let hasHeader = true
+let rowHeight:Int
+if hasHeader{
+     rowHeight = contentHeight + 50
+}else{
+     rowHeight = contentHeight + 20
+}*/
+
+//三元運算式(? :)
+let contentHeight = 40
+let hasHeader = false
+let rowHeight = contentHeight + (hasHeader ? 50 : 20)
+rowHeight
+
+
