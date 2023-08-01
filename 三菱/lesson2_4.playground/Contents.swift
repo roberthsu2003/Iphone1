da264533332681087ea8cb29bcc53d13f811e260
roberthsu2003 <roberthsu2003@gmail.com>
2023-08-01 15:26:51 +0800

    modify

---
+++ 三菱/lesson2_4.playground/Contents.swift
@@ -0,0 +1,17 @@
+//comparison operator(比較運算子)
+1 == 1.0
+2 != 1
+2 > 1
+1 < 2
+1 >= 1
+2 <= 1
+
+let name = "World"
+if name == "world"{
+    print("Hello, world")
+}else{
+    print("不相等")
+}
+
+(1, "zebra") < (2, "apple")
+(3, "apple") < (3, "bird")
