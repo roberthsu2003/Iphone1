d3d819a0f84060ed201a05307bddc4d4f66ac510
roberthsu2003 <roberthsu2003@gmail.com>
2023-08-01 14:10:56 +0800

    modify

--- 三菱/lesson2_1.playground/Contents.swift
+++ 三菱/lesson2_1.playground/Contents.swift
@@ -15,3 +15,16 @@ if let actualNumber = Int(possibleNumber){
 }else{
     print("資料為nil,轉換錯誤")
 }
+
+if let firstNumber = Int("4"){
+    if let secondNumber = Int("42"){
+        if firstNumber < secondNumber && secondNumber < 100{
+            print("\(firstNumber) < \(secondNumber) < 100")
+        }
+    }
+}
+
+if let firstNumber = Int("4"),let secondNumber = Int("42"), firstNumber < secondNumber && secondNumber < 100 {
+    print("\(firstNumber) < \(secondNumber) < 100")
+}
+
