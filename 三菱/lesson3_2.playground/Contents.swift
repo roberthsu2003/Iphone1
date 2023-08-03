828fc7e8d5a2d7a2dd587f31af7cdcfd114fdb8f
roberthsu2003 <roberthsu2003@gmail.com>
2023-08-03 14:45:09 +0800

    modify

--- 三菱/lesson3_2.playground/Contents.swift
+++ 三菱/lesson3_2.playground/Contents.swift
@@ -4,3 +4,14 @@ greeting[greeting.index(before: greeting.endIndex)]
 
 let index = greeting.index(greeting.startIndex, offsetBy: 7)
 greeting[index]
+
+for index in greeting.indices{
+    print("\(greeting[index])",terminator: "-")
+}
+
+//inserting and removing
+
+var welcome = "hello"
+welcome.insert("!", at: welcome.endIndex)
+
+welcome.insert(contentsOf: " there", at: welcome.endIndex)
