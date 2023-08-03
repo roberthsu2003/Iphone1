bb525d8928ed940dc1cd22e21d5eabbc5ce20a22
roberthsu2003 <roberthsu2003@gmail.com>
2023-08-03 13:38:03 +0800

    modify

---
+++ 三菱/lesson3_1.playground/Contents.swift
@@ -0,0 +1,10 @@
+let someString = "Some string literal value"
+let quotation = """
+The White Rabbit put on his spectacles.  "Where shall I begin,
+please your Majesty?" he asked.
+
+"Begin at the beginning," the King said gravely, "and go on
+till you come to the end; then stop.
+"""
+print(quotation)
+
