bd03f1d13289ee3d5e3c883f8f5acfb5cb8019e0
roberthsu2003 <roberthsu2003@gmail.com>
2023-08-03 15:56:07 +0800

    modify

--- 三菱/lesson3_4.playground/Contents.swift
+++ 三菱/lesson3_4.playground/Contents.swift
@@ -5,3 +5,19 @@ var someInts:[Int] = []
 //var someInts:Array<Int> = []
 
 print("someInts的元素數量\(someInts.count)")
+
+//append()實體方法
+someInts.append(3)
+
+//全部清空
+someInts = []
+
+//建立Array,同時有預設的元素值
+var threeDoubles = Array(repeating: 0.0, count: 3)
+threeDoubles
+
+var anotherThreeDouble = Array(repeating: 2.5, count: 3)
+anotherThreeDouble
+
+var sixDouble = threeDoubles + anotherThreeDouble
+sixDouble
