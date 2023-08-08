2ce6aa325fb31777ba11d8c126789189cd715192
徐國堂 <roberthsu2003@gmail.com>
2023-08-08 16:25:48 +0800

    modify

--- 三菱/lesson4_7.playground/Contents.swift
+++ 三菱/lesson4_7.playground/Contents.swift
@@ -17,3 +17,20 @@ case "z":
 default:
     print("其它字元")
 }
+
+//case後可以使用Range()
+let approximateCount = 0
+switch approximateCount{
+case 0:
+    print("0")
+case 1..<5:
+    print("1~4")
+case 5..<12:
+    print("5~11")
+case 12..<100:
+    print("12~99")
+case 100..<1000:
+    print("100~999")
+default:
+    print(">=1000")
+}
