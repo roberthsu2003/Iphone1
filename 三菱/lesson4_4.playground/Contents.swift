c9d546158ddaa32242397072830aa52ca3542a62
徐國堂 <roberthsu2003@gmail.com>
2023-08-08 15:44:02 +0800

    modify

---
+++ 三菱/lesson4_4.playground/Contents.swift
@@ -0,0 +1,22 @@
+//while
+//不明確知道執行的次數
+let finalSquare = 25
+var board = Array(repeating: 0, count: finalSquare + 1)
+board[03] = +08;board[06] = +11; board[09] = +09; board[10] = +02
+board[14] = -10; board[19] = -11; board[22] = -02; board[24] = -08
+
+var square = 0;
+var diceRoll = 0;
+
+while square < finalSquare{
+    /*
+    diceRoll += 1
+    if diceRoll == 7 { diceRoll = 1}
+     */
+    diceRoll = Int.random(in: 1...6)
+    square += diceRoll
+    if square < board.count{
+        square += board[square]
+    }
+}
+print("Game over!")
