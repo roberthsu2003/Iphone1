670fca69e60a5ac128a17045e3c5c208e891eddd
徐國堂 <roberthsu2003@gmail.com>
2023-08-08 15:19:46 +0800

    modify

--- 三菱/lesson4_3.playground/Contents.swift
+++ 三菱/lesson4_3.playground/Contents.swift
@@ -23,3 +23,18 @@ for _ in 1...power{
 }
 
 print("\(base)的\(power)次方是\(answer)")
+
+let minutes = 60
+for tickMark in 0..<minutes{
+    print(tickMark)
+}
+
+let minuteInterval = 5
+for tickMark in stride(from: 0, to: minutes, by: minuteInterval){
+    print(tickMark)
+}
+
+for tickMark in stride(from: 0, through: minutes, by: minuteInterval){
+    print(tickMark)
+}
+        
