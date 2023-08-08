7c70231ff1f3382da19a14f1148988ba557c392b
徐國堂 <roberthsu2003@gmail.com>
2023-08-08 14:38:10 +0800

    modify

--- 三菱/lesson4_2.playground/Contents.swift
+++ 三菱/lesson4_2.playground/Contents.swift
@@ -13,3 +13,26 @@ var airports:[String:String] = [
                                 "DUB":"Dublin"
                                 ]
 print(airports)
+airports.count
+airports.isEmpty
+
+//新增元素
+airports["LHR"] = "London"
+//更新
+airports["LHR"] = "London Heathrow"
+airports
+
+//更新使用updataeValue
+if let oldValue = airports.updateValue("Dublin Airport", forKey: "DUB"){
+    print("更新成功,原來的值是\(oldValue)")
+}
+
+//重要的,取出值傳出的是optional type
+if let airportName = airports["DUB"]{
+    print("DUB機場名稱\(airportName)")
+}
+
+airports["APL"] = "Apple International"
+//刪除元素
+airports["APL"] = nil
+airports
