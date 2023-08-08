e36cd8c0b568a7887333bba72122cdc8df021e2e
徐國堂 <roberthsu2003@gmail.com>
2023-08-08 14:28:38 +0800

    modify

---
+++ 三菱/lesson4_2.playground/Contents.swift
@@ -0,0 +1,15 @@
+var namesOfIntegers:[Int:String] = [:]
+var namesOfIntegers1 = [Int:String]()
+var namesOfIntegers2 = Dictionary<Int, String>()
+
+//新增元素
+namesOfIntegers[16] = "sixteen"
+namesOfIntegers
+namesOfIntegers = [:]
+
+//使用符號建立Dictionary
+var airports:[String:String] = [
+                                "YYZ":"Toronto pearson",
+                                "DUB":"Dublin"
+                                ]
+print(airports)
