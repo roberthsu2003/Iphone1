419987ea9638faf7fd898f9726b095521b61261e
徐國堂 <roberthsu2003@gmail.com>
2023-08-08 13:56:46 +0800

    modify

--- 三菱/lesson4_1.playground/Contents.swift
+++ 三菱/lesson4_1.playground/Contents.swift
@@ -4,3 +4,28 @@ letters.insert("a")
 letters
 letters = []
 
+//使用Array純值表示法建立Set
+//必需明確宣告
+//var favoriteGenres:Set<String> = ["Rock", "Classical", "Hip hop"]
+
+var favoriteGenres:Set = ["Rock", "Classical", "Hip hop"]
+
+//存取和修改set
+favoriteGenres.isEmpty
+//插入
+favoriteGenres.insert("Jazz")
+favoriteGenres.insert("Jazz")
+favoriteGenres
+//移除
+
+if let removedGenre = favoriteGenres.remove("Rock"){
+    print(removedGenre)
+    
+}
+
+//檢查元素
+favoriteGenres.contains("Funk")
+
+
+
+
