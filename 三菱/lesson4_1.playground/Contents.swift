d7639e3450c3d151c8448a715a22b2b2329e721c
徐國堂 <roberthsu2003@gmail.com>
2023-08-08 14:11:14 +0800

    modify

--- 三菱/lesson4_1.playground/Contents.swift
+++ 三菱/lesson4_1.playground/Contents.swift
@@ -26,6 +26,38 @@ if let removedGenre = favoriteGenres.remove("Rock"){
 //檢查元素
 favoriteGenres.contains("Funk")
 
+//set iterating
+for genre in favoriteGenres{
+    print("\(genre)")
+}
+
+//set的集合運算
+let oddDigits:Set = [1, 3, 5, 7, 9]
+let evenDigits:Set = [0, 2, 4, 6, 8]
+let singleDigitPrimeNumbers = [2, 3, 5, 7]
+
+//set可以行容易轉換為array
+//聯集
+oddDigits.union(evenDigits).sorted()
+
+//交集
+oddDigits.intersection(evenDigits).sorted()
+
+//差集
+oddDigits.subtracting(singleDigitPrimeNumbers).sorted()
+
+oddDigits.symmetricDifference(singleDigitPrimeNumbers).sorted()
+
+let houseAnimals: Set = ["🐶", "🐱"]
+let farmAnimals: Set = ["🐮", "🐔", "🐑", "🐶", "🐱"]
+let cityAnimals: Set = ["🐦", "🐭"]
+
+
+houseAnimals.isSubset(of: farmAnimals)
+farmAnimals.isSuperset(of: houseAnimals)
+farmAnimals.isDisjoint(with: cityAnimals)
+
+
 
 
 
