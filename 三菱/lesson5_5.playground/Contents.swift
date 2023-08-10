6f55197acd8cc4d8ccdd01c4008f6703294cec01
徐國堂 <roberthsu2003@gmail.com>
2023-08-10 16:17:30 +0800

    modify

--- 三菱/lesson5_5.playground/Contents.swift
+++ 三菱/lesson5_5.playground/Contents.swift
@@ -4,3 +4,28 @@ func greet(person:String) -> String{
 }
 print(greet(person: "robert"))
 print(greet(person: "Brian"))
+
+func greetAgain(person:String) -> String{
+    return "Hello agian, " + person + "!"
+}
+
+greetAgain(person: "Anna")
+
+//沒有參數
+func sayHelloWorld() -> String{
+    return "Hello! World!"
+}
+
+sayHelloWorld()
+
+//多個參數
+//overloading
+func greet(person:String, alreadyGreeted:Bool) -> String{
+    if alreadyGreeted{
+        return greetAgain(person: person)
+    }else{
+        return greet(person: person)
+    }
+}
+
+greet(person: "Tim", alreadyGreeted: true)
