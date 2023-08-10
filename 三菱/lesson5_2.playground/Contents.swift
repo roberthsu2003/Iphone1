789c656b39fcdf993cfb074b189edcad1a7f254b
徐國堂 <roberthsu2003@gmail.com>
2023-08-10 14:26:13 +0800

    modify

--- 三菱/lesson5_2.playground/Contents.swift
+++ 三菱/lesson5_2.playground/Contents.swift
@@ -20,3 +20,26 @@ for character in puzzleInput{
 
 print(puzzleOutput)
 
+let numberSymbol: Character = "๓"  // Chinese symbol for the number 3
+var possibleIntegerValue: Int?
+
+switch numberSymbol {
+case "1", "١", "一", "๑":
+    possibleIntegerValue = 1
+case "2", "٢", "二", "๒":
+    possibleIntegerValue = 2
+case "3", "٣", "三", "๓":
+    possibleIntegerValue = 3
+case "4", "٤", "四", "๔":
+    possibleIntegerValue = 4
+default:
+    break
+}
+
+if let integerValue = possibleIntegerValue{
+    print("符號:\(numberSymbol)代表\(integerValue)")
+}else{
+    print("符號:\(numberSymbol)代表不明")
+}
+
+
