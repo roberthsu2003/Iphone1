974b9ebdae177de7d5c5d52cff43cab193c6cc8c
徐國堂 <roberthsu2003@gmail.com>
2023-08-10 13:45:46 +0800

    modify

--- 三菱/lesson5_1.playground/Contents.swift
+++ 三菱/lesson5_1.playground/Contents.swift
@@ -14,3 +14,19 @@ case 100..<1000:
 default:
     break
 }
+
+
+let somePoint = (0, 0)
+//tuple matching
+switch somePoint{
+case (0,0):
+    print("中心點")
+case (_,0):
+    print("x軸上")
+case (0,_):
+    print("y軸上")
+case (-2...2,-2...2):
+    print("在範圍內")
+default:
+    print("在範圍外")
+}
