3fb1847e7782981290ac0c7fe96cf5e1e3e599f7
徐國堂 <roberthsu2003@gmail.com>
2023-08-15 16:29:18 +0800

    modify

--- 三菱/lesson6_3.playground/Contents.swift
+++ 三菱/lesson6_3.playground/Contents.swift
@@ -14,5 +14,21 @@ names.sorted(by: {
     return s1 < s2
 })
 
+//compiler可以透過別人定義的參數資料類型,來推測closure參數的資料類型
+names.sorted(by: {
+    s1,s2 in
+    return s1 < s2
+})
 
+//function的程式區塊,只有1行,會自動return
+names.sorted(by: {
+    s1,s2 in s1 < s2
+})
+
+//compiler知道closure一定提供2個參數
+
+names.sorted(by: { $0 < $1 })
+
+//
+names.sorted(by: <)
 
