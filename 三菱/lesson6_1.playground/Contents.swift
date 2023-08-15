56416c8b73e42d95667c610efb14730481c7c575
徐國堂 <roberthsu2003@gmail.com>
2023-08-15 13:57:46 +0800

    modify

--- 三菱/lesson6_1.playground/Contents.swift
+++ 三菱/lesson6_1.playground/Contents.swift
@@ -17,3 +17,26 @@ func minMax(array:[Int]) -> (min:Int,max:Int){
 //呼叫時使用引數名稱
 let bounds = minMax(array: [8, -6, 2, 109, 3, 71])
 print("最小值是\(bounds.min),最大值是\(bounds.max)")
+
+//function傳出optional type
+func minMax(array1:[Int]) -> (min:Int,max:Int)?{
+    //定義function,要使用參數名稱
+    if array1.isEmpty{return nil}
+    var currentMin = array1[0]
+    var currentMax = array1[0]
+    for value in array1[1..<array1.count]{
+        if value < currentMin{
+            currentMin = value
+        }else if value > currentMax{
+            currentMax = value
+        }
+    }
+    return (currentMin,currentMax)
+}
+
+//呼叫時使用optional binding
+if let bounds1 = minMax(array1: [8, -6, 2, 109, 3, 71]){
+    print("最小值是\(bounds1.min),最大值是\(bounds1.max)")
+}
+
+
