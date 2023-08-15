30aa89f4cf16db27a1482d4c3e651e05ac67d140
徐國堂 <roberthsu2003@gmail.com>
2023-08-15 15:30:04 +0800

    modify

--- 三菱/lesson6_2.playground/Contents.swift
+++ 三菱/lesson6_2.playground/Contents.swift
@@ -17,4 +17,17 @@ mathFunction = multiplyTwoInts
 
 mathFunction(2, 3)
 
+//function 可以當作引數值
+//function參數可以定義function type
+
+func printMathResult(_ mathFunction:(Int,Int)->Int,_ a:Int,_ b:Int){
+    print("處理完的結果是:\(mathFunction(a,b))")
+}
+
+printMathResult(addTwoInts, 10, 5)
+printMathResult(multiplyTwoInts, 10, 5)
+
+
+
+
 
