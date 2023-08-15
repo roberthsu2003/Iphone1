9b0e251be763bd3fcb14243baf3794deafa93bab
徐國堂 <roberthsu2003@gmail.com>
2023-08-15 14:14:00 +0800

    modify

--- 三菱/lesson6_1.playground/Contents.swift
+++ 三菱/lesson6_1.playground/Contents.swift
@@ -50,4 +50,25 @@ func anotherGreeting(for person:String) ->String{
 }
 print(anotherGreeting(for: "Dave"))
 
+//如果只有定義參數名稱,則引數名稱和參數名稱是相同的名稱
+func someFunction(firstParameterName:Int, secondParameterName:Int){
+    
+}
+someFunction(firstParameterName: 1, secondParameterName: 1)
+
+//前面引數名稱,後面的叫參數名稱
+func someFunction(argumentLabel parameterName:Int){
+    
+}
+someFunction(argumentLabel: 3)
+
+//混合式
+func greet(person:String,from hometown:String) -> String{
+    return "\(person),您從\(hometown)來的"
+}
+
+greet(person: "Bill", from: "新北市")
+
+
+
 
