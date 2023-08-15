971d807c093fc61239ef7ed4787549383aeaeb07
徐國堂 <roberthsu2003@gmail.com>
2023-08-15 16:18:48 +0800

    modify

--- 三菱/lesson6_3.playground/Contents.swift
+++ 三菱/lesson6_3.playground/Contents.swift
@@ -2,11 +2,17 @@ let names = ["Chris", "Alex", "Ewa", "Barry", "Daniella"]
 //names.sort()
 //names.sorted()
 
-func abc(_ s1:String,_ s2:String) -> Bool{
+func a1(_ s1:String,_ s2:String) -> Bool{
     return s1 > s2
 }
 
-names.sorted(by: abc)
+names.sorted(by: a1)
+
+//closure->暱名的function
+names.sorted(by: {
+    (s1:String,s2:String)->Bool in
+    return s1 < s2
+})
 
 
 
