df555aeaea1647e717836d16f9c9b49595e8ae5e
徐國堂 <roberthsu2003@gmail.com>
2023-08-22 15:44:22 +0800

    modify

--- 三菱/lesson7_3.playground/Contents.swift
+++ 三菱/lesson7_3.playground/Contents.swift
@@ -10,5 +10,21 @@ enum Planet{
     case mercury, venus, earth, mars, jupiter, saturn, uranus, neptune
 }
 
+//建立列舉型別的變數
 var directionToHead = CompassPoint.west
 directionToHead = .east
+directionToHead = .south
+
+//檢查列舉型別變數內儲存的值是?
+//使用switch...case
+
+switch directionToHead{
+    case .east:
+        print("東")
+    case .north:
+        print("北")
+    case .south:
+        print("南")
+    case .west:
+        print("西")
+}
