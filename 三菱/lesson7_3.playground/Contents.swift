f8e49dd65cc0943b73e2f1b6292209c4adc170de
徐國堂 <roberthsu2003@gmail.com>
2023-08-22 16:14:50 +0800

    modify

--- 三菱/lesson7_3.playground/Contents.swift
+++ 三菱/lesson7_3.playground/Contents.swift
@@ -48,4 +48,24 @@ for beverage in Beverage.allCases{
     print(beverage)
 }
 
+//列舉使用associate value
+enum Barcode{
+    case upc(Int, Int, Int, Int)
+    case qrCode(String)
+}
+
+var productBarcode = Barcode.upc(8, 85909, 51226, 3)
+//productBarcode = .qrCode("ABCDEFGHIJKLMN")
+
+switch productBarcode{
+case .upc(let numberSystem, let manufacturer, let product, let check):
+    print("UPC:\(numberSystem),\(manufacturer),\(product),\(check)")
+
+case .qrCode(let productCode):
+    print("QR code:\(productCode)")
+}
+
+
+
+
 
