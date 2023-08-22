0f726d779330e74df44e0f00ffae62217a30ecdb
徐國堂 <roberthsu2003@gmail.com>
2023-08-22 14:08:39 +0800

    modify

--- 三菱/lesson7_1.playground/Contents.swift
+++ 三菱/lesson7_1.playground/Contents.swift
@@ -44,4 +44,21 @@ names.sorted{
     return s1 < s2
 }
 
+let digitNames = [
+    0: "Zero", 1: "One", 2: "Two",   3: "Three", 4: "Four",
+    5: "Five", 6: "Six", 7: "Seven", 8: "Eight", 9: "Nine"
+]
 
+let numbers = [16, 58, 510]
+
+//trailing closure
+numbers.map{
+    (number:Int)->String in
+    var number = number
+    var output = ""
+    repeat{
+        output = digitNames[number % 10]! + output
+        number /= 10
+    }while(number > 0)
+    return output
+}
