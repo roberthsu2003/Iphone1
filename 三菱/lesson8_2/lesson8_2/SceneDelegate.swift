2bcdf644da7288b160a58b6f65ac9aa8c5916637
徐國堂 <roberthsu2003@gmail.com>
2023-08-24 16:22:09 +0800

    modify

--- 三菱/lesson8_2/lesson8_2/SceneDelegate.swift
+++ 三菱/lesson8_2/lesson8_2/SceneDelegate.swift
@@ -17,7 +17,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
-        print(#function)
+        if let window = window, let rootViewC = window.rootViewController{
+           print(rootViewC)
+        }else{
+            print("沒有window的實體")
+        }
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
--- 三菱/lesson8_2/lesson8_2/ViewController.swift
+++ 三菱/lesson8_2/lesson8_2/ViewController.swift
@@ -8,10 +8,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("view的實體已經建立完成")
+        if let color = view.backgroundColor{
+            print(color)
+        }
+        view.backgroundColor = UIColor.systemBlue
     }
 
 
