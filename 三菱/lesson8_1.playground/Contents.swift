4fb33bd22601de6a87f995cbc85cf96d22635795
徐國堂 <roberthsu2003@gmail.com>
2023-08-24 14:07:24 +0800

    modify

--- 三菱/lesson8_1.playground/Contents.swift
+++ 三菱/lesson8_1.playground/Contents.swift
@@ -8,4 +8,28 @@ class SomeClass{
     
 }
 
+struct Resolution{
+    var width = 0
+    var height = 0
+}
 
+class VideoMode{
+    var resolution = Resolution()
+    var interlaced = false
+    var frameRate = 0.0
+    var name:String?
+}
+
+let someResolution = Resolution()
+someResolution.width
+someResolution.height
+
+let someVideoMode = VideoMode()
+someVideoMode.resolution.width
+someVideoMode.resolution.height
+someVideoMode.interlaced
+someVideoMode.name
+someVideoMode.frameRate
+
+someVideoMode.resolution.width = 1280
+someVideoMode.resolution.width
