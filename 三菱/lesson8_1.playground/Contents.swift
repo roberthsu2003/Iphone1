2b8449b259e5e7531087257e1e0a0c915b79f77c
徐國堂 <roberthsu2003@gmail.com>
2023-08-24 14:20:36 +0800

    modify

--- 三菱/lesson8_1.playground/Contents.swift
+++ 三菱/lesson8_1.playground/Contents.swift
@@ -33,3 +33,13 @@ someVideoMode.frameRate
 
 someVideoMode.resolution.width = 1280
 someVideoMode.resolution.width
+
+let vga = Resolution(width: 640, height: 480)
+vga.width
+vga.height
+
+let hd = Resolution(width: 1920, height: 1080)
+var cinema = hd
+cinema.width = 2048
+hd.width
+cinema.width
