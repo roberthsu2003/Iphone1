c7a4a56a2a4380fcde18faafc9d96f371b346c22
徐國堂 <roberthsu2003@gmail.com>
2023-08-29 16:16:55 +0800

    modify

--- 三菱/lesson9_4/lesson9_4/ViewController.swift
+++ 三菱/lesson9_4/lesson9_4/ViewController.swift
@@ -14,6 +14,9 @@ class ViewController: UIViewController {
         print("storyboard內的所有view,已經可以存取了")
         print(view.frame)
         view.backgroundColor = .cyan
+        let v1 = UIView(frame: CGRect(x: 113, y: 111, width: 132, height: 194))
+        v1.backgroundColor = UIColor.blue
+        view.addSubview(v1)
     }
 
 
