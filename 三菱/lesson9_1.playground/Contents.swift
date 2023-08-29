3209a1c6032fed7abc0a34d15582e152d2b32cc2
徐國堂 <roberthsu2003@gmail.com>
2023-08-29 14:14:24 +0800

    modify

--- 三菱/lesson9_1.playground/Contents.swift
+++ 三菱/lesson9_1.playground/Contents.swift
@@ -11,3 +11,20 @@ rangeOfThreeItems.firstValue = 6
 let rangeOfFourItems = FixedLengthRange(firstValue: 0, length: 4)
 rangeOfFourItems.firstValue
 rangeOfFourItems.length
+
+//lazy store property
+class DataImporter{
+    var filename = "data.txt" //會花費3秒的時間
+}
+
+class DataManager{
+    lazy var importer = DataImporter() //花費3秒的時間
+    var data:[String] = []
+}
+
+let manager = DataManager()
+manager.data.append("some data")
+manager.data.append("some more data")
+manager
+manager.importer.filename
+manager
