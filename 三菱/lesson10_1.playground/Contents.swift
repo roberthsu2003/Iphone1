52feedb76b51b6dd72e02535bdd145b6af06f5ef
徐國堂 <roberthsu2003@gmail.com>
2023-08-31 14:02:33 +0800

    modify

--- 三菱/lesson10_1.playground/Contents.swift
+++ 三菱/lesson10_1.playground/Contents.swift
@@ -16,3 +16,28 @@ struct SmallRectangle{
 var rectangle = SmallRectangle()
 print(rectangle.height)
 print(rectangle.width)
+
+rectangle.height = 10
+print(rectangle.height)
+
+rectangle.height = 24
+print(rectangle.height)
+
+
+struct SmallRectangle1{
+    private var _height = TwelveOrLess()
+    private var _width = TwelveOrLess()
+    
+    var height:Int{
+        get { return _height.wrappedValue }
+        set {_height.wrappedValue = newValue}
+    }
+    
+    var width:Int{
+        get {return _width.wrappedValue}
+        set {_width.wrappedValue = newValue}
+    }
+    
+}
+
+
