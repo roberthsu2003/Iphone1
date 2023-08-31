30519e7a335d4c43279263bf4f6db36cb2d8acf1
徐國堂 <roberthsu2003@gmail.com>
2023-08-31 16:04:32 +0800

    modify

--- 三菱/lesson10_5/lesson10_5/ViewController.swift
+++ 三菱/lesson10_5/lesson10_5/ViewController.swift
@@ -12,10 +12,39 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         let v1 = UIView(frame: CGRect(x: 113, y: 111, width: 132, height: 194))
+        /*
         v1.backgroundColor = UIColor{
             (tc:UITraitCollection) -> UIColor in
-            return UIColor(red: 0.4, green: 0.4, blue: 0.4, alpha: 1)
+            switch tc.userInterfaceStyle{
+            case .dark:
+                return UIColor(red: 0.4, green: 0.4, blue: 0.4, alpha: 1)
+            default:
+                return UIColor(red: 0, green: 0.1, blue: 0.1, alpha: 1)
+            }
+            
           }
+         */
+        /*
+        v1.backgroundColor = UIColor{
+        tc in
+            switch tc.userInterfaceStyle{
+            case .dark:
+                return UIColor(red: 0.4, green: 0.4, blue: 0.4, alpha: 1)
+            default:
+                return UIColor(red: 0, green: 0.1, blue: 0.1, alpha: 1)
+            }
+            
+          }
+         */
+        v1.backgroundColor = UIColor(dynamicProvider: { (tc:UITraitCollection) in
+            switch tc.userInterfaceStyle{
+            case .dark:
+                return UIColor(red: 0.4, green: 0.4, blue: 0.4, alpha: 1)
+            default:
+                return UIColor(red: 0, green: 0.1, blue: 0.1, alpha: 1)
+            }
+            
+        })
         view.addSubview(v1)
     }
 
