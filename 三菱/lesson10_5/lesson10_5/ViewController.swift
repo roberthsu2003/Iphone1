a1a069b2dc8860de1f51300bbdf2bcc1ea9a42b6
徐國堂 <roberthsu2003@gmail.com>
2023-08-31 15:49:37 +0800

    modify

--- 三菱/lesson10_5/lesson10_5/ViewController.swift
+++ 三菱/lesson10_5/lesson10_5/ViewController.swift
@@ -12,7 +12,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         let v1 = UIView(frame: CGRect(x: 113, y: 111, width: 132, height: 194))
-        v1.backgroundColor = UIColor(red: 1, green: 0.4, blue: 1, alpha: 1.0)
+        v1.backgroundColor = UIColor{
+            (tc:UITraitCollection) -> UIColor in
+            return UIColor(red: 0.4, green: 0.4, blue: 0.4, alpha: 1)
+          }
         view.addSubview(v1)
     }
 
