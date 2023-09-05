0a4562bc2c6b12928135155063ada1d6e8109066
徐國堂 <roberthsu2003@gmail.com>
2023-09-05 13:57:46 +0800

    modify

--- 三菱/lesson11_1.playground/Contents.swift
+++ 三菱/lesson11_1.playground/Contents.swift
@@ -2,15 +2,15 @@ class Counter{
     var count = 0
     //定義3個實體方法
     func increment(){
-        count += 1
+        self.count += 1
     }
     
     func increment(by amount:Int){
-        count += amount
+        self.count += amount
     }
     
     func reset(){
-        count = 0
+        self.count = 0
     }
 }
 
@@ -18,3 +18,17 @@ let counter = Counter()
 counter.increment()
 counter.increment(by: 6)
 counter.reset()
+
+//有一個狀況一定要用self,當參數名稱和property是相同名稱時
+
+struct Point{
+    var x = 0.0, y = 0.0
+    func isToTheRightOf(x:Double) -> Bool{
+        return self.x > x
+    }
+}
+
+let somePoint = Point(x: 4.0, y: 5.0)
+if somePoint.isToTheRightOf(x: 1.0){
+    print("實體的x,大於參數1.0")
+}
