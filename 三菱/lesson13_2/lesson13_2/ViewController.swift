3775920e2d60e19bc97924ae976e4c22a8a91004
roberthsu2003 <roberthsu2003@gmail.com>
2023-09-14 15:56:35 +0800

    modify

---
+++ 三菱/lesson13_2/lesson13_2/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  lesson13_2
+//
+//  Created by 徐國堂 on 2023/9/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var heightField:UITextField!
+    @IBOutlet var weightField:UITextField!
+    @IBOutlet var bmiBtn:UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
