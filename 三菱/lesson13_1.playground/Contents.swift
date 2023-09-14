bce3cc17bd05ac5c365fcd8fb4e0c4479b2c84d2
roberthsu2003 <roberthsu2003@gmail.com>
2023-09-14 13:56:28 +0800

    modify

--- 三菱/lesson13_1.playground/Contents.swift
+++ 三菱/lesson13_1.playground/Contents.swift
@@ -21,3 +21,13 @@ class Bicycle:Vehicle{
 let bicycle = Bicycle()
 bicycle.currentSpeed = 15.0
 print("Bicycle:\(bicycle.description)")
+
+class Tandem:Bicycle{
+    var currentNumberOfpassengers = 0
+}
+
+let tandem = Tandem()
+tandem.hasBasket = true
+tandem.currentSpeed = 22.0
+tandem.currentNumberOfpassengers = 2
+print("Tandem:\(tandem.description)")
