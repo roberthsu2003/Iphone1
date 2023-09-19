a18b39744cf7c0297d94144ba1717836e5ed9f80
roberthsu2003 <roberthsu2003@gmail.com>
2023-09-19 15:05:24 +0800

    modify

--- 三菱/lesson13_2/lesson13_2/ViewController.swift
+++ 三菱/lesson13_2/lesson13_2/ViewController.swift
@@ -28,11 +28,30 @@ class ViewController: UIViewController {
         let heightValue = Int(heightField.text!)
         let weightValue = Int(weightField.text!)
         
+        var bmi = 0.0
         if let heightValue = heightValue, let weightValue = weightValue{
-            let bmi = Double(weightValue) / pow(Double(heightValue) / 100.0,2)
-            print(bmi)
+            bmi = Double(weightValue) / pow(Double(heightValue) / 100.0,2)
+        }
+        print(bmi)
+        var message = ""
+        switch bmi{
+        case 0 ..< 18.5:
+            message = "體重過輕"
+        case 18.5 ..< 24:
+            message = "標準"
+        case 24..<27:
+            message = "過重"
+        case 27..<30:
+            message = "輕度肥胖"
+        case 30..<35:
+            message = "中度肥胖"
+        case 35..<1000:
+            message = "重度肥胖"
+        default:
+            break
         }
         
+        print(message)
         
     }
 
