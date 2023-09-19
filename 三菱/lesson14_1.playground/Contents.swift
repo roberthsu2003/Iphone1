5e523749c73f3db1b7b48dc2d3bd7445cf2f9817
roberthsu2003 <roberthsu2003@gmail.com>
2023-09-19 13:52:45 +0800

    modify

--- 三菱/lesson14_1.playground/Contents.swift
+++ 三菱/lesson14_1.playground/Contents.swift
@@ -9,3 +9,28 @@ struct Fahrenheit{
 var f = Fahrenheit()
 print(f.temperature)
 
+
+struct Fahrenheit1{
+    var temperature = 32.0
+}
+
+var f1 = Fahrenheit1()
+print(f1.temperature)
+
+struct Celsius{
+    var temperatureInCelsius:Double
+    init(fromFahrenheit fahrenheit:Double){
+        temperatureInCelsius = (fahrenheit - 32.0) / 1.8
+    }
+    
+    init(fromKelvin kelvin:Double){
+        temperatureInCelsius = kelvin - 273.15
+    }
+}
+
+let boilingPointOfWater = Celsius(fromFahrenheit: 212.0)
+boilingPointOfWater.temperatureInCelsius
+
+let freezingPointOfWater = Celsius(fromKelvin: 273.15)
+freezingPointOfWater.temperatureInCelsius
+
