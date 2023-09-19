4a09ee8fd8d59dd632a563d4f18ee7883a6900e3
roberthsu2003 <roberthsu2003@gmail.com>
2023-09-19 14:41:26 +0800

    modify

--- 三菱/lesson14_1.playground/Contents.swift
+++ 三菱/lesson14_1.playground/Contents.swift
@@ -106,12 +106,54 @@ class ShoppingListItem{
  var item = ShoppingListItem()
 item
 
-struct Size{
+struct Size1{
     var width = 0.0
     var height = 0.0
 }
 
-Size(width: 2.0, height: 2.0)
-let zeroByTwo = Size(height: 2.0)
-let zeroByZero = Size(width: 2.0)
+Size1(width: 2.0, height: 2.0)
+let zeroByTwo = Size1(height: 2.0)
+let zeroByZero = Size1(width: 2.0)
+
+//initializer Delegation for value types
+
+struct Size{
+    var width = 0.0, height = 0.0
+}
+
+struct Point{
+    var x = 0.0, y = 0.0
+}
+
+struct Rect{
+    var origin = Point()
+    var size = Size()
+    
+    init(){}
+    
+    init(origin:Point, size:Size){
+        self.origin = origin
+        self.size = size
+    }
+     
+     
+    
+    init(center:Point, size:Size){
+        let originX = center.x - (size.width / 2)
+        let originY = center.y - (size.height / 2)
+        self.init(origin: Point(x: originX, y: originY), size: size) // initializer Delegation
+    }
+    
+}
+
+let basicRect = Rect()
+let originRect = Rect(origin: Point(x: 2.0, y: 2.0), size: Size(width: 5.0, height: 5.0))
+
+let centerRect = Rect(center: Point(x: 4.0, y: 4.0), size: Size(width: 3.0, height: 3.0))
+
+
+
+
+
+
 
