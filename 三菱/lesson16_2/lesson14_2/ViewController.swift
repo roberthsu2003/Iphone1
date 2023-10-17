a200ee9721cc904f9f6a52916e194c95a81e98e1
roberthsu2003 <roberthsu2003@gmail.com>
2023-10-17 15:34:12 +0800

    modify

---
+++ 三菱/lesson16_2/lesson14_2/ViewController.swift
@@ -0,0 +1,154 @@
+//
+//  ViewController.swift
+//  lesson14_2
+//
+//  Created by 徐國堂 on 2023/9/19.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+    var n:Int
+    var v1:UIView!
+    
+    required init?(coder: NSCoder) {
+        self.n = 5
+        super.init(coder: coder)
+        print(self.n)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //v1要使用自動產生constraints
+        v1 = UIView(frame: CGRect(x: 100, y: 111, width: 132, height: 194))
+        v1.backgroundColor = UIColor(red: 1, green: 0.4, blue: 1, alpha: 1)
+        
+        
+        let v2 = UIView()
+        v2.backgroundColor = UIColor(red: 0.5, green: 1, blue: 0, alpha: 1)
+        //v2請不要自動產生autoresing的contraints
+        v2.translatesAutoresizingMaskIntoConstraints = false
+        
+        let v3 = UIView()
+        v3.backgroundColor = UIColor(red: 1, green: 0, blue: 0, alpha: 1)
+        v3.translatesAutoresizingMaskIntoConstraints = false
+        
+        
+        
+        self.view.addSubview(v1)
+        v1.addSubview(v2)
+        v1.addSubview(v3)
+        
+        var which:Int{
+            return 2
+        }
+        
+        switch which{
+        case 1:
+            v1.addConstraint(NSLayoutConstraint(item: v1!,
+                                                attribute: .top,
+                                                relatedBy: .equal,
+                                                toItem: v2,
+                                                attribute: .top,
+                                                multiplier: 1,
+                                                constant: 0))
+            v1.addConstraint(NSLayoutConstraint(item: v1!,
+                                                attribute: .leading,
+                                                relatedBy: .equal,
+                                                toItem: v2,
+                                                attribute: .leading,
+                                                multiplier: 1,
+                                                constant: 0))
+            v1.addConstraint(NSLayoutConstraint(item: v1!,
+                                                attribute: .trailing,
+                                                relatedBy: .equal,
+                                                toItem: v2,
+                                                attribute: .trailing,
+                                                multiplier: 1,
+                                                constant: 0))
+            v2.addConstraint(NSLayoutConstraint(item: v2,
+                                                attribute: .height,
+                                                relatedBy: .equal,
+                                                toItem: nil,
+                                                attribute: .notAnAttribute,
+                                                multiplier: 1,
+                                                constant: 10))
+            
+            v1.addConstraint(NSLayoutConstraint(item: v1!,
+                                                attribute: .trailing,
+                                                relatedBy: .equal,
+                                                toItem: v3,
+                                                attribute: .trailing,
+                                                multiplier: 1,
+                                                constant: 0))
+            
+            v1.addConstraint(NSLayoutConstraint(item: v1!,
+                                                attribute: .bottom,
+                                                relatedBy: .equal,
+                                                toItem: v3,
+                                                attribute: .bottom,
+                                                multiplier: 1,
+                                                constant: 0))
+            
+            v3.addConstraint(NSLayoutConstraint(item: v3,
+                                                attribute: .width,
+                                                relatedBy: .equal,
+                                                toItem: nil,
+                                                attribute: .notAnAttribute,
+                                                multiplier: 1,
+                                                constant: 20))
+            
+            v3.addConstraint(NSLayoutConstraint(item: v3,
+                                                attribute: .height,
+                                                relatedBy: .equal,
+                                                toItem: nil,
+                                                attribute: .notAnAttribute,
+                                                multiplier: 1,
+                                                constant: 20))
+            
+        case 2:
+            // visual format language
+            let d = ["v2":v2,"v3":v3]
+            NSLayoutConstraint.activate([
+                NSLayoutConstraint.constraints(
+                    withVisualFormat: "H:|[v2]|",
+                    metrics: nil,
+                    views: d),
+                NSLayoutConstraint.constraints(
+                    withVisualFormat: "V:|[v2(10)]",
+                    metrics: nil,
+                    views: d),
+                NSLayoutConstraint.constraints(
+                    withVisualFormat: "H:[v3(20)]|",
+                    metrics: nil,
+                    views: d),
+                NSLayoutConstraint.constraints(
+                    withVisualFormat: "V:[v3(20)]|",
+                    metrics: nil,
+                    views: d)
+                
+            ].flatMap({$0}))
+            
+        case 3:break
+            
+        default:break
+        }
+        
+        
+        
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        delay(2, closure: {
+            self.v1.bounds.size.width += 40
+            self.v1.bounds.size.height -= 50
+        
+        })
+    }
+
+
+}
+
