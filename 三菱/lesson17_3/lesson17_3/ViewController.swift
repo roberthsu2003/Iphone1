b80a58bf51ac7c9ab3cdf86067bed5336de3b849
roberthsu2003 <roberthsu2003@gmail.com>
2023-10-19 16:09:52 +0800

    modify

--- 三菱/lesson17_3/lesson17_3/ViewController.swift
+++ 三菱/lesson17_3/lesson17_3/ViewController.swift
@@ -8,10 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        if let width = self.view.viewWithTag(5)?.frame.width{
+            print(width)
+        }
     }
 
 
