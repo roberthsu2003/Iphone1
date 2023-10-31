c151fff6fc9791b057c381b1fa3d6efa9ae35f4f
roberthsu2003 <roberthsu2003@gmail.com>
2023-10-31 15:56:37 +0800

    modify

---
+++ 三菱/lesson18_2/lesson18_2/FirstViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FirstViewController.swift
+//  lesson18_2
+//
+//  Created by 徐國堂 on 2023/10/31.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- 三菱/lesson18_2/lesson18_2/ViewController.swift
+++ 三菱/lesson18_2/lesson18_2/ViewController.swift
@@ -13,6 +13,10 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func clickSecond(_ sender:UIButton){
+        print("SecondViewController")
+    }
 
 
 }
