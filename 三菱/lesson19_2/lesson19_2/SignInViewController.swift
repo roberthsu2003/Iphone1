0d6538cb0fd540e419ddbf032df0934f555a3e5c
roberthsu2003 <roberthsu2003@gmail.com>
2023-11-07 15:27:24 +0800

    modify

--- 三菱/lesson19_2/lesson19_2/SignInViewController.swift
+++ 三菱/lesson19_2/lesson19_2/SignInViewController.swift
@@ -8,11 +8,30 @@
 import UIKit
 
 class SignInViewController: UIViewController {
+    
+    private lazy var stackView:UIStackView = {
+        let stackView = UIStackView()
+        stackView.backgroundColor = .secondarySystemBackground
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.alignment = .center
+        stackView.distribution = .fill
+        stackView.spacing = 10
+        return stackView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         navigationItem.title = "SignIn Button"
+        view.addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            stackView.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor),
+            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            stackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
     }
 
 }
