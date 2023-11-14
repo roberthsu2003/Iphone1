c0518dd58b97b568cf47bdd8186e72a43b9d078c
roberthsu2003 <roberthsu2003@gmail.com>
2023-11-14 13:59:32 +0800

    modify

--- 三菱/lesson21_1.playground/Contents.swift
+++ 三菱/lesson21_1.playground/Contents.swift
@@ -29,3 +29,44 @@ double1.mm
 3.0.ft
 
 let aMarothon = 42.km + 195.m
+
+
+struct Size{
+    var width = 0.0, height = 0.0
+}
+
+struct Point{
+    var x = 0.0, y = 0.0
+}
+
+struct Rect{
+    var origin = Point()
+    var size = Size()
+}
+
+let defaultRect = Rect()
+let memberwiseRect = Rect(origin: Point(x: 2.0,
+                                        y: 2.0),
+                          size: Size(width: 5.0,
+                                     height: 5.0))
+//擴充initializer
+
+extension Rect{
+    init(center:Point, size:Size){
+        let originX = center.x - (size.width/2)
+        let originY = center.y - (size.height/2)
+        self.init(origin: Point(x: originX,
+                                y: originY),
+                  size: Size(width: size.width,
+                             height: size.height))
+    }
+}
+
+let centerRect = Rect(center: Point(x: 4.0,
+                                    y: 4.0),
+                      size: Size(width: 3.0,
+                                 height: 3.0))
+centerRect.origin.x
+centerRect.origin.y
+centerRect.size.width
+centerRect.size.height
