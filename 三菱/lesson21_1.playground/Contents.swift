1b9e58608f933998cb299b78e5d1ea5516350990
roberthsu2003 <roberthsu2003@gmail.com>
2023-11-14 14:21:51 +0800

    modify

--- 三菱/lesson21_1.playground/Contents.swift
+++ 三菱/lesson21_1.playground/Contents.swift
@@ -107,3 +107,37 @@ extension Int{
 746381295[0]
 746381295[1]
 746381295[2]
+
+//擴充巢狀型別
+extension Int{
+    enum Kind{
+        case negative, zero, positive
+    }
+    
+    var kind:Kind{
+        switch self{
+        case 0:
+            return .zero
+        case let x where x > 0:
+            return .positive
+        default:
+            return .negative
+        }
+    }
+}
+
+func printIntegerKinds(_ numbers:[Int]){
+    for number in numbers{
+        switch number.kind{
+        case .negative:
+            print("- ",terminator: "")
+        case .zero:
+            print("0 ",terminator: "")
+        case .positive:
+            print("+ ", terminator: "")
+        }
+    }
+    print("")
+}
+
+printIntegerKinds([3, 19, -27, 0, -6, 0, 7])
