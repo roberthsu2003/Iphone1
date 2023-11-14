5ea33f8eca847ed7453f0ce7a4e21daab8cb71bb
roberthsu2003 <roberthsu2003@gmail.com>
2023-11-14 14:55:41 +0800

    modify

--- 三菱/lesson21_2/lesson19_2/views/CardPanelView.swift
+++ 三菱/lesson21_2/lesson19_2/views/CardPanelView.swift
@@ -8,5 +8,13 @@
 import UIKit
 
 class CardPanelView: UIView {
-
+    
+    init(){
+        super.init(frame: CGRect.zero)        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
 }
