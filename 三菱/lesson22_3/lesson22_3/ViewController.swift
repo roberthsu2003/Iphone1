6fe18ec2ed6c3d8e23a99331ed448a866359b0ae
roberthsu2003 <roberthsu2003@gmail.com>
2023-11-21 14:45:02 +0800

    modify

---
+++ 三菱/lesson22_3/lesson22_3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lesson22_3
+//
+//  Created by 徐國堂 on 2023/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var shippingLabel:UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
