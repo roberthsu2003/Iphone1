9f495103a059ad7b0a2e184c1d4cbf112a094f19
roberthsu2003 <roberthsu2003@gmail.com>
2023-11-21 13:57:34 +0800

    modify

--- 三菱/lesson22_1.playground/Contents.swift
+++ 三菱/lesson22_1.playground/Contents.swift
@@ -9,3 +9,20 @@ struct Person:FullyNamed{
 
 let john = Person(fullName: "John")
 john.fullName
+
+
+class Starship:FullyNamed{
+    var prefix:String?
+    var name:String
+    init(name:String, prefix:String?=nil){
+        self.name = name
+        self.prefix = prefix
+    }
+    
+    var fullName: String{
+        return (prefix != nil ? prefix! + " " : "") + name
+    }
+}
+
+var ncc1701 = Starship.init(name: "Enterprise", prefix: "USS")
+ncc1701.fullName
