90b8f6a08f04c28b9ec1a50fb07c14bf0379c77a
roberthsu2003 <roberthsu2003@gmail.com>
2023-11-23 14:15:29 +0800

    modify

--- 三菱/lesson23_1.playground/Contents.swift
+++ 三菱/lesson23_1.playground/Contents.swift
@@ -67,6 +67,18 @@ let simonTheHamster = Hamster(name: "Simon")
 let somethingTextRepresentable:TextRepresentable = simonTheHamster
 somethingTextRepresentable.textualDescription
 
+struct Vector3D:Equatable{
+    var x = 0.0, y = 0.0, z = 0.0
+}
+
+let twoThreeFour = Vector3D(x: 2.0, y: 4.0, z: 4.0)
+
+let anotherTwoThreeFour = Vector3D(x: 2.0, y: 3.0, z: 4.0)
+
+twoThreeFour == anotherTwoThreeFour
+
+
+
 
 
 
