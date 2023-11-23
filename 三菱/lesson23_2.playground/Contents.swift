f60f5422761186656a8f73a4100fc11dee8725f9
roberthsu2003 <roberthsu2003@gmail.com>
2023-11-23 14:51:00 +0800

    modify

--- 三菱/lesson23_2.playground/Contents.swift
+++ 三菱/lesson23_2.playground/Contents.swift
@@ -18,3 +18,29 @@ func wishHappyBirthday(to celebrator:Named&Aged){
 
 let birthdayPerson = Person(name: "Malcolm", age: 21)
 wishHappyBirthday(to: birthdayPerson)
+
+class Location{
+    var latitude:Double
+    var longitude:Double
+    init(latitude: Double, longitude: Double) {
+        self.latitude = latitude
+        self.longitude = longitude
+    }
+}
+//繼承Location,採納Named
+class City:Location, Named{
+    var name:String
+    init(name:String ,latitude: Double, longitude: Double) {
+        self.name = name
+        super.init(latitude: latitude, longitude: longitude)
+    }
+}
+
+func beginConcert(in location:Location & Named){
+    print("Hello, \(location.name)")
+}
+
+let seattle = City(name: "Seattle", latitude: 47.6, longitude: -122.3)
+beginConcert(in: seattle)
+
+
