25175221d3aa3a64dbccea6e20f839a5894ebcc2
roberthsu2003 <roberthsu2003@gmail.com>
2023-11-23 10:59:54 +0800

    modify

--- 參考範例/3_1_0程式建立scrollerView1/test1/ViewController.swift
+++ 參考範例/3_1_0程式建立scrollerView1/test1/ViewController.swift
@@ -12,13 +12,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         let scrollView = UIScrollView()
-        scrollView.contentInsetAdjustmentBehavior = .always //預設為.automatic
+        //scrollView.contentInsetAdjustmentBehavior = .always //預設為.automatic
         scrollView.indicatorStyle = .default
         scrollView.backgroundColor = .systemBackground
         scrollView.translatesAutoresizingMaskIntoConstraints = false
         self.view.addSubview(scrollView)
         NSLayoutConstraint.activate([
-            scrollView.topAnchor.constraint(equalTo: self.view.topAnchor),
+            scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             scrollView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
             scrollView.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor),
             scrollView.trailingAnchor.constraint(equalTo: self.view.layoutMarginsGuide.trailingAnchor)
@@ -27,6 +27,7 @@ class ViewController: UIViewController {
         //contentLayouGuide
         let contentLayoutGuide = scrollView.contentLayoutGuide
         let frameLayoutGuide = scrollView.frameLayoutGuide
+        //預設的FrameLayouGuide大小會和scrollView相同
         print(frameLayoutGuide)
         var previousLab:UILabel? = nil
         
@@ -35,13 +36,14 @@ class ViewController: UIViewController {
             lab.translatesAutoresizingMaskIntoConstraints = false
             lab.text = "This is a Label \(i+1)"
             scrollView.addSubview(lab)
-            lab.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 10).isActive = true
+            //lab.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 10).isActive = true
+            lab.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor).isActive = true
             lab.topAnchor.constraint(equalTo: previousLab?.bottomAnchor ?? scrollView.topAnchor,
-                constant: 10).isActive = true
+                constant: 20).isActive = true
             previousLab = lab
         }
         
-        /* contentLayouGuide會自動對齊上,左,右,有需要改變的才要寫
+        /* contentLayouGuide會自動對齊scrollView的上,左,右,有需要改變的才要寫
         contentLayoutGuide.topAnchor.constraint(equalTo: frameLayoutGuide.topAnchor).isActive = true
         
         contentLayoutGuide.leadingAnchor.constraint(equalTo: frameLayoutGuide.leadingAnchor).isActive = true
