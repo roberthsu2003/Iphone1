32b65256ec5cbb3de620a0f823e972088be6386a
roberthsu2003 <roberthsu2003@gmail.com>
2023-11-23 16:26:43 +0800

    modify

--- 三菱/lesson23_5/lesson23_5/ViewController.swift
+++ 三菱/lesson23_5/lesson23_5/ViewController.swift
@@ -8,21 +8,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let scrollView = UIScrollView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        let scrollView = UIScrollView()
         scrollView.indicatorStyle = .default
         scrollView.backgroundColor = .blue
         scrollView.translatesAutoresizingMaskIntoConstraints = false
         self.view.addSubview(scrollView)
         NSLayoutConstraint.activate([
             scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
             scrollView.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor),
             scrollView.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor)
         ])
+        let contentLayouGuide = scrollView.contentLayoutGuide
+        /*
+        NSLayoutConstraint.activate([
+            contentLayouGuide.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            contentLayouGuide.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor),
+            contentLayouGuide.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor),
+            contentLayouGuide.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 1000)
+        ])
+         */
+        contentLayouGuide.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 1000).isActive = true
+        //contentLayouGuide.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor, constant: 1000).isActive = true
         
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        print("scrollView的frame")
+        print(scrollView.frame)
+        
+        print("frameLayoutGuide")
+        print(scrollView.frameLayoutGuide)
+        
+        print("contentLayoutGuide")
+        print(scrollView.contentLayoutGuide)
     }
 
 
