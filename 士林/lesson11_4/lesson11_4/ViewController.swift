ba1455a06d7703d26d30d7bf2eadbec782373013
roberthsu2003 <roberthsu2003@gmail.com>
2023-11-24 21:52:14 +0800

    modify

--- 士林/lesson11_4/lesson11_4/ViewController.swift
+++ 士林/lesson11_4/lesson11_4/ViewController.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var myLabel:UILabel!
+    @IBOutlet var xCenter:NSLayoutConstraint!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,11 +22,30 @@ class ViewController: UIViewController {
         }
         
         //view.backgroundColor = UIColor.systemOrange
+        /*全部程式做的
         let myLabel = UILabel()
         myLabel.text = "Hello! World!"
         myLabel.font = UIFont.systemFont(ofSize: 20)
         view.addSubview(myLabel)
         myLabel.frame = CGRect(x: 102, y: 183, width: 139, height: 32)
+         */
+        
+        //半自動
+        myLabel.text = "Hello! World!"
+        myLabel.font = UIFont.systemFont(ofSize: 20)
+        //myLabel.frame = CGRect(x: 102, y: 183, width: 139, height: 32)
+        
+        //constraints由程式來寫
+        /*
+        myLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            myLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            myLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+         */
+        
+        xCenter.constant = -100
+        
         
     }
 
