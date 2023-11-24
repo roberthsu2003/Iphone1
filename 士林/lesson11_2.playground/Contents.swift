b505944a0f6025b0afe0a4d2c823a0d60cb786b4
roberthsu2003 <roberthsu2003@gmail.com>
2023-11-24 19:55:00 +0800

    modify

---
+++ 士林/lesson11_2.playground/Contents.swift
@@ -0,0 +1,29 @@
+
+struct SomeStructure{
+    
+}
+
+class SomeClass{
+    
+}
+
+struct Resolution{
+    //property
+    var width = 0
+    var height = 0
+}
+
+let resolution = Resolution()
+
+resolution.height
+resolution.width
+//錯誤
+//resolution.height = 50
+
+
+var resolution1 = Resolution()
+resolution1.width = 50
+resolution1.height = 100
+resolution1.width
+resolution1.height
+
