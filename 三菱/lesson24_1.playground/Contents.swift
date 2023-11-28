b24a9d9b2f81569624aebb008effe06027cc783c
roberthsu2003 <roberthsu2003@gmail.com>
2023-11-28 14:14:56 +0800

    modify

--- 三菱/lesson24_1.playground/Contents.swift
+++ 三菱/lesson24_1.playground/Contents.swift
@@ -49,3 +49,33 @@ for _ in 1...5{
     print(counter.count)
 }
 
+
+protocol RandomNumberGenerator{
+    func random() -> Double
+}
+
+class LinearCongruentialGenerator: RandomNumberGenerator {
+    var lastRandom = 42.0
+    let m = 139968.0
+    let a = 3877.0
+    let c = 29573.0
+    func random() -> Double {
+        lastRandom = ((lastRandom * a + c)
+            .truncatingRemainder(dividingBy:m))
+        return lastRandom / m
+    }
+}
+
+extension RandomNumberGenerator{
+    func randomBool() -> Bool{
+        return random() > 0.5
+    }
+}
+
+let generator = LinearCongruentialGenerator()
+generator.random()
+generator.randomBool()
+
+
+
+
