636eddc629dfee210a7fe661a5d09a7451e07838
roberthsu2003 <roberthsu2003@gmail.com>
2023-11-28 16:07:41 +0800

    modify

---
+++ 三菱/lesson24_4/lesson24_4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lesson24_4
+//
+//  Created by 徐國堂 on 2023/11/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var scrollView:UIScrollView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
