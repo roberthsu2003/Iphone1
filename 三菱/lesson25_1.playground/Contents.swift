17d74a683bfefd13f4e7acdb5c8316f7e1f800ed
roberthsu2003 <roberthsu2003@gmail.com>
2023-11-30 13:47:42 +0800

    modify

--- 三菱/lesson25_1.playground/Contents.swift
+++ 三菱/lesson25_1.playground/Contents.swift
@@ -6,10 +6,39 @@ func swapTwoInts(_ a:inout Int,_ b:inout Int){
     b = temporaryA
 }
 
+
+
+
+
+
+func swapTwoStrings(_ a:inout String,_ b:inout String){
+    let temporaryA = a
+    a = b
+    b = temporaryA
+}
+
+func swapTwoDoubles(_ a:inout Double,_ b:inout Double){
+    let temporaryA = a
+    a = b
+    b = temporaryA
+}
+
+//利用generic function解決這樣的問題
+
+func swapTwoValues<T>(_ a:inout T,_ b:inout T){
+    let temporaryA = a
+    a = b
+    b = temporaryA
+}
+
 var someInt = 3
 var anotherInt = 107
 
-swapTwoInts(&someInt, &anotherInt)
-
-
+swapTwoValues(&someInt, &anotherInt)
 print(someInt, anotherInt)
+
+var someString = "hello"
+var anotherString = "world"
+swapTwoValues(&someString, &anotherString)
+
+print(someString, anotherString)
