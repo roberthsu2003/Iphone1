ca507cc847e71527f2336c2e8b46e4b5110e9ae0
roberthsu2003 <roberthsu2003@gmail.com>
2023-11-30 15:54:11 +0800

    modify

---
+++ 三菱/lesson25_3/lesson25_3/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  lesson25_3
+//
+//  Created by 徐國堂 on 2023/11/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var scrollView:UIScrollView!
+    var imageView:UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
