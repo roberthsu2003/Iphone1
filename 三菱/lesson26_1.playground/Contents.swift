1615def8726b3f04a9f08399d574fc9751af52c3
roberthsu2003 <roberthsu2003@gmail.com>
2023-12-05 14:53:24 +0800

    modify

--- 三菱/lesson26_1.playground/Contents.swift
+++ 三菱/lesson26_1.playground/Contents.swift
@@ -139,3 +139,25 @@ extension Container where Item == Double{
 }
 
 [1260.0, 1200.0, 98.6, 37.0].average()
+
+
+extension Container{
+    func average() -> Double where Item == Int{
+        var sum = 0.0
+        for index in 0..<count{
+            sum += Double(self[index])
+        }
+        
+        return sum / Double(count)
+    }
+    
+    func endsWith(_ item:Item) -> Bool where Item:Equatable{
+        return count >= 1 && self[count-1] == item
+    }
+}
+
+let numbers = [1260, 1200, 98, 37]
+numbers.average()
+
+
+numbers.endsWith(37)
