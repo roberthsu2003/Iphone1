d4eace81566b03b8a2ba1dd1119d75f5d1d62606
roberthsu2003 <roberthsu2003@gmail.com>
2023-12-05 14:44:47 +0800

    modify

--- 三菱/lesson26_1.playground/Contents.swift
+++ 三菱/lesson26_1.playground/Contents.swift
@@ -127,3 +127,15 @@ if [9, 9, 9].startsWith(42){
 }else{
     print("不是42")
 }
+
+extension Container where Item == Double{
+    func average() -> Double{
+        var sum = 0.0
+        for index in 0..<count{
+            sum += self[index]
+        }
+        return sum / Double(count)
+    }
+}
+
+[1260.0, 1200.0, 98.6, 37.0].average()
