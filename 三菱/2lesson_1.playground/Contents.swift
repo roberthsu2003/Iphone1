bf5b997db6e417b0dde6c0875b7bd5cddde4a0ac
roberthsu2003 <roberthsu2003@gmail.com>
2023-12-07 14:18:51 +0800

    modify

--- 三菱/2lesson_1.playground/Contents.swift
+++ 三菱/2lesson_1.playground/Contents.swift
@@ -67,5 +67,18 @@ let trapezoid = makeTrapezoid()
 trapezoid.draw()
 
 
+func flip<T:Shape>(_ shape:T) -> some Shape{
+    return FlippedShape(shape: shape)
+}
+
+func join<T:Shape, U:Shape>(_ top:T,_ bottom:U) -> some Shape{
+    return JoinedShape(top: top, bottom: bottom)
+}
+
+let opaqueJoinedTriangles = join(smallTriangle, flip(smallTriangle))
+print(opaqueJoinedTriangles.draw())
+
+
+
 
 
