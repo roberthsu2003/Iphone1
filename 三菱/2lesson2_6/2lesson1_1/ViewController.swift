f4224741df9265d01ffe3b191081a1db6ad5b01a
roberthsu2003 <roberthsu2003@gmail.com>
2023-12-12 15:46:06 +0800

    modify

--- 三菱/2lesson2_6/2lesson1_1/ViewController.swift
+++ 三菱/2lesson2_6/2lesson1_1/ViewController.swift
@@ -8,12 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+}
 
-
+extension ViewController:UITextFieldDelegate{
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool{
+        //使用者按了return
+        print("按了return")
+        textField.resignFirstResponder()
+        return true
+    }
 }
 
