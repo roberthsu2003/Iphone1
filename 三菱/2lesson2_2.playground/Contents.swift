600193b18872e7f88e82ecb6dae396dd056a2a77
roberthsu2003 <roberthsu2003@gmail.com>
2023-12-12 14:07:08 +0800

    modify

---
+++ 三菱/2lesson2_2.playground/Contents.swift
@@ -0,0 +1,36 @@
+
+class Person{
+    let name:String
+    init(name: String) {
+        self.name = name
+    }
+    
+    var apartment:Apartment?
+    deinit{
+        print("\(name)記憶體被釋放")
+    }
+}
+
+class Apartment{
+    let unit:String
+    init(unit: String) {
+        self.unit = unit
+    }
+    var tenant:Person?
+    deinit{
+        print("\(unit)記憶體被釋放")
+    }
+}
+
+var john:Person?
+var unit4A:Apartment?
+
+john = Person(name: "John Appleseed")
+unit4A = Apartment(unit: "4A")
+
+john?.apartment = unit4A
+unit4A?.tenant = john
+
+john = nil
+unit4A = nil
+
