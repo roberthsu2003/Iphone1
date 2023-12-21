2173eb9a9538355402ba0f8febc396a6805ad1a6
roberthsu2003 <roberthsu2003@gmail.com>
2023-12-21 14:31:17 +0800

    modify

--- 三菱/2lesson4_1/2lesson4_1/ViewController.swift
+++ 三菱/2lesson4_1/2lesson4_1/ViewController.swift
@@ -18,6 +18,15 @@ class ViewController: UIViewController {
         let docs = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)
         print(docs.last ?? "沒有路徑")
     }
+    
+    @IBAction func doButton2(_ sender:UIButton){
+        let fileManager = FileManager.default
+        if let documentURL = try? fileManager.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false){
+            print("Document的目錄(url)\(documentURL)")
+            print("Document的目錄(字串)\(documentURL.path())")
+        }
+        
+    }
 
 }
 
