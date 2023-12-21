901407e0387226015f308f174183f1cff9419e2e
roberthsu2003 <roberthsu2003@gmail.com>
2023-12-21 12:57:02 +0800

    modify

--- 參考範例/4_0_1_plist解析_singleton/4_0_1_plist解析/ViewController.swift
+++ 參考範例/4_0_1_plist解析_singleton/4_0_1_plist解析/ViewController.swift
@@ -8,16 +8,51 @@
 import UIKit
 
 class ViewController: UIViewController {
-    var cities:[[String:Any]]
+    var cities:[City]
     
     required init?(coder: NSCoder) {
-        cities = DataSource.main?.cities ?? [[String:Any]]()
+        cities = DataSource.main?.cities ?? [City]()
         super.init(coder: coder)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         print(cities)
+    }
+    
+    @IBAction func save(_ sender:UIButton)
+    {
+        do
+        {
+            let fileManager = FileManager.default
+            let documentFile = try fileManager.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
+            let cityFile = documentFile.appending(path: "citys.plist", directoryHint: .notDirectory)
+            print(cityFile.path(percentEncoded: true))
+            let plister = PropertyListEncoder()
+            try plister.encode(cities).write(to: cityFile, options: .atomic)
+        }
+        catch
+        {
+            print(error)
+        }
+        
+    }
+    
+    @IBAction func retrive(_ sender:UIButton)
+    {
+        do
+        {
+            let fileManager = FileManager.default
+            let documentFile = try fileManager.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
+            let cityFile = documentFile.appending(path: "citys.plist", directoryHint: .notDirectory)
+            let cityData = try Data(contentsOf: cityFile)
+            let cities = try PropertyListDecoder().decode([City].self, from: cityData)
+            print(cities)
+        }
+        catch
+        {
+            print(error)
+        }
     }
    
 
