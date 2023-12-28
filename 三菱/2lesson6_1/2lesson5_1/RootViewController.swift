8b998453e852900de1057c00398a1bc4e47f663d
roberthsu2003 <roberthsu2003@gmail.com>
2023-12-28 14:15:26 +0800

    modify

--- 三菱/2lesson6_1/2lesson5_1/RootViewController.swift
+++ 三菱/2lesson6_1/2lesson5_1/RootViewController.swift
@@ -39,8 +39,7 @@ class RootViewController: UITableViewController {
         //self.tableView.backgroundColor = .brown
         tableView.dataSource = self //預設就為self
         navigationItem.title = "世界城市"
-        print(cities)
-        
+        //print(cities)
         
     }
 
@@ -49,67 +48,50 @@ class RootViewController: UITableViewController {
 extension RootViewController{
     //UITableViewController已經採納UITableViewDataSource
     
-    override func numberOfSections(in tableView: UITableView) -> Int{
-        //多少個section
-        return 1
-    }
+    
     
     override func tableView(
         _ tableView: UITableView,
         numberOfRowsInSection section: Int
     ) -> Int{
         //每個section,多少個row
-        return 100
+        return cities.count
     }
         
     override func tableView(
         _ tableView: UITableView,
         cellForRowAt indexPath: IndexPath
-    ) -> UITableViewCell{
+    ) -> UITableViewCell
+    {
         //給Row資料
         let rowIndex = indexPath.row
-        if #available(iOS 15, *)
-        {
-            var cell = tableView.dequeueReusableCell(withIdentifier: cellID)
-            if cell == nil{
-                //print(rowIndex)
-                cell = UITableViewCell(style: .default, reuseIdentifier: cellID)
-                //cell外觀永遠不會改變的
-                cell?.textLabel?.textColor = .systemRed
-                let imageView = UIImageView(image: UIImage(named: "linen.png"))
-                imageView.contentMode = .scaleToFill
-                cell?.backgroundView = imageView
-                let selectedView = UIView()
-                selectedView.backgroundColor = UIColor.blue.withAlphaComponent(0.2)
-                cell?.selectedBackgroundView = selectedView
-            }
-            //cell外觀會改變的
-            var contentConfiguration = cell?.defaultContentConfiguration()
-            contentConfiguration?.text = "cell-\(rowIndex)"
-            contentConfiguration?.textProperties.color = .systemRed
-            cell?.contentConfiguration = contentConfiguration
-            return cell!
+        print(rowIndex)
+        print("=====city資料==========")
+        let city = cities[rowIndex]
+        let cityName = city["City"] as? String ?? ""
+       
+        var cell = tableView.dequeueReusableCell(withIdentifier: cellID)
+        if cell == nil{
+            //print(rowIndex)
+            cell = UITableViewCell(style: .default, reuseIdentifier: cellID)
+            //cell外觀永遠不會改變的
+            cell?.textLabel?.textColor = .systemRed
+            let imageView = UIImageView(image: UIImage(named: "linen.png"))
+            imageView.contentMode = .scaleToFill
+            cell?.backgroundView = imageView
+            let selectedView = UIView()
+            selectedView.backgroundColor = UIColor.blue.withAlphaComponent(0.2)
+            cell?.selectedBackgroundView = selectedView
         }
-        else
-        {
-            var cell = tableView.dequeueReusableCell(withIdentifier: cellID)
-            if cell == nil{
-                //print(rowIndex)
-                cell = UITableViewCell(style: .default, reuseIdentifier: cellID)
-                //cell外觀永遠不會改變的
-                cell?.textLabel?.textColor = .systemRed
-                let imageView = UIImageView(image: UIImage(named: "linen.png"))
-                imageView.contentMode = .scaleToFill
-                cell?.backgroundView = imageView
-                let selectedView = UIView()
-                selectedView.backgroundColor = UIColor.blue.withAlphaComponent(0.2)
-                cell?.selectedBackgroundView = selectedView
-            }
-            //cell外觀會改變的
-            cell?.textLabel?.text = "cell-\(rowIndex)"
-            return cell!
-            
-        }
+        //cell外觀會改變的
+        var contentConfiguration = cell?.defaultContentConfiguration()
+        contentConfiguration?.text = cityName
+        contentConfiguration?.textProperties.color = .systemRed
+        cell?.contentConfiguration = contentConfiguration
+        return cell!
+    
+    
     }
+    
     
 }
