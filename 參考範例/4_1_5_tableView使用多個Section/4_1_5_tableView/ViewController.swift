4090b33148d97d02a738288263b0fb32baa1800d
roberthsu2003 <roberthsu2003@gmail.com>
2024-01-03 12:14:56 +0800

    modify

--- 參考範例/4_1_5_tableView使用多個Section/4_1_5_tableView/ViewController.swift
+++ 參考範例/4_1_5_tableView使用多個Section/4_1_5_tableView/ViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
     let cellID = "CELL"
+    let headerID = "Header"
     
     @IBOutlet var tableView:UITableView!
     var states:[String:[String]] = DataSource.states
@@ -17,7 +18,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self
-        DataSource.states
+        tableView.delegate = self
+        self.tableView.register(UITableViewHeaderFooterView.self, forHeaderFooterViewReuseIdentifier: headerID)
+        tableView.sectionIndexColor = .white
+        tableView.sectionIndexBackgroundColor = .red
+        tableView.sectionIndexTrackingBackgroundColor = .blue
     }
 
 
@@ -64,6 +69,51 @@ extension ViewController:UITableViewDataSource{
     
     func sectionIndexTitles(for tableView: UITableView) -> [String]?{
         return names
+    }
+}
+
+extension ViewController:UITableViewDelegate{
+    func tableView(
+        _ tableView: UITableView,
+        viewForHeaderInSection section: Int
+    ) -> UIView?{
+        let headerView = tableView.dequeueReusableHeaderFooterView(withIdentifier: headerID)
+        headerView?.tintColor = .red //顯示紅色背景,等一下會被backgroundView蓋住
+        if headerView?.viewWithTag(1) == nil{
+            headerView?.backgroundView = UIView()
+            headerView?.backgroundView?.backgroundColor = .black
+            let lab = UILabel()
+            lab.tag = 1
+            lab.font = UIFont(name: "Georgia-Bold", size: 16)
+            lab.textColor = .green
+            lab.backgroundColor = .clear
+            headerView?.contentView.addSubview(lab)
+            
+            let v = UIImageView()
+            v.tag = 2
+            v.backgroundColor = .black
+            v.image = UIImage(systemName: "square.and.arrow.down")
+            
+            headerView?.contentView.addSubview(v)
+            lab.translatesAutoresizingMaskIntoConstraints = false
+            v.translatesAutoresizingMaskIntoConstraints = false
+            let constraints = [NSLayoutConstraint.constraints(withVisualFormat: "H:|-5-[lab]-10-[v(40)]", metrics: nil, views: ["v":v,"lab":lab]),
+                               NSLayoutConstraint.constraints(withVisualFormat: "V:|-5-[v(40)]", metrics: nil, views: ["v":v,"lab":lab]),
+                               NSLayoutConstraint.constraints(withVisualFormat: "V:|-5-[lab]|", metrics: nil, views: ["v":v,"lab":lab])
+            ].flatMap { $0 }
+            NSLayoutConstraint.activate(constraints)
+        }
+        
+        let lab = headerView?.contentView.viewWithTag(1) as! UILabel
+        lab.text = names[section]
+        return headerView
+    }
+    
+    func tableView(
+        _ tableView: UITableView,
+        heightForHeaderInSection section: Int
+    ) -> CGFloat{
+        return 60
     }
 }
 
