ab41e74615cb267fe759f936930679abf6380b5b
roberthsu2003 <roberthsu2003@gmail.com>
2024-01-04 15:07:28 +0800

    modify

---
+++ 三菱/2lesson7_3/2lesson5_1/RootViewController.swift
@@ -0,0 +1,107 @@
+//
+//  RootViewController.swift
+//  2lesson5_1
+//
+//  Created by 徐國堂 on 2023/12/26.
+//
+
+import UIKit
+
+class RootViewController: UITableViewController {
+    let cellID = "cellID"
+    var cities:[[String:Any]] = []
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        let mainBundle = Bundle.main
+        if let plistURL = mainBundle.url(forResource: "citylist", withExtension: "plist"){
+            //print(plistURL.path())
+            do{
+                let plistData = try Data(contentsOf: plistURL)
+                self.cities = try PropertyListSerialization.propertyList(from: plistData, format: nil) as! [[String:Any]]
+            }
+            catch
+            {
+                print("讀檔錯誤")
+            }
+            
+        }
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //self.tableView.backgroundColor = .brown
+        //tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellID)
+        tableView.register(UINib(nibName: "MyCell", bundle: nil), forCellReuseIdentifier: cellID)
+        tableView.dataSource = self //預設就為self
+        navigationItem.title = "世界城市"
+        //tableView.separatorColor = .red
+        //tableView.separatorStyle = .none
+        tableView.separatorInset = UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 20)
+        tableView.rowHeight = 100
+        //tableView.estimatedRowHeight = 100
+        //print(cities)
+        
+    }
+
+}
+
+extension RootViewController{
+    //UITableViewController已經採納UITableViewDataSource
+    
+    
+    
+    override func tableView(
+        _ tableView: UITableView,
+        numberOfRowsInSection section: Int
+    ) -> Int{
+        //每個section,多少個row
+        return cities.count
+    }
+        
+    override func tableView(
+        _ tableView: UITableView,
+        cellForRowAt indexPath: IndexPath
+    ) -> UITableViewCell
+    {
+        //給Row資料
+        let rowIndex = indexPath.row
+        //print(rowIndex)
+        //print("=====city資料==========")
+        let city = cities[rowIndex]
+        let cityName = city["City"] as? String ?? ""
+        let countryName = city["Country"] as? String ?? ""
+        let imageName = city["Image"] as? String ?? ""
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath)
+        
+        let imageView = cell.viewWithTag(1) as! UIImageView
+        imageView.contentMode = .scaleAspectFill
+        imageView.layer.cornerRadius = 40
+        let sourceImage = UIImage(named: imageName)!
+        let imageRender = UIGraphicsImageRenderer(size: CGSize.init(width: 150, height: 80), format: sourceImage.imageRendererFormat)
+        let smallImage = imageRender.image { (context:UIGraphicsImageRendererContext) in
+            sourceImage.draw(in: CGRect.init(x: 0, y: 0, width: 150, height: 80))
+        }
+        imageView.image = smallImage
+        
+        let titleLabel = cell.viewWithTag(2) as! UILabel
+        titleLabel.text = cityName
+        
+        let subLabel = cell.viewWithTag(3) as! UILabel
+        subLabel.text = countryName
+        
+        
+        return cell
+    
+    
+    }
+    
+    
+}
