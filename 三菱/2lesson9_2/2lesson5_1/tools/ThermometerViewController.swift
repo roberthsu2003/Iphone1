e9c6eea665a4a90eb3dd579da682c86f4076a27b
roberthsu2003 <roberthsu2003@gmail.com>
2024-01-11 16:15:00 +0800

    modify

--- 三菱/2lesson9_2/2lesson5_1/tools/ThermometerViewController.swift
+++ 三菱/2lesson9_2/2lesson5_1/tools/ThermometerViewController.swift
@@ -13,11 +13,18 @@ class ThermometerViewController: UIViewController {
             self.progressView1.progress = 0.0
             Timer.scheduledTimer(timeInterval: 0.4, target: self, selector: #selector(incre1), userInfo: nil, repeats: true)
         }
-        let seg = UISegmentedControl(items: [action1])
+        
+        let action2 = UIAction(title: "progress2") { [self] _ in
+            self.progressView2.progress = 0.0
+            Timer.scheduledTimer(timeInterval: 0.4, target: self, selector: #selector(incre2), userInfo: nil, repeats: true)
+        }
+        
+        let seg = UISegmentedControl(items: [action1,action2])
         return seg
     }()
     
     @IBOutlet var progressView1:UIProgressView!
+    @IBOutlet var progressView2:UIProgressView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,6 +36,18 @@ class ThermometerViewController: UIViewController {
         var value = progressView1.progress
         value += 0.1
         progressView1.progress = value
+        if value >= 1.0{
+            timer.invalidate()
+            seg.isEnabled = true
+            seg.selectedSegmentIndex = -1
+        }
+    }
+    
+    @objc func incre2(_ timer:Timer){
+        seg.isEnabled = false
+        var value = progressView2.progress
+        value += 0.1
+        progressView2.progress = value
         if value >= 1.0{
             timer.invalidate()
             seg.isEnabled = true
