c4cbc6d8be1f0cc9d93e569bf563b8516e4b5b88
roberthsu2003 <roberthsu2003@gmail.com>
2024-01-16 15:57:28 +0800

    modify

---
+++ 三菱/2lesson10_2/2lesson10_2/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  2lesson10_2
+//
+//  Created by 徐國堂 on 2024/1/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func doButton7(_ sender:UIButton){
+        print("doButton7")
+    }
+
+
+}
+
