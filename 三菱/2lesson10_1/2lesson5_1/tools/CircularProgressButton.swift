372018911481900beaeccc82d49e6704607d9894
roberthsu2003 <roberthsu2003@gmail.com>
2024-01-16 14:55:09 +0800

    modify

--- 三菱/2lesson10_1/2lesson5_1/tools/CircularProgressButton.swift
+++ 三菱/2lesson10_1/2lesson5_1/tools/CircularProgressButton.swift
@@ -9,6 +9,11 @@ import UIKit
 
 class CircularProgressButton: UIButton {
     var shapeLayer:CAShapeLayer!
+    var progress:Float = 0{
+        didSet{
+            self.shapeLayer.strokeEnd = CGFloat(progress)
+        }
+    }
     
     override func layoutSubviews() {
         super.layoutSubviews()
@@ -21,6 +26,9 @@ class CircularProgressButton: UIButton {
         layer.strokeColor = UIColor.red.cgColor
         let b = UIBezierPath(ovalIn: self.bounds.insetBy(dx: 3, dy: 3))
         layer.path = b.cgPath
+        layer.strokeStart = 0
+        layer.strokeEnd = 0
+        layer.setAffineTransform(CGAffineTransform.init(rotationAngle: -.pi/2.0))
         self.layer.addSublayer(layer)
         shapeLayer = layer
     }
