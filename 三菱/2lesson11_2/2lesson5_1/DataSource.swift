acda399d6ae90faee0e5645a6c5e03ffd2340b6f
roberthsu2003 <roberthsu2003@gmail.com>
2024-01-18 14:30:46 +0800

    modify

--- 三菱/2lesson11_2/2lesson5_1/DataSource.swift
+++ 三菱/2lesson11_2/2lesson5_1/DataSource.swift
@@ -7,7 +7,26 @@
 
 import Foundation
 class City:NSObject{
+    var city:String
+    var continent:String
+    var country:String
+    var image:String
+    var local:String
+    var lat:Double
+    var long:Double
+    var url:String
     
+    init(city: String, continent: String, country: String, image: String, local: String, lat: Double, long: Double, url: String) {
+        self.city = city
+        self.continent = continent
+        self.country = country
+        self.image = image
+        self.local = local
+        self.lat = lat
+        self.long = long
+        self.url = url
+        super.init()
+    }
 }
 
 class DataSource{
