35d43ba6479a3f1951542d6bf3abd367e257b18f
roberthsu2003 <roberthsu2003@gmail.com>
2024-01-18 14:44:28 +0800

    modify

--- 三菱/2lesson11_2/2lesson5_1/DataSource.swift
+++ 三菱/2lesson11_2/2lesson5_1/DataSource.swift
@@ -6,7 +6,7 @@
 //
 
 import Foundation
-class City:NSObject{
+class City:NSObject,Codable{
     var city:String
     var continent:String
     var country:String
@@ -26,6 +26,34 @@ class City:NSObject{
         self.long = long
         self.url = url
         super.init()
+    }
+    
+    override var description: String{
+        let des = "\(city),\(continent),\(country),\(image),\(local),\(lat),\(long),\(url)"
+        return des
+    }
+    
+    func encode(to encoder: NSCoder){
+        encoder.encode(self.city, forKey: "city")
+        encoder.encode(self.continent, forKey: "continent")
+        encoder.encode(self.country, forKey: "country")
+        encoder.encode(self.image, forKey: "image")
+        encoder.encode(self.local, forKey: "local")
+        encoder.encode(self.lat, forKey: "lat")
+        encoder.encode(self.long, forKey: "long")
+        encoder.encode(self.url, forKey: "url")
+    }
+    
+    required init?(_ decoder:NSCoder) {
+        self.city = decoder.decodeObject(of: NSString.self, forKey: "continent")! as String
+        self.continent = decoder.decodeObject(of: NSString.self, forKey: "city")! as String
+        self.country = decoder.decodeObject(of: NSString.self, forKey: "country")! as String
+        self.image = decoder.decodeObject(of: NSString.self, forKey: "image")! as String
+        self.local = decoder.decodeObject(of: NSString.self, forKey: "local")! as String
+        self.url = decoder.decodeObject(of: NSString.self, forKey: "url")! as String
+        self.long = decoder.decodeDouble(forKey: "long")
+        self.lat = decoder.decodeDouble(forKey: "lat")
+        
     }
 }
 
