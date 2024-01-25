54045693134e05b21931398d21033a7ecd36d979
roberthsu2003 <roberthsu2003@gmail.com>
2024-01-25 14:30:17 +0800

    modify

--- 三菱/2lesson13_1/2lesson5_1/MapViewController.swift
+++ 三菱/2lesson13_1/2lesson5_1/MapViewController.swift
@@ -15,11 +15,14 @@ class MapViewController: UIViewController {
         super.viewDidLoad()
         title = city.city
         let navigationBar = self.navigationController!.navigationBar
+        navigationBar.tintColor = .lightGray
         navigationBar.prefersLargeTitles = true
         //建立bar的外觀
         let navBarAppearance = UINavigationBarAppearance()
+        
         navBarAppearance.backgroundColor = .darkGray
         navBarAppearance.largeTitleTextAttributes = [.foregroundColor:UIColor.white]
+        navBarAppearance.backButtonAppearance = UIBarButtonItemAppearance(style: .plain)
         //navigationBar.standardAppearance = navBarAppearance
         navigationBar.scrollEdgeAppearance = navBarAppearance
         
