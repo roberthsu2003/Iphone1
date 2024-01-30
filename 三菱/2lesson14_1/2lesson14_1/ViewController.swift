fb26375b27cb1af4c86d6b6a50367e9793b73439
roberthsu2003 <roberthsu2003@gmail.com>
2024-01-30 13:40:44 +0800

    modify

---
+++ 三菱/2lesson14_1/2lesson14_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2lesson14_1
+//
+//  Created by 徐國堂 on 2024/1/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
