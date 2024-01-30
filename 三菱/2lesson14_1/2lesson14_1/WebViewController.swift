6e3cd75002cb174347429f37f68f750845f18b4f
roberthsu2003 <roberthsu2003@gmail.com>
2024-01-30 14:40:00 +0800

    modify

--- 三菱/2lesson14_1/2lesson14_1/WebViewController.swift
+++ 三菱/2lesson14_1/2lesson14_1/WebViewController.swift
@@ -22,12 +22,30 @@ class WebViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationItem.title = "Home"
         self.webView = self.view as? WKWebView
+        let activityIndicator = UIActivityIndicatorView(style: .large)
+        activityIndicator.backgroundColor = UIColor.white.withAlphaComponent(0.1)
+        activityIndicator.color = UIColor.white
+        self.webView.addSubview(activityIndicator)
+        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            activityIndicator.centerXAnchor.constraint(equalTo: webView.centerXAnchor),
+            activityIndicator.centerYAnchor.constraint(equalTo: webView.centerYAnchor)
+        ])
+        activityIndicator.startAnimating()
+        
         //取得inance-html/index.html的URL
         if let url = Bundle.main.url(forResource: "index", withExtension: "html", subdirectory: "inance-html"){
             //載入網頁
             webView.loadFileURL(url, allowingReadAccessTo: url)
         }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.hidesBarsOnSwipe = true
+        //self.navigationController?.hidesBarsOnTap = true
     }
 
 
