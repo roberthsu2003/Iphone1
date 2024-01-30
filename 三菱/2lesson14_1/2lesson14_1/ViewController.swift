d3bce34169552581bbb4ac7b759a98d58c7d3276
roberthsu2003 <roberthsu2003@gmail.com>
2024-01-30 13:49:32 +0800

    modify

--- 三菱/2lesson14_1/2lesson14_1/ViewController.swift
+++ 三菱/2lesson14_1/2lesson14_1/ViewController.swift
@@ -11,7 +11,10 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        navigationItem.title = "WebView"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(primaryAction: UIAction(title: "static", handler: { _ in
+            print("Hello")
+        }))
     }
 
 
