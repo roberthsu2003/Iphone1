aeb4d061a4731ee1c63fdc5b7ee296798bc3d471
roberthsu2003 <roberthsu2003@gmail.com>
2024-01-30 14:20:31 +0800

    modify

--- 三菱/2lesson14_1/2lesson14_1/WebViewController.swift
+++ 三菱/2lesson14_1/2lesson14_1/WebViewController.swift
@@ -14,12 +14,20 @@ class WebViewController: UIViewController {
     override func loadView() {
         super.loadView()
         let config = WKWebViewConfiguration()
+        let preferences = WKWebpagePreferences()
+        preferences.allowsContentJavaScript = true
+        config.defaultWebpagePreferences = preferences
         self.view = WKWebView(frame: .zero, configuration: config)
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.webView = self.view as? WKWebView
+        //取得inance-html/index.html的URL
+        if let url = Bundle.main.url(forResource: "index", withExtension: "html", subdirectory: "inance-html"){
+            //載入網頁
+            webView.loadFileURL(url, allowingReadAccessTo: url)
+        }
     }
 
 
