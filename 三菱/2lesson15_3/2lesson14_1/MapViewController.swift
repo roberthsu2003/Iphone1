63cb37f413a5ca3f0c106a1102ea61714d71b51a
roberthsu2003 <roberthsu2003@gmail.com>
2024-02-01 15:55:18 +0800

    modify

--- 三菱/2lesson15_3/2lesson14_1/MapViewController.swift
+++ 三菱/2lesson15_3/2lesson14_1/MapViewController.swift
@@ -10,11 +10,22 @@ import MapKit
 
 class MapViewController: UIViewController {
     @IBOutlet var mapView:MKMapView!
+    //停車場
+    let parkLoc = CLLocationCoordinate2DMake(24.157603, 120.667282)
+    //hotel
+    let hotelLoc = CLLocationCoordinate2DMake(24.154901, 120.661713)
+    //台中國立自然科學博物館
+    let museumLoc = CLLocationCoordinate2DMake(24.15811, 120.66862)
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.mapView.tintColor = .green
+        //let span = MKCoordinateSpan(latitudeDelta: 0.015, longitudeDelta: 0.015)
+        //MKCoordinateRegion(center: museumLoc, span:span )
+        
+        let reg = MKCoordinateRegion(center: museumLoc, latitudinalMeters: 1600, longitudinalMeters: 1600)
+        self.mapView.region = reg
     }
     
 
