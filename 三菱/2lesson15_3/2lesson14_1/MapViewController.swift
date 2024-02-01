bdd2c5bf8e837631a4724ec6d3422554807e0539
roberthsu2003 <roberthsu2003@gmail.com>
2024-02-01 15:43:10 +0800

    modify

--- 三菱/2lesson15_3/2lesson14_1/MapViewController.swift
+++ 三菱/2lesson15_3/2lesson14_1/MapViewController.swift
@@ -6,9 +6,11 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
-
+    @IBOutlet var mapView:MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -16,14 +18,8 @@ class MapViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func showMapApp(_ sender:UIButton){
+        print("Show Map App")
     }
-    */
 
 }
