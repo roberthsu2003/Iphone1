7476beb0922f6273e4fc4963c9acfdbae3774bc8
roberthsu2003 <roberthsu2003@gmail.com>
2024-02-01 15:37:40 +0800

    modify

---
+++ 三菱/2lesson15_3/2lesson14_1/MapViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MapViewController.swift
+//  2lesson15_3
+//
+//  Created by 徐國堂 on 2024/2/1.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ 三菱/2lesson15_3/2lesson14_1/WebViewController.swift
@@ -0,0 +1,208 @@
+//
+//  WebViewController.swift
+//  2lesson14_1
+//
+//  Created by 徐國堂 on 2024/1/30.
+//
+
+import UIKit
+import WebKit
+import SafariServices
+
+class WebViewController: UIViewController {
+    var webView:WKWebView!
+    var obs = Set<NSKeyValueObservation>()
+    var activityIndicator:UIActivityIndicatorView!
+    
+    override func loadView() {
+        super.loadView()
+        let config = WKWebViewConfiguration()
+        config.userContentController.add(self, name: "toApp")
+        let preferences = WKWebpagePreferences()
+        preferences.allowsContentJavaScript = true
+        config.defaultWebpagePreferences = preferences
+        self.view = WKWebView(frame: .zero, configuration: config)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.title = "Home"
+        self.webView = self.view as? WKWebView
+        self.activityIndicator = UIActivityIndicatorView(style: .large)
+        self.activityIndicator.backgroundColor = UIColor(white: 0.1, alpha: 0.5)
+        self.activityIndicator.color = UIColor.white
+        self.webView.addSubview(activityIndicator)
+        self.activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            self.activityIndicator.centerXAnchor.constraint(equalTo: webView.centerXAnchor),
+            self.activityIndicator.centerYAnchor.constraint(equalTo: webView.centerYAnchor)
+        ])
+        //KVO
+        let kvo = self.webView.observe(\.isLoading, options: .new) { [unowned self](webView:WKWebView, change:NSKeyValueObservedChange<Bool>) in
+            if let val = change.newValue{
+                if val{
+                    print("動畫開始")
+                    activityIndicator.startAnimating()
+                }else{
+                    activityIndicator.stopAnimating()
+                    print("動畫結束")
+                }
+            }
+        }
+        
+        obs.insert(kvo)
+        
+        
+        //取得inance-html/index.html的URL
+        if let url = Bundle.main.url(forResource: "index", withExtension: "html", subdirectory: "inance-html"){
+            //載入網頁
+            webView.loadFileURL(url, allowingReadAccessTo: url)
+        }
+        
+        //加入手勢
+        let swipe = UISwipeGestureRecognizer(target: self, action: #selector(swiped))
+        swipe.direction = .right
+        webView.addGestureRecognizer(swipe)
+        
+        //導覽的代理人機制
+        webView.navigationDelegate = self
+        
+        //加入toolbarItem
+        self.setToolbarItems([
+            UIBarButtonItem(systemItem: .flexibleSpace)
+            ,
+            UIBarButtonItem(title: "Home", primaryAction: UIAction.init(handler: { _ in
+                self.webView.evaluateJavaScript("goHome();") { (result:Any?, error:Error?) in
+                    guard let result = result as? String,error == nil else{
+                        print("錯誤")
+                        return
+                    }
+                    self.title = result
+                }
+            })),
+            UIBarButtonItem(title: "contact", primaryAction: UIAction.init(handler: { _ in
+                self.webView.evaluateJavaScript("goContact();") { (result:Any?, error:Error?) in
+                    guard let result = result as? String,error == nil else{
+                        print("錯誤")
+                        return
+                    }
+                    self.title = result
+                }
+            })),
+            UIBarButtonItem(title: "service", primaryAction: UIAction.init(handler: { _ in
+                self.webView.evaluateJavaScript("goService();") { (result:Any?, error:Error?) in
+                    guard let result = result as? String,error == nil else{
+                        print("錯誤")
+                        return
+                    }
+                    self.title = result
+                }
+            })),
+            UIBarButtonItem(title: "about", primaryAction: UIAction.init(handler: { _ in
+                self.webView.evaluateJavaScript("goAbout();") { (result:Any?, error:Error?) in
+                    guard let result = result as? String,error == nil else{
+                        print("錯誤")
+                        return
+                    }
+                    self.title = result
+                }
+            })),
+            UIBarButtonItem(systemItem: .flexibleSpace)
+            
+        ], animated: false)
+    }
+    
+    @objc func swiped(_ sender:UISwipeGestureRecognizer){
+        print("swiped")
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.hidesBarsOnSwipe = true
+        //self.navigationController?.hidesBarsOnTap = true
+        self.navigationController?.isToolbarHidden = false
+        
+        
+    }
+
+
+}
+
+extension WebViewController:WKScriptMessageHandler{
+    func userContentController(
+        _ userContentController: WKUserContentController,
+        didReceive message: WKScriptMessage
+    ){
+        if message.name == "toApp"{
+            if let body = message.body as? String{
+                if body == "contact"{
+                    let alertController = UIAlertController(title: "聯絡方式", message: "AM:9:00 to PM:6:00", preferredStyle: .alert)
+                    let taipeiAction = UIAlertAction(title: "台北公司", style: .default) { _ in
+                        if let phoneCallURL = URL(string: "tel://0976878878"){
+                            let application = UIApplication.shared
+                            if application.canOpenURL(phoneCallURL){
+                                application.open(phoneCallURL)
+                            }
+                        }
+                        
+                    }
+                    
+                    let taichanAction = UIAlertAction(title: "台中公司", style: .default) { _ in
+                        print("台中公司")
+                    }
+                    
+                    alertController.addAction(taipeiAction)
+                    alertController.addAction(taichanAction)
+                    self.present(alertController, animated: true)
+                    
+                }
+            }
+        }
+    }
+}
+
+extension WebViewController:WKNavigationDelegate{
+    func webView(
+        _ webView: WKWebView,
+        decidePolicyFor navigationAction: WKNavigationAction,
+        preferences: WKWebpagePreferences,
+        decisionHandler: @escaping (WKNavigationActionPolicy, WKWebpagePreferences) -> Void
+    ){
+        if navigationAction.navigationType == .linkActivated{
+            if let url = navigationAction.request.url{
+                if url.lastPathComponent == "goBack"{
+                    self.navigationController?.popViewController(animated: true)
+                    decisionHandler(.cancel,preferences)
+                    return
+                }
+                
+                if url.absoluteString == "http://tidbits.com/article/11682"{
+                    let svc = SFSafariViewController(url: url)
+                    svc.delegate = self
+                    self.present(svc, animated: true)
+                    decisionHandler(.cancel,preferences)
+                    return
+                    
+                }
+            }
+            
+        }
+        
+        decisionHandler(.allow,preferences)
+        
+        
+        
+        
+    }
+}
+
+extension WebViewController:SFSafariViewControllerDelegate{
+    func safariViewController(
+        _ controller: SFSafariViewController,
+        didCompleteInitialLoad didLoadSuccessfully: Bool
+    ){
+        print("safariViewContorller載入完成")
+    }
+    
+}
