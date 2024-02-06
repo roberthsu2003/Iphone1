9bd2d5561b238d9aef572fb778d8e7c2e26cb981
roberthsu2003 <roberthsu2003@gmail.com>
2024-02-06 15:59:48 +0800

    modify

--- 三菱/2lesson16_1/2lesson16_1/ViewController.swift
+++ 三菱/2lesson16_1/2lesson16_1/ViewController.swift
@@ -19,7 +19,19 @@ class ViewController: UIViewController {
                 print("下載錯誤")
                 return
             }
-            print("下載成功")
+            let status = (response as! HTTPURLResponse).statusCode
+            guard status == 200 else{
+                print("下載成功但有錯誤訊息")
+                return
+            }
+            
+            guard let imageData = try? Data(contentsOf: url) else{
+                print("資料無法轉換")
+                return
+            }
+            
+            let image = UIImage(data: imageData)
+            print(image!)
         }
         
         task.resume()
