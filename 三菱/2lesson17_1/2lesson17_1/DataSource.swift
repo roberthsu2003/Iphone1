b1dcf452ac5802d827abce34a8f731a7651e4ec6
roberthsu2003 <roberthsu2003@gmail.com>
2024-02-20 14:10:30 +0800

    modify

--- 三菱/2lesson17_1/2lesson17_1/DataSource.swift
+++ 三菱/2lesson17_1/2lesson17_1/DataSource.swift
@@ -8,8 +8,14 @@
 import Foundation
 
 class DataSource{
+    static var session:URLSession?
+    
     static func startDownLoad(){
-        print("執行下載")
+        let youbike = "https://youbike-json.onrender.com/youbike"
+        let configuration = URLSessionConfiguration.default
+        configuration.allowsExpensiveNetworkAccess = true
+        configuration.timeoutIntervalForRequest = 180
+        session = URLSession(configuration: configuration)
     }
     
     private init(){
