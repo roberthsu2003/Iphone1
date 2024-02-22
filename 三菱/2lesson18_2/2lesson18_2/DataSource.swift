50e31ef6b1d1eaa78f93a1bc2d8cac4c568b80fd
roberthsu2003 <roberthsu2003@gmail.com>
2024-02-22 15:01:11 +0800

    modify

--- 三菱/2lesson18_2/2lesson18_2/DataSource.swift
+++ 三菱/2lesson18_2/2lesson18_2/DataSource.swift
@@ -6,10 +6,16 @@
 //
 
 import Foundation
-class DataSource{
+class DataSource:NSObject{
     static var main = DataSource()
     
-    private init(){
-        print("DataSource的實體被建立了")
+    private override init(){
+        super.init()
+        download()
+    }
+    
+    func download(){
+        let AQI_URL = "https://data.moenv.gov.tw/api/v2/aqx_p_488?api_key=e8dd42e6-9b8b-43f8-991e-b3dee723a52d&limit=1000&sort=datacreationdate%20desc&format=JSON"
+        print(AQI_URL)
     }
 }
