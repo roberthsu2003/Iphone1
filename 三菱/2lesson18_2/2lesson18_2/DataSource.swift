385b642f5412a4bb2b8ffbc17391d071fccfaff8
roberthsu2003 <roberthsu2003@gmail.com>
2024-02-22 15:38:43 +0800

    modify

--- 三菱/2lesson18_2/2lesson18_2/DataSource.swift
+++ 三菱/2lesson18_2/2lesson18_2/DataSource.swift
@@ -8,6 +8,7 @@
 import Foundation
 class DataSource:NSObject{
     static var main = DataSource()
+    var urlSession:URLSession!
     
     private override init(){
         super.init()
@@ -15,7 +16,28 @@ class DataSource:NSObject{
     }
     
     func download(){
-        let AQI_URL = "https://data.moenv.gov.tw/api/v2/aqx_p_488?api_key=e8dd42e6-9b8b-43f8-991e-b3dee723a52d&limit=1000&sort=datacreationdate%20desc&format=JSON"
-        print(AQI_URL)
+        guard let AQI_URL = URL(string: "https://data.moenv.gov.tw/api/v2/aqx_p_488?api_key=e8dd42e6-9b8b-43f8-991e-b3dee723a52d&limit=1000&sort=datacreationdate%20desc&format=JSON") else{
+            print("url有錯誤")
+            return
+        }
+        let AQI_URLRequest = URLRequest(url: AQI_URL)
+        let configuration = URLSessionConfiguration.default
+        configuration.allowsExpensiveNetworkAccess = true
+        configuration.timeoutIntervalForRequest = 120
+        urlSession = URLSession(configuration: configuration, delegate: self as URLSessionDownloadDelegate, delegateQueue: OperationQueue.main)
+        let downloadTask = urlSession.downloadTask(with: AQI_URLRequest)
+        downloadTask.resume()
     }
+}
+
+extension DataSource:URLSessionDownloadDelegate{
+    func urlSession(
+        _ session: URLSession,
+        downloadTask: URLSessionDownloadTask,
+        didFinishDownloadingTo location: URL
+    ){
+        print("下載完成\(location)")
+    }
+    
+    
 }
