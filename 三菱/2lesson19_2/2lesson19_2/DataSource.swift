033fe289e6e94929b14dd2983804ee07868d09fe
roberthsu2003 <roberthsu2003@gmail.com>
2024-02-27 16:27:14 +0800

    modify

--- 三菱/2lesson19_2/2lesson19_2/DataSource.swift
+++ 三菱/2lesson19_2/2lesson19_2/DataSource.swift
@@ -16,6 +16,23 @@ class DataSource{
             return
         }
         //print(sqliteURL.path())
+        copySqliteToDocument(sqliteURL: sqliteURL)
+    }
+    
+    private func copySqliteToDocument(sqliteURL:URL){
+        do{
+            let fileManager = FileManager.default
+            let documentURL = try fileManager.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
+            print("document路徑",documentURL)
+            let targetURL = documentURL.appending(path: "city.db", directoryHint: .notDirectory)
+            if !fileManager.fileExists(atPath: targetURL.path()){
+                try fileManager.copyItem(at: sqliteURL, to: targetURL)
+                print("複制city0811.db至document/city.db成功")
+            }
+            
+        }catch{
+            print(error.localizedDescription)
+        }
         
     }
 }
