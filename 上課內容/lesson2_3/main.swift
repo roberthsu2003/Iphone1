4ebd379cf948bbfb8871606aa5d4254feb824d15
徐國堂 <roberthsu2003@gmail.com>
2024-09-06 15:59:54 +0800

    Update main.swift

--- 上課內容/lesson2_3/main.swift
+++ 上課內容/lesson2_3/main.swift
@@ -17,3 +17,21 @@ switch someCharacter{
 	default:
 		print("other")
 }
+
+//範圍運算子
+
+let approximateCount = 3
+switch approximateCount{
+	case 0:
+		print("0")
+	case 1..<5:
+		print("1~4")
+	case 5..<12:
+		print("5~11")
+	case 12..<100:
+		print("12~99")
+	case 100..<1000:
+		print("100~999")
+	default:
+		print("1000~..")
+}
