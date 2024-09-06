f09e79a31488e386249fd2c389cc04e5ed48ba2e
徐國堂 <roberthsu2003@gmail.com>
2024-09-06 11:19:25 +0800

    Update main.swift

--- 上課內容/lesson1_2/main.swift
+++ 上課內容/lesson1_2/main.swift
@@ -18,5 +18,27 @@ let pointOneFourOneFiveNine = 0.14159
 let pi = Double(three) + pointOneFourOneFiveNine
 print(pi)
 
+//tuple
+let http404Error = (404, "Not Found")
+print(http404Error)
+
+//拆解法
+let (statusCode, statusMessage) = http404Error
+print(statusCode)
+print(statusMessage)
+
+let (justTheStatusCode, _) = http404Error
+print(justTheStatusCode)
+
+//透過索引編號
+print(http404Error.0)
+print(http404Error.1)
+
+//透過key
+let http200Status = (statusCode:200, description:"ok")
+print(http200Status.statusCode)
+print(http200Status.description)
+
+
 
 
