ac1708cc02d19863c71f26e96af4ddb749c0e973
徐國堂 <roberthsu2003@gmail.com>
2024-09-06 10:56:28 +0800

    Create main.swift

---
+++ 上課內容/lesson1_2/main.swift
@@ -0,0 +1,22 @@
+let paddedDouble = 000123.456
+let oneMillion = 1_000_000
+let justOverOneMillion = 1_000_000.000_000_1
+
+/*
+錯誤的
+//let cannotBeNegative:UInt8 = -1
+//let tooBig:Int8 = Int8.max + 1
+*/
+
+
+let twoThousand:UInt16 = 2_000
+let one:UInt8 = 1
+let twoThousandAndOne = twoThousand + UInt16(one)
+
+let three = 3
+let pointOneFourOneFiveNine = 0.14159
+let pi = Double(three) + pointOneFourOneFiveNine
+print(pi)
+
+
+
