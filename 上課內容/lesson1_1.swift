d5799f2542af15f741b8d440ed06a4971f03fec1
徐國堂 <roberthsu2003@gmail.com>
2024-09-06 10:30:32 +0800

    Update lesson1_1.swift

--- 上課內容/lesson1_1.swift
+++ 上課內容/lesson1_1.swift
@@ -14,3 +14,28 @@ friendlyWelcome = "Bonjour!"
 
 print("The current value of friendlyWelcome is \(friendlyWelcome)")
 
+//Integer 
+let minValue = UInt8.min
+let maxValue = UInt8.max
+print("UInt8最小值是:\(minValue)")
+print("UInt8最大值是:\(maxValue)")
+
+//Integer 
+let minValue1 = Int.min
+let maxValue1 = Int.max
+print("Int64最小值是:\(minValue1)")
+print("Int64最大值是:\(maxValue1)")
+
+//浮點數
+//Double 64bit
+//Float 32bit
+
+let meaningOfLife = 42.0 //Double
+let meaningOfLife1:Float = 42.0 //Float
+
+
+
+
+
+
+
