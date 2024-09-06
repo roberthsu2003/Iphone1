b50c5d27d4b0e2996ac153426f8a632072f93ca9
徐國堂 <roberthsu2003@gmail.com>
2024-09-06 16:17:25 +0800

    Update main.swift

--- 上課內容/lesson2_3/main.swift
+++ 上課內容/lesson2_3/main.swift
@@ -34,4 +34,26 @@ switch approximateCount{
 		print("100~999")
 	default:
 		print("1000~..")
+
+}
+
+//範圍運算子,tuple
+let somePoint = (9, 7)
+
+switch somePoint{
+	case (0, 0):
+	   print("0,0")
+	
+	case (_, 0):
+		print("x axis")
+
+	case (0,_):
+	  print("y axis")
+
+	case (-2...2,-2...2):
+		print("在範圍內")
+
+	default:
+		print("超出範圍")
+	
 }
