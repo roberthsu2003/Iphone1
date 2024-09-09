05e7c0484a413cf069ba075dfb845106358f0b35
徐國堂 <roberthsu2003@gmail.com>
2024-09-09 13:26:42 +0800

    Create main.swift

---
+++ 上課內容/lesson4_1/main.swift
@@ -0,0 +1,14 @@
+enum CompassPoint{
+	case north
+	case south
+	case east
+	case west
+}
+
+enum Planet{
+	case mercury, venus, earth, mars, jupiter, saturn, uranus, neptune
+}
+
+//var directionToHead:CompassPoint = .west
+var directionToHead = CompassPoint.west
+directionToHead = .east
