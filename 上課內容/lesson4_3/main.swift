a827bc358ab5828654b64dd39a64467bdb8626f3
徐國堂 <roberthsu2003@gmail.com>
2024-09-09 16:00:43 +0800

    Update main.swift

--- 上課內容/lesson4_3/main.swift
+++ 上課內容/lesson4_3/main.swift
@@ -27,3 +27,41 @@ let manager = DataManager()
 manager.data.append("Some data")
 manager.data.append("Some more data")
 print(manager.importer.filename)
+
+//computed proerty
+
+struct Point{
+	var x = 0.0, y = 0.0
+}
+
+struct Size{
+	var width=0.0, height=0.0
+}
+
+struct Rect{
+	var origin = Point()
+	var size = Size()
+	//computer property,必需明確宣告型別
+	var center:Point{
+		get{
+			let centerX = origin.x + (size.width / 2)
+			let centerY = origin.y + (size.height / 2)
+			return Point(x: centerX, y: centerY)
+		}
+
+		set(newCenter){
+			origin.x = newCenter.x - (size.width / 2)
+			origin.y = newCenter.y - (size.height / 2)
+		}
+		
+	}
+}
+
+var square = Rect(origin: Point(x: 0.0, y: 0.0) ,
+		 size:Size(width: 10.0, height: 10.0)
+		)
+let initialSquareCenter = square.center
+square.center = Point(x: 15.0, y: 15.0)
+print(square.origin.x)
+print(square.origin.y)
+
