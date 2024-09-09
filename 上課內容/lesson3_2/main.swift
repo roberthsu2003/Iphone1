fc4d6134e1de903459b1339c489906a8de01288f
徐國堂 <roberthsu2003@gmail.com>
2024-09-09 10:37:21 +0800

    Create main.swift

---
+++ 上課內容/lesson3_2/main.swift
@@ -0,0 +1,36 @@
+func greet(person:String) -> String{
+	let greeting = "Hello, " + person + "!"
+	return greeting
+}
+
+
+func greet(for person:String) -> String{
+	let greeting = "Hello, " + person + "!"
+	return greeting
+}
+
+print(greet(for: "Dave"))
+
+func someFunction(firstParamenterName:Int, secondParamenterName:Int){
+	
+}
+
+someFunction(firstParamenterName: 30, secondParamenterName: 2)
+
+func someFunction(argumentLabel paramerterName:Int){
+}
+
+someFunction(argumentLabel: 8)
+
+func greet(person:String, from hometown:String) -> String{
+	return "Hello \(person)! Glad you could visit from \(hometown)."
+}
+
+print(greet(person: "Bill", from: "Cupertino"))
+
+
+func someFunction(_ firstParamenterName:Int, secondParamenterName:Int){
+
+}
+
+someFunction(1, secondParamenterName: 87)
