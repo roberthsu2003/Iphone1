d381aa31fcf27011a907565408b61703381994c5
徐國堂 <roberthsu2003@gmail.com>
2024-09-09 10:18:19 +0800

    Update main.swift

--- 上課內容/lesson3_1/main.swift
+++ 上課內容/lesson3_1/main.swift
@@ -45,3 +45,36 @@ func minMax(array:[Int]) -> (min:Int,max:Int){
 
 let bounds = minMax(array: [8, -6, 2, 109, 3, 71])
 print("min is \(bounds.min) and max is \(bounds.max)")
+
+
+func minMax1(array:[Int]) -> (min:Int,max:Int)?{
+	if array.isEmpty { return nil }
+	var currentMin = array[0]
+	var currentMax = array[0]
+	for value in array[1 ..< array.count]{
+		if value < currentMin{
+			currentMin = value
+		}else if value > currentMax{
+			currentMax = value
+		}
+	}
+
+	return (currentMin, currentMax)	
+}
+/*沒有檢查optional資料
+let bounds1 = minMax1(array: [8, -6, 2, 109, 3, 71])
+print("min is \(bounds1!.min) and max is \(bounds1!.max)")
+*/
+/* nil後面不可以是點運算子
+let bounds2 = minMax1(array: [])
+print("min is \(bounds2!.min) and max is \(bounds2!.max)")
+*/
+
+//optional binding
+if let bounds1 = minMax1(array: [8, -6, 2, 109, 3, 71]){
+	print("min is \(bounds1.min) and max is \(bounds1.max)")
+}else{
+	print("array是空的")
+}
+
+
