4a7a62ba5e0f3ff9aefee6f21b466f682bcb2255
徐國堂 <roberthsu2003@gmail.com>
2024-09-09 13:35:05 +0800

    Update main.swift

--- 上課內容/lesson4_1/main.swift
+++ 上課內容/lesson4_1/main.swift
@@ -12,3 +12,27 @@ enum Planet{
 //var directionToHead:CompassPoint = .west
 var directionToHead = CompassPoint.west
 directionToHead = .east
+
+//列舉變數是使用那一個成員
+//switch...case
+
+switch directionToHead{
+	case .north:
+		print("north")
+	case .south:
+		print("south")
+	case .east:
+		print("east")
+	case .west:
+		print("west")
+}
+
+let somePlanet = Planet.earth
+//somePlanet = .mars
+
+switch somePlanet{
+	case .earth:
+	print("地球")
+	default:
+	print("沒有人住的星球")
+}
