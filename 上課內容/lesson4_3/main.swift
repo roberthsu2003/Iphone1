97d3c9721b9159f9bad1df0f93631bbf457cd721
徐國堂 <roberthsu2003@gmail.com>
2024-09-09 16:08:11 +0800

    Update main.swift

--- 上課內容/lesson4_3/main.swift
+++ 上課內容/lesson4_3/main.swift
@@ -65,3 +65,16 @@ square.center = Point(x: 15.0, y: 15.0)
 print(square.origin.x)
 print(square.origin.y)
 
+
+struct Cuboid{
+	var width = 0.0, height = 0.0, depth = 0.0
+	var volume:Double{ //read-only computed property
+		
+			return width * height * depth
+		
+	}
+}
+
+let fourByFiveByTwo = Cuboid(width: 4.0, height: 5.0, depth: 2.0)
+print(fourByFiveByTwo.volume)
+
