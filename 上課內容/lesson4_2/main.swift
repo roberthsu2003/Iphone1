07a42e88a397d253f3292bf2302711b5fee43662
徐國堂 <roberthsu2003@gmail.com>
2024-09-09 14:53:02 +0800

    Update main.swift

--- 上課內容/lesson4_2/main.swift
+++ 上課內容/lesson4_2/main.swift
@@ -25,3 +25,23 @@ print(someVideoMode.resolution.width)
 print(someVideoMode.resolution.height)
 print(someVideoMode.interlaced)
 print(someVideoMode.name ?? "是nil")
+
+//結構
+//default init
+//智慧型成員初始化
+let hd = Resolution(width: 1920, height: 1080)
+//value type 是把hd的實體,copy一份給sinema
+var cinema = hd
+cinema.width = 2048
+print(hd.width)
+print(cinema.width)
+
+
+let someVideoMode1 = someVideoMode
+
+someVideoMode1.resolution.width = 1920
+someVideoMode1.resolution.height = 1080
+
+print(someVideoMode.resolution.width)
+print(someVideoMode.resolution.height)
+
