2092d3f4b48bf06e48cb4f3c9f433bf2d4d85429
roberthsu2003 <roberthsu2003@gmail.com>
2024-11-06 09:52:48 +0800

    modify

--- 士林1/lesson1.playground/Contents.swift
+++ 士林1/lesson1.playground/Contents.swift
@@ -2,3 +2,27 @@ import UIKit
 
 var greeting = "Hello, playground"
 print(greeting)
+
+//simple Value
+var myVariable = 42
+myVariable = 100
+let myConstant = 42
+//myConstant = 92
+
+let implicitInteger = 70
+let implicitDouble = 70.0
+let explicitDouble: Double = 70
+
+let label = "The width is "
+let width = 94
+
+let widthLable = label + String(width)
+
+let apples = 3
+let oranges = 5
+let appleSummary = "I have \(apples) apples and \(oranges) oranges."
+
+
+
+
+
