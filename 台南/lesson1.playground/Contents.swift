27b27bd590b0a230573bef650014bb7a8c91980b
roberthsu2003 <roberthsu2003@gmail.com>
2025-01-09 20:36:05 +0800

    modify

--- 台南/lesson1.playground/Contents.swift
+++ 台南/lesson1.playground/Contents.swift
@@ -20,6 +20,34 @@ let implicitInteger = 70 //透過純值推測資料類型
 let implicitDouble = 70.0 //透過純值推測資料類型
 let explicitDouble: Double = 70 //明確告知資料類型
 
+let label = "The width is "
+let width = 94
+
+label + String(width)
+
+/*
+var x = 0.0
+var y = 0.0
+var z = 0.0
+*/
+
+var x = 0.0, y = 0.0, z = 0.0
+print(x, y, z)
+
+//明確宣告(type annotation)
+//var welcomeMessage:String
+//welcomeMessage = "Hello World!"
+
+//明確宣告,同時給值
+//var welcomeMessage:String = "Hello World!"
+
+//透過推測,給予資料類型
+var welcomeMessage = "Hello World!"
+
+
+
+
+
 
 
 
