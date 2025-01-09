cc830f3e4319a565b90718700c4af5cec59f59e3
roberthsu2003 <roberthsu2003@gmail.com>
2025-01-09 20:20:08 +0800

    modify

--- 台南/lesson1.playground/Contents.swift
+++ 台南/lesson1.playground/Contents.swift
@@ -8,12 +8,18 @@ var myVariable = 42
 print(myVariable)
 
 myVariable = 100
+myVariable = 101
 
 //建立常數
 let myConstant = 42
 
 //常數不可以改變裏面的值
 //myConstant = 100
+
+let implicitInteger = 70 //透過純值推測資料類型
+let implicitDouble = 70.0 //透過純值推測資料類型
+let explicitDouble: Double = 70 //明確告知資料類型
+
 
 
 
