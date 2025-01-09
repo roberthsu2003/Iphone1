15bf497efb53d4300b525aa1db9c1f2ea19007ff
roberthsu2003 <roberthsu2003@gmail.com>
2025-01-09 20:04:18 +0800

    modify

--- 台南/lesson1.playground/Contents.swift
+++ 台南/lesson1.playground/Contents.swift
@@ -2,3 +2,19 @@ import UIKit
 
 var greeting = "Hello, playground"
 print(greeting)
+
+//建立變數
+var myVariable = 42
+print(myVariable)
+
+myVariable = 100
+
+//建立常數
+let myConstant = 42
+
+//常數不可以改變裏面的值
+//myConstant = 100
+
+
+
+
