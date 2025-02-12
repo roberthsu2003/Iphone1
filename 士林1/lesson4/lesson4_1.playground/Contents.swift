c09ab1a43b7e9bc43677a23aca386456cc07f70f
roberthsu2003 <roberthsu2003@gmail.com>
2025-02-12 10:13:51 +0800

    modify

--- 士林1/lesson4/lesson4_1.playground/Contents.swift
+++ 士林1/lesson4/lesson4_1.playground/Contents.swift
@@ -28,4 +28,44 @@ manager.data.append("Some data")
 manager.data.append("Some more data")
 manager.importer.filename
 
+//computed property 計算
+struct Point{
+    var x = 0.0
+    var y = 0.0
+}
+
+struct Size{
+    var width = 0.0, height = 0.0
+}
+
+struct Rect{
+    var origin = Point()
+    var size = Size()
+    var center:Point{ //computed property
+        get{ //被別人取出,要有return
+            let centerX = origin.x + size.width / 2
+            let centerY = origin.y + size.height / 2
+            return Point(x:centerX, y:centerY)
+        }
+        
+        set(newCenter){ //要有參數
+            origin.x = newCenter.x - size.width / 2
+            origin.y = newCenter.y - size.height / 2
+        }
+    }
+    
+}
+
+var square = Rect(origin: Point(x: 0, y: 0), size: Size(width: 10.0, height: 10.0))
+let initialSquareCenter = square.center
+
+square.center = Point(x: 10.0, y: 10.0)
+print("square.origin.x \(square.origin.x)")
+print("square.origin.y \(square.origin.y)")
+
+
+
+
+
+
 
