9e97c25dd4fd61d529c147d6afcbea3efa038642
roberthsu2003 <roberthsu2003@gmail.com>
2025-02-13 19:19:51 +0800

    modify

---
+++ 台南/2025_02_13/lesson1.playground/Contents.swift
@@ -0,0 +1,29 @@
+import UIKit
+
+//指定運算子
+let b = 10
+
+var a = b
+
+a = 20
+
+let (x, y) = (10, 20)
+x
+
+y
+
+1 + 2
+
+5 - 2
+
+2 * 3
+
+10.0 / 2
+
+let n = 2
+
+10.0 / Double(n)
+
+"Hello" + " World"
+
+
