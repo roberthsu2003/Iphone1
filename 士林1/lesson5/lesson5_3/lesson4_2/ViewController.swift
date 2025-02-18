dcccf66c34dd2b9c50622860a2ffdf34ede9c674
roberthsu2003 <roberthsu2003@gmail.com>
2025-02-18 11:59:15 +0800

    modify

--- 士林1/lesson5/lesson5_3/lesson4_2/ViewController.swift
+++ 士林1/lesson5/lesson5_3/lesson4_2/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        /*
         //force unwrapping
         print(height_textField.text!)
         //optional binding
@@ -30,10 +31,32 @@ class ViewController: UIViewController {
         }
         
         print(weight_value)
+        */
+        height_textField.keyboardType = .numberPad
+        weight_textField.keyboardType = .numberPad
+        caculate_button.addTarget(self, action: #selector(caculate), for: .touchUpInside)
         
+    }
+    
+    @objc func caculate(){
+        /*
+        guard !height_textField.text!.isEmpty, !weight_textField.text!.isEmpty else{
+            print("不可以是空的")
+            return
+        }
+         */
+        guard let height_textField_value = height_textField.text,!height_textField_value.isEmpty else{
+            print("身高不可以是空的")
+            return
+        }
         
-        
-        
+        guard let weight_textField_value = weight_textField.text,!weight_textField_value.isEmpty else{
+            print("體重不可以是空的")
+            return
+        }
+              
+        print("身高:\(height_textField_value)")
+        print("體重:\(weight_textField_value)")
     }
 
 
