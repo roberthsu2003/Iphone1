a5202ad9b66e601f7d2d906d52445ec8f611601e
roberthsu2003 <roberthsu2003@gmail.com>
2025-02-18 10:29:28 +0800

    modify

---
+++ 士林1/lesson5/lesson5_3/lesson4_2/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  lesson4_2
+//
+//  Created by 徐國堂 on 2025/2/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var height_textFile: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(age)
+        
+    }
+
+
+}
+
