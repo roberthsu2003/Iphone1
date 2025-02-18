d780a207f4dbc80ce947389971fa29c1dd2bf57c
roberthsu2003 <roberthsu2003@gmail.com>
2025-02-18 11:22:38 +0800

    modify

--- 士林1/lesson5/lesson5_3/lesson4_2/ViewController.swift
+++ 士林1/lesson5/lesson5_3/lesson4_2/ViewController.swift
@@ -8,11 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet var height_textFile: UITextField!
+    @IBOutlet var height_textField: UITextField!
+    @IBOutlet var weight_textField: UITextField!
+    @IBOutlet var caculate_button:UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(age)
+        //force unwrapping
+        print(height_textField.text!)
+        //optional binding
+        /*
+        if let weight_value = weight_textField.text, !weight_value.isEmpty{
+            print("不是空的")
+        }else{
+            print("是空的")
+        }
+         */
+        guard let weight_value = weight_textField.text, !weight_value.isEmpty else {
+            print("是空的")
+            return
+        }
+        
+        print(weight_value)
+        
+        
+        
         
     }
 
