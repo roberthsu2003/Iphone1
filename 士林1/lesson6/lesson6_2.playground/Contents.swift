30ca8bfd993ef6981e00734f0b759bb1201ef028
roberthsu2003 <roberthsu2003@gmail.com>
2025-02-26 11:58:11 +0800

    modify

--- 士林1/lesson6/lesson6_2.playground/Contents.swift
+++ 士林1/lesson6/lesson6_2.playground/Contents.swift
@@ -43,6 +43,37 @@ struct Point1{
 }
 
 
+struct Point2{
+    var x=0.0, y=0.0
+    
+    mutating func moveBy(x:Double, y:Double){ //mutating func才可以更改store property
+        self = Point2(x: x + self.x, y: y + self.y)
+    }
+}
+
+enum TriStateSwitch{
+    case off, low, high
+    
+    mutating func toggle(){
+        switch self{
+        case .off:
+            self = .low
+        case .low:
+            self = .high
+        case .high:
+            self = .off
+        }
+    }
+}
+
+var ovenLight = TriStateSwitch.low
+ovenLight.toggle()
+ovenLight.toggle()
+ovenLight.toggle()
+
+
+
+
 
 
 
