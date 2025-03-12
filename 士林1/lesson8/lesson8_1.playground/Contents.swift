8ddd69190dfece7d2290ee65428e383b8fac1321
roberthsu2003 <roberthsu2003@gmail.com>
2025-03-12 09:24:46 +0800

    modify

---
+++ 士林1/lesson8/lesson8_1.playground/Contents.swift
@@ -0,0 +1,19 @@
+struct Fahrenheit{
+    var temperature: Double
+    init(){
+        temperature = 32.0
+    }
+}
+
+var f = Fahrenheit()
+f.temperature
+
+struct Fahrenheit1{
+    var temperature = 32.0
+}
+
+var f1 = Fahrenheit1()
+f1.temperature
+
+
+
