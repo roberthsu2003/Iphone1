6bbc7125ab4e9703c341fc10da265a6a2b7133d2
roberthsu2003 <roberthsu2003@gmail.com>
2025-03-12 09:35:02 +0800

    modify

--- 士林1/lesson8/lesson8_1.playground/Contents.swift
+++ 士林1/lesson8/lesson8_1.playground/Contents.swift
@@ -15,5 +15,31 @@ struct Fahrenheit1{
 var f1 = Fahrenheit1()
 f1.temperature
 
+struct Celsius{
+    var temperatureInCelsius: Double
+    init(fromFahrenheit fahrenheit:Double){
+        temperatureInCelsius = (fahrenheit - 32) * 5 / 9
+    }
+    
+    init(fromKelvin kelvin:Double){
+        temperatureInCelsius = kelvin - 273.15
+    }
+    
+    init(_ celcius:Double){
+        temperatureInCelsius = celcius
+    }
+}
+
+let boilingPointOfWaterInCelsius = Celsius(fromFahrenheit: 212.0)
+
+let freezingPointOfWaterInCelsius = Celsius(fromKelvin: 273.15)
+
+let bodyTemperatureInCelsius = Celsius(36.0)
+
+
+
+
+
+
 
 
