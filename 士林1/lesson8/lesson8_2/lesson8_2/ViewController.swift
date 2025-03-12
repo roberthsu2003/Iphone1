8f8e0de5408e3202c0aeee6406b071a37a0810d8
roberthsu2003 <roberthsu2003@gmail.com>
2025-03-12 10:04:07 +0800

    modify

---
+++ 士林1/lesson8/lesson8_2/lesson8_2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lesson8_2
+//
+//  Created by 徐國堂 on 2025/3/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
