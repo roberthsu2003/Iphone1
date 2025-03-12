d395d765463a08a0cd302d4b676481c081eb863f
roberthsu2003 <roberthsu2003@gmail.com>
2025-03-12 09:55:14 +0800

    modify

--- 士林1/lesson8/lesson8_1.playground/Contents.swift
+++ 士林1/lesson8/lesson8_1.playground/Contents.swift
@@ -67,7 +67,29 @@ struct Size{
     var width = 0.0, height = 0.0
 }
 
-Size(height: 10)
+struct Point{
+    var x = 0.0, y = 0.0
+}
+
+struct Rect{
+    var origin = Point()
+    var size = Size()
+    init(){}
+    init(origin: Point, size: Size) {
+        self.origin = origin
+        self.size = size
+    }
+    
+    init (center: Point, size: Size){
+        let originX = center.x - size.width / 2
+        let originY = center.y - size.height / 2
+        self.init(origin: Point(x: originX, y: originY), size: size)
+    }
+}
+
+
+
+
 
 
 
