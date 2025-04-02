44dfd67e764b4746965634a8cd78421661a5859d
roberthsu2003 <roberthsu2003@gmail.com>
2025-04-02 10:52:28 +0800

    modify

--- 士林1/lesson10/lesson10_3/lesson10_3/ViewController.swift
+++ 士林1/lesson10/lesson10_3/lesson10_3/ViewController.swift
@@ -17,9 +17,29 @@ class ViewController: UITableViewController {
         guard let pathURL = bundle.url(forResource: "citylist", withExtension: "plist")else{
             return
         }
-        self.cities = NSArray(contentsOf: pathURL) as? [[String:Any]] ?? []
-                
+        cities = NSArray(contentsOf: pathURL) as? [[String:Any]] ?? []
+        
     }
+    
+    //MARK: - UITableViewDataSource
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cities.count
+    }
+
+
+    // Provide a cell object for each row.
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+       let index = indexPath.row
+       let cell = tableView.dequeueReusableCell(withIdentifier: "cellTypeIdentifier", for: indexPath)
+       
+       // Configure the cell’s contents.
+       let city = cities[index]
+       
+       cell.textLabel!.text = city["City"] as? String
+           
+       return cell
+    }
+    //MARK: - UITableViewDelegate
 
 
 }
