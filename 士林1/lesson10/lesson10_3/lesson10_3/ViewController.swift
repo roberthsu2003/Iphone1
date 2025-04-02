2f1a1509b8f2fdb55c6fb9b0d745dc8d272c533f
roberthsu2003 <roberthsu2003@gmail.com>
2025-04-02 10:23:30 +0800

    modify

---
+++ 士林1/lesson10/lesson10_3/lesson10_3/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  lesson10_3
+//
+//  Created by 徐國堂 on 2025/4/2.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    
+    var cities = [[String:Any]]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let bundle = Bundle.main
+        guard let pathURL = bundle.url(forResource: "citylist", withExtension: "plist")else{
+            return
+        }
+        self.cities = NSArray(contentsOf: pathURL) as? [[String:Any]] ?? []
+                
+    }
+
+
+}
+
