e1709f4c3890e4e8440f361b923a788913675190
roberthsu2003 <roberthsu2003@gmail.com>
2025-04-10 20:23:56 +0800

    modify

--- 台南/2025_02_13/lesson5_3.playground/Contents.swift
+++ 台南/2025_02_13/lesson5_3.playground/Contents.swift
@@ -5,10 +5,27 @@ import UIKit
 //呼叫function
 
 //建立一個function
-func greet(person:String)->String{
-    let greeting = "Hello, \(person)!"
+func greet(person1:String)->String{
+    let greeting = "Hello, \(person1)!"
     return greeting
 }
 
 //呼叫function
-print(greet(person:"Alice"))
+print(greet(person1:"Alice"))
+
+//提早離開function
+//guard....else
+
+func greet(person:String?=nil, location:String?=nil){
+    guard let name = person else{
+        return
+    }
+    print("您好:\(name)")
+    
+    guard let location = location else{
+        return
+    }
+    print("希望您在:\(location)")
+}
+
+greet(person:"Bob",location: "台北")
