4b2e674bac868ec45f9b3b91b46f68206d4adf46
roberthsu2003 <roberthsu2003@gmail.com>
2025-04-16 09:58:58 +0800

    modify

---
+++ 士林1/lesson12/lesson12_2.playground/Contents.swift
@@ -0,0 +1,44 @@
+//Nested Types
+
+struct BlackjackCard{
+    enum Suit:Character{
+        case spades = "♠️"
+        case hearts = "❤️"
+        case diamonds = "♦️"
+        case clubs = "♣️"
+    }
+    
+    enum Rank:Int{
+        case two = 2, three, four, five, six, seven, eight, nine, ten
+        case jack, queen, king , ace
+        
+        struct Values{
+            let first:Int, second:Int?
+        }
+        
+        var values:Values{
+            switch self{
+            case .ace:
+                return Values(first:1, second:11)
+            case .jack, .queen, .king:
+                return Values(first:10, second:nil)
+            default:
+                return Values(first:self.rawValue, second:nil)
+            }
+        }
+    }
+    
+    let rank:Rank, suit:Suit
+    var description:String{
+        var output = "suit is \(suit.rawValue)"
+        output += ", rank is \(rank.values.first)"
+        if let second = rank.values.second{
+            output += ", or \(second)"
+        }
+        return output
+    }
+}
+
+var card1 = BlackjackCard(rank: .ace, suit: .spades)
+print(card1.description)
+
