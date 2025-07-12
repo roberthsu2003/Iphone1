edb6e8ce3cb04b899edf4a75854291bc85c26807
徐國堂 <roberthsu2003@gmail.com>
2025-07-12 19:42:41 +0800

    modify

--- 台南/2025_02_13/lesson7_1.playground/Contents.swift
+++ 台南/2025_02_13/lesson7_1.playground/Contents.swift
@@ -12,3 +12,23 @@ func greet1(person:String, from hometown:String) -> String {
 }
 
 greet1(person: "Bill", from: "Cupertino")
+
+func addTwoInts(_ a: Int, _ b: Int) -> Int {
+    return a + b
+}
+
+func multiplyTwoInts(_ a: Int, _ b: Int) -> Int {
+    return a * b
+}
+
+var mathFunction: (Int, Int) -> Int = addTwoInts
+
+mathFunction(10, 20)
+
+mathFunction = multiplyTwoInts
+
+mathFunction(10, 20)
+
+
+
+
