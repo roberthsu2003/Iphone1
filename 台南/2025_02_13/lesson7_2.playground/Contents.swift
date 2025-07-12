952e64e5fb53b7e15c4882da4c36a604f99b2f93
徐國堂 <roberthsu2003@gmail.com>
2025-07-12 20:28:01 +0800

    modify

--- 台南/2025_02_13/lesson7_2.playground/Contents.swift
+++ 台南/2025_02_13/lesson7_2.playground/Contents.swift
@@ -13,3 +13,18 @@ names.sorted(
     by:{(s1:String,s2:String)->Bool in
         return s1 > s2}
     )
+
+names.sorted(
+    by:{(s1,s2) in return s1 > s2}
+    )
+
+names.sorted(
+    by:{(s1,s2) in s1 > s2}
+    )
+
+names.sorted(
+    by:{$0 > $1}
+    )
+
+names.sorted(by: <)
+
