3061c725a11e1901131e433534c6290dcdb6e436
徐國堂 <roberthsu2003@gmail.com>
2025-08-29 20:12:59 +0800

    modify

--- 台南/2025_02_13/lesson8_1.playground/Contents.swift
+++ 台南/2025_02_13/lesson8_1.playground/Contents.swift
@@ -22,4 +22,11 @@ print("The width of someVideoMode is \(someVideoMode.resolution.width)")
 someVideoMode.resolution.width = 1280
 print("The width of someVideoMode is now \(someVideoMode.resolution.width)")
 
+let vga = Resolution(width: 640, height: 480) //智慧型成員初始化,只有結構有
+print("The width of VGA is \(vga.width)")
+print("The height of VGA is \(vga.height)")
+
+//結構是Value Type
+//類別是Reference Type
+
 
