9161d01037b43cee4af945763721432b3f0b951d
徐國堂 <roberthsu2003@gmail.com>
2025-08-29 20:34:42 +0800

    modify

--- 台南/2025_02_13/lesson8_1.playground/Contents.swift
+++ 台南/2025_02_13/lesson8_1.playground/Contents.swift
@@ -27,6 +27,30 @@ print("The width of VGA is \(vga.width)")
 print("The height of VGA is \(vga.height)")
 
 //結構是Value Type
-//類別是Reference Type
+//value type傳遞給別人時,是copy一份實體
+
+let hd = Resolution(width: 1920, height: 1080)
+var cinema = hd
+cinema.width = 2048
+print("The width of HD is \(hd.width)")
+print("The width of cinema is \(cinema.width)")
+
+//類別是Reference Type,傳遞給別人時,是傳遞參考(遙控器)
+let tenEighty = VideoMode()
+tenEighty.resolution = hd
+tenEighty.interlanced = true
+tenEighty.frameRate = 25.0
+tenEighty.name = "1080i"
+
+let alsoTenEighty = tenEighty
+alsoTenEighty.frameRate = 30.0
+tenEighty.frameRate
+
+
+
+
+
+
+
 
 
