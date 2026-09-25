d75a1955752dc529e7b5e9849a1465301054e340
roberthsu2003 <roberthsu2003@gmail.com>
2026-09-25 19:34:48 +0800

    modify

---
+++ 台南/0925_1/0925_1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  0925_1
+//
+//  Created by roberthsu2003 on 2026/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 台南/0925_1/0925_1/_925_1App.swift
@@ -0,0 +1,17 @@
+//
+//  _925_1App.swift
+//  0925_1
+//
+//  Created by roberthsu2003 on 2026/9/25.
+//
+
+import SwiftUI
+
+@main
+struct _925_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
